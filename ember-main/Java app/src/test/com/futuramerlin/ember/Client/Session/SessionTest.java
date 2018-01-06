e7eaec29034ed1783052ec54f3c5830331ae5321
Elliot Chandler <elliot@futuramerlin.com>
2018-01-06 17:33:03 -0500

    Update Ember tree

---
+++ ember-main/Java app/src/test/com/futuramerlin/ember/Client/Session/SessionTest.java
@@ -0,0 +1,17 @@
+package com.futuramerlin.ember.Client.Session;
+
+import com.futuramerlin.ember.Client.Session.Session;
+import org.junit.Test;
+
+/**
+ * Created by elliot on 14.11.27.
+ */
+public class SessionTest {
+
+    @Test
+    public void testCreateSession() throws Exception {
+        Session p = new Session();
+
+    }
+
+}
