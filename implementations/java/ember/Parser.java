ed9fe7c7b7c98e8f73ea3bf5a467f66a1eff78b8
Elliot Chandler <elliot@futuramerlin.com>
2018-01-07 17:14:29 -0500

    Update Ember tree

--- implementations/java/ember/Parser.java
+++ implementations/java/ember/Parser.java
@@ -20,7 +20,28 @@ public class Parser {
         return parseBytes(input.getBytes(StandardCharsets.UTF_8));
     }
     public Document parseBytes(byte[] input) {
-        getout
+        getoutRuntime rt = Runtime.getRuntime();
+        String[] commands = {"system.exe","-get t"};
+        Process proc = rt.exec(commands);
+
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(proc.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(proc.getErrorStream()));
+
+// read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+
+// read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
         return null;
     }
 }
