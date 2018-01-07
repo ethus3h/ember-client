d7dea56a91fdfbba4658ad827a88413201bc601d
Elliot Chandler <elliot@futuramerlin.com>
2018-01-07 17:21:08 -0500

    Update Ember tree

--- implementations/java/ember/Parser.java
+++ implementations/java/ember/Parser.java
@@ -28,22 +28,8 @@ public class Parser {
         Process process = rt.exec(commands);
 
         // Read stdout of command
-        new InputStreamReader(process.getInputStream())
-        BufferedReader stdInput = new BufferedReader();
+        InputStreamReader stdInput = new InputStreamReader(process.getInputStream());
 
-        BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-        // read the output from the command
-        String s = null;
-        while ((s = stdInput.readLine()) != null) {
-            System.out.println(s);
-        }
-
-        // read any errors from the attempted command
-        System.out.println("Here is the standard error of the command (if any):\n");
-        while ((s = stdError.readLine()) != null) {
-            System.out.println(s);
-        }
         return null;
     }
 }
