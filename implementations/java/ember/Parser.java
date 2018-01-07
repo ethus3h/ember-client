bdf0e312b76a2eab3c71ff57fff347270ff4054b
Elliot Chandler <elliot@futuramerlin.com>
2018-01-07 17:12:19 -0500

    Update Ember tree

--- implementations/java/ember/Parser.java
+++ implementations/java/ember/Parser.java
@@ -5,6 +5,7 @@ import ember.Common.Exception.CommandExecutionError;
 import org.w3c.dom.*;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -16,7 +17,7 @@ public class Parser {
         return parseBytes(Files.readAllBytes(Paths.get(path)));
     }
     public Document parseString(String input) {
-        return null;
+        return parseBytes(input.getBytes(StandardCharsets.UTF_8));
     }
     public Document parseBytes(byte[] input) {
         return null;
