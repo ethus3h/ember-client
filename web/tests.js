418e2a840d94580d3349394f3848741a902a2693
ethus3h <kolubat@gmail.com>
2018-02-20 14:57:03 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -10,6 +10,7 @@ oReq.onload = function (oEvent) {
   var arrayBuffer = oReq.response; // Note: not oReq.responseText
   if (arrayBuffer) {
     var byteArray = new Uint8Array(arrayBuffer);
+    console.log(buf2hex(arrayBuffer));
     for (var i = 0; i < byteArray.byteLength; i++) {
       // do something with each byte in the array
       console.log(byteArray[i]);
