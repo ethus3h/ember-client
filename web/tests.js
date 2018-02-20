757046bb2a17fd788fbaa4980af015697383d5c3
ethus3h <kolubat@gmail.com>
2018-02-20 14:27:50 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -1,3 +1,14 @@
 import eite.js
 
+var oReq = new XMLHttpRequest();
+oReq.open("GET", "idiomatic-hello-world.sems", true);
+oReq.responseType = "blob";
+
+oReq.onload = function(oEvent) {
+  var blob = oReq.response;
+  
+};
+
+oReq.send();
+
 em_execute("idiomatic-hello-world.sems")
