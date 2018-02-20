870c7c9a0585bb52a676f9b15695654c6c98b675
ethus3h <kolubat@gmail.com>
2018-02-20 14:29:00 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -2,13 +2,18 @@ import eite.js
 
 var oReq = new XMLHttpRequest();
 oReq.open("GET", "idiomatic-hello-world.sems", true);
-oReq.responseType = "blob";
+oReq.responseType = "arraybuffer";
 
-oReq.onload = function(oEvent) {
-  var blob = oReq.response;
-  
+oReq.onload = function (oEvent) {
+  var arrayBuffer = oReq.response; // Note: not oReq.responseText
+  if (arrayBuffer) {
+    var byteArray = new Uint8Array(arrayBuffer);
+    for (var i = 0; i < byteArray.byteLength; i++) {
+      // do something with each byte in the array
+    }
+  }
 };
 
-oReq.send();
+oReq.send(null);
 
 em_execute("idiomatic-hello-world.sems")
