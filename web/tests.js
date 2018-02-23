b1f25b17a59de8331ff0e968c0a5734ba150aa10
ethus3h <kolubat@gmail.com>
2018-02-22 23:26:35 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -1,52 +1,50 @@
-window.onload = function () {
+window.onload = function() {
 
-// Set-up & utility functions
-{
-    // Return a hexdump of an ArrayBuffer
-    function buf2hex(buffer) { // buffer is an ArrayBuffer
-      return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+    // Set-up & utility functions
+    {
+        // Return a hexdump of an ArrayBuffer
+        function buf2hex(buffer) { // buffer is an ArrayBuffer
+            return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+        }
+
+        // Attach console.log to log element
+        (function() {
+            var old = console.log;
+            var logger = document.getElementById('log');
+            console.log = function(message) {
+                if (typeof message == 'object') {
+                    logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+                } else {
+                    logger.innerHTML += message + '<br />';
+                }
+                logger.scrollTop = logger.scrollHeight;
+            }
+        })();
     }
 
-    // Attach console.log to log element
-    (function () {
-        var old = console.log;
-        var logger = document.getElementById('log');
-        console.log = function (message) {
-            if (typeof message == 'object') {
-                logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
-            } else {
-                logger.innerHTML += message + '<br />';
+    // Main logic
+    function handleDocument(arrayBuffer) {
+        // Takes an ArrayBuffer
+        if (arrayBuffer) {
+            var byteArray = new Uint8Array(arrayBuffer);
+            for (var i = 0; i < byteArray.byteLength; i++) {
+                // do something with each byte in the array. byteArray[i] holds the decimal value of the given byte.
+                console.log(byteArray[i]);
             }
-            logger.scrollTop = logger.scrollHeight;
         }
-    })();
-}
+    }
 
-// Main logic
-function handleDocument(arrayBuffer) {
-    // Takes an ArrayBuffer
-      if (arrayBuffer) {
-        var byteArray = new Uint8Array(arrayBuffer);
-        for (var i = 0; i < byteArray.byteLength; i++) {
-          // do something with each byte in the array. byteArray[i] holds the decimal value of the given byte.
-          console.log(byteArray[i]);
-        }
-      }
-}
-function eiteFromUrl() {
-    var oReq = new XMLHttpRequest();
-    oReq.open("GET", "idiomatic-hello-world.sems", true);
-    oReq.responseType = "arraybuffer";
+    function eiteFromUrl() {
+        var oReq = new XMLHttpRequest();
+        oReq.open("GET", "idiomatic-hello-world.sems", true);
+        oReq.responseType = "arraybuffer";
 
-    oReq.onload = function (oEvent) {
-      handleDocument( oReq.response); // Note: not oReq.responseText
-     
-      
-    };oReq.send(null);
+        oReq.onload = function(oEvent) {
+            handleDocument(oReq.response); // Note: not oReq.responseText
 
-}
+        };
+        oReq.send(null);
 
-
-
+    }
 
 }
