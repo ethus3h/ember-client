946410700047201aa03ac2d38a4842bee2d8325f
ethus3h <kolubat@gmail.com>
2018-02-22 19:23:23 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -1,7 +1,8 @@
 window.onload = function () {
 
-// Set-up
+// Set-up & utility functions
 {
+    // Return a hexdump of an ArrayBuffer
     function buf2hex(buffer) { // buffer is an ArrayBuffer
       return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
     }
