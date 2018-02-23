6f1ac311a4c4c78f40ba84338043b1964b38ed60
ethus3h <kolubat@gmail.com>
2018-02-22 19:12:24 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -2,6 +2,19 @@ function buf2hex(buffer) { // buffer is an ArrayBuffer
   return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
 }
 
+// from https://stackoverflow.com/questions/20256760/javascript-console-log-to-html
+(function () {
+    var old = console.log;
+    var logger = document.getElementById('log');
+    console.log = function (message) {
+        if (typeof message == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+        } else {
+            logger.innerHTML += message + '<br />';
+        }
+    }
+})();
+
 var oReq = new XMLHttpRequest();
 oReq.open("GET", "idiomatic-hello-world.sems", true);
 oReq.responseType = "arraybuffer";
