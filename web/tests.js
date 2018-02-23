cb55cdb7aef626723808a5232e72dd34c28f845b
ethus3h <kolubat@gmail.com>
2018-02-23 00:00:46 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -41,9 +41,10 @@ window.onload = function() {
                 return renderOutputBuf;
             }
             doc.run = function () {
-                for (var i = 0; i < dcSeq.length; i++) {
-                    console.log(dcSeq[i]);
-                }
+                // for (var i = 0; i < dcSeq.length; i++) {
+                //     console.log(dcSeq[i]);
+                // }
+                render();
             };
         return doc;
     }
