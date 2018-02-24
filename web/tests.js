e97fe72bf757d29db8d13f81f8c8469660991525
ethus3h <kolubat@gmail.com>
2018-02-23 21:38:44 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -1,11 +1,11 @@
 window.onload = function() {
 
 
-// This function check if number n is between a and b 
-// from https://stackoverflow.com/questions/28087842/check-if-number-is-between-two-numbers
-function isBetween(n, a, b) {
-   return (n - a) * (n - b) <= 0
-}
+    // Checks whether n is within the range a and b, including endpoints
+    function isBetween(n, a, b) {
+        return (n - a) * (n - b) <= 0;
+    }
+
     // Attach console.log to log element
     (function() {
         var logger = document.getElementById('log');
