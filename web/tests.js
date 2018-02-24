f652915bb6cf1a411e664d5d503fed76325cdb97
ethus3h <kolubat@gmail.com>
2018-02-23 21:41:07 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -5,10 +5,11 @@ window.onload = function() {
     function isBetween(n, a, b) {
         return (n - a) * (n - b) <= 0;
     }
-0    NUL    16    DLE    32    SP    48    0    64    @    80    P    96    `    112    p
-1    SOH    17    DC1    33    !    49    1    65    A    81    Q    97    a    113    q
-2    STX    18    DC2    34    "    50    2    66    B    82    R    98    b    114    r
-3    ETX    19    DC3    35    #    51    3    67    C    83    S    99    c    115    s
+/*
+0    NUL    16    DLE    32    SP   48    0    64    @    80    P    96     `    112    p
+1    SOH    17    DC1    33    !    49    1    65    A    81    Q    97     a    113    q
+2    STX    18    DC2    34    "    50    2    66    B    82    R    98     b    114    r
+3    ETX    19    DC3    35    #    51    3    67    C    83    S    99     c    115    s
 4    EOT    20    DC4    36    $    52    4    68    D    84    T    100    d    116    t
 5    ENQ    21    NAK    37    %    53    5    69    E    85    U    101    e    117    u
 6    ACK    22    SYN    38    &    54    6    70    F    86    V    102    f    118    v
@@ -21,6 +22,7 @@ window.onload = function() {
 13   CR     29    GS     45    -    61    =    77    M    93    ]    109    m    125    }
 14   SO     30    RS     46    .    62    >    78    N    94    ^    110    n    126    ~
 15   SI     31    US     47    /    63    ?    79    O    95    _    111    o    127    DEL
+*/
 
     // Attach console.log to log element
     (function() {
