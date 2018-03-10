b7c1c0d88768a85cb6e590004e6a43953e4f5551
ethus3h <kolubat@gmail.com>
2018-03-09 20:06:31 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -62,11 +62,21 @@ window.onload = function() {
             type=dcGetType(dc);
             generalType=type[0];
             switch(type) {
+                case 'Zl':
+                    return false;
+                    break;
+                default:
+                    break;
+            }
+            switch(generalType) {
                 case '!':
                 case 'C':
                     return true;
+                    break;
+                default:
+                    break;
             }
-            return false; // TODO: Unimplemented
+            return true; // TODO: Unimplemented
         }
     }
 
