c3009e2d9076ae31874eac8fddd277a90f6a7846
ethus3h <kolubat@gmail.com>
2018-03-09 19:45:05 -0500

    Update Ember tree

--- web/tests.js
+++ web/tests.js
@@ -332,6 +332,7 @@ window.onload = function() {
     ];
 
     loadDatasets(function() {
+        console.log(dcData);
         // This is where the actual commands to run (entry points) should go
         runEiteTest('ept', 'idiomatic-hello-world-sems');
         docFromUrl('sems', 'idiomatic-hello-world.sems', function (doc) { doc.run(); } );
