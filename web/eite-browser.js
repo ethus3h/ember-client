5e2da098fc7e58218f3d11bcff947723d7d38bfd
ethus3h <kolubat@gmail.com>
2018-03-12 17:54:39 -0400

    Update Ember tree

--- web/eite-browser.js
+++ web/eite-browser.js
@@ -1,155 +1,155 @@
-    // Implementation-specific overrides of routines available portably
+// Implementation-specific overrides of routines available portably
 
-    // Override error reporting method to show alert
-    function eiteError(message) {
-        console.trace();
-        eiteLog('EITE reported error!: '+normalizeMessage(message));
-        alert('EITE reported error!: '+normalizeMessage(message));
-        throw 'EITE reported error!: '+normalizeMessage(message);
+// Override error reporting method to show alert
+function eiteError(message) {
+    console.trace();
+    eiteLog('EITE reported error!: '+normalizeMessage(message));
+    alert('EITE reported error!: '+normalizeMessage(message));
+    throw 'EITE reported error!: '+normalizeMessage(message);
+}
+function eiteWarn(message) {
+    console.trace();
+    eiteLog('EITE reported warning: '+normalizeMessage(message));
+    alert('EITE reported warning: '+normalizeMessage(message));
+}
+
+// Fully implementation-specific code
+
+function eiteImplLog(message) {
+    // This function implements logging (which may differ between platforms).
+    console.log(normalizeMessage(message));
+};
+
+function getEnvironmentBestFormat() {
+    return 'immutableCharacterCells';
+}
+
+function getEnvironmentRenderTraits(targetFormat) {
+    if ( targetFormat === undefined ) {
+        eiteError('getEnvironmentRenderTraits was called without any targetFormat!');
     }
-    function eiteWarn(message) {
-        console.trace();
-        eiteLog('EITE reported warning: '+normalizeMessage(message));
-        alert('EITE reported warning: '+normalizeMessage(message));
-    }
-
-    // Fully implementation-specific code
-
-    function eiteImplLog(message) {
-        // This function implements logging (which may differ between platforms).
-        console.log(normalizeMessage(message));
-    };
-
-    function getEnvironmentBestFormat() {
-        return 'immutableCharacterCells';
-    }
-
-    function getEnvironmentRenderTraits(targetFormat) {
-        if ( targetFormat === undefined ) {
-            eiteError('getEnvironmentRenderTraits was called without any targetFormat!');
-        }
-        var traits = {};
-        switch (targetFormat) {
-            case 'integerList':
-            case 'immutableCharacterCells':
-                traits.cellTableWidth = -1; // unlimited
-                traits.cellTableHeight = -1; // unlimited
-                let cs = document.characterSet.toLowerCase();
-                switch(cs) {
-                    case 'utf-8':
-                        traits.characterEncoding = 'UTF-8';
-                        break;
-                    default:
-                        eiteWarn('Unimplemented character set: '+cs+'. Falling back to ASCII-safe-subset.');
-                        traits.characterEncoding = 'ASCII-safe-subset';
-                        break;
-                }
-                break;
-        }
-        return traits;
-    }
-
-    function loadCsv(url, lineLoadedCallback, documentLoadedCallback, errorCallback) {
-        Papa.parse(url, {
-            download: true,
-            encoding: 'UTF-8',
-            newline: "\n",
-            delimiter: ',',
-            quoteChar: '"',
-            step: function(results, parser) {
-                lineLoadedCallback(results, parser);
-            },
-            complete: function(results, file) {
-                documentLoadedCallback(results, file);
-            },
-            error: function(results, file) {
-                errorCallback(results, file);
+    var traits = {};
+    switch (targetFormat) {
+        case 'integerList':
+        case 'immutableCharacterCells':
+            traits.cellTableWidth = -1; // unlimited
+            traits.cellTableHeight = -1; // unlimited
+            let cs = document.characterSet.toLowerCase();
+            switch(cs) {
+                case 'utf-8':
+                    traits.characterEncoding = 'UTF-8';
+                    break;
+                default:
+                    eiteWarn('Unimplemented character set: '+cs+'. Falling back to ASCII-safe-subset.');
+                    traits.characterEncoding = 'ASCII-safe-subset';
+                    break;
             }
-        })
+            break;
     }
+    return traits;
+}
 
-    function doRenderIo(targetFormat, renderBuffer) {
-        switch (targetFormat) {
-            case 'integerList':
-            case 'immutableCharacterCells':
-                let immutableCharCellOutput = document.getElementById('log');
-                for (var i = 0; i < renderBuffer.length; i++) {
-                    immutableCharCellOutput.innerHTML += normalizeMessage(renderBuffer[i]) + '<br />';
-                    immutableCharCellOutput.scrollTop = immutableCharCellOutput.scrollHeight;
-                }
-                break;
-            default:
-                eiteError('Unimplemented render I/O format: '+targetFormat);
-                break;
+function loadCsv(url, lineLoadedCallback, documentLoadedCallback, errorCallback) {
+    Papa.parse(url, {
+        download: true,
+        encoding: 'UTF-8',
+        newline: "\n",
+        delimiter: ',',
+        quoteChar: '"',
+        step: function(results, parser) {
+            lineLoadedCallback(results, parser);
+        },
+        complete: function(results, file) {
+            documentLoadedCallback(results, file);
+        },
+        error: function(results, file) {
+            errorCallback(results, file);
         }
-    }
+    })
+}
 
-    function urlLoadForCallback(url, callback) {
-        var oReq = new XMLHttpRequest();
-        oReq.open("GET", url, true);
-        oReq.responseType = "arraybuffer";
-        oReq.onload = function(oEvent) {
-            callback(oReq.response); // Note: not oReq.responseText
-        };
-        oReq.send(null);
+function doRenderIo(targetFormat, renderBuffer) {
+    switch (targetFormat) {
+        case 'integerList':
+        case 'immutableCharacterCells':
+            let immutableCharCellOutput = document.getElementById('log');
+            for (var i = 0; i < renderBuffer.length; i++) {
+                immutableCharCellOutput.innerHTML += normalizeMessage(renderBuffer[i]) + '<br />';
+                immutableCharCellOutput.scrollTop = immutableCharCellOutput.scrollHeight;
+            }
+            break;
+        default:
+            eiteError('Unimplemented render I/O format: '+targetFormat);
+            break;
     }
+}
 
-    function docFromUrl(format, url, callback) {
-        urlLoadForCallback(url, function(responseArrayBuffer) { callback(createDocObj(format, responseArrayBuffer)); })
-    }
+function urlLoadForCallback(url, callback) {
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", url, true);
+    oReq.responseType = "arraybuffer";
+    oReq.onload = function(oEvent) {
+        callback(oReq.response); // Note: not oReq.responseText
+    };
+    oReq.send(null);
+}
 
-    function runEiteTest(format, name) {
-        urlPrefix='../tests/'+name+'.'+format+'/';
-        inFormatUrl='../tests/'+name+'.'+format+'/in-format';
-        switch (format) {
-            case 'ept': // Parser test
-                urlLoadForCallback(inFormatUrl, function(responseArrayBuffer) {})
-                break;
-            case 'comment':
-                if (isNewline(byteArray[i])) {
-                    parserState = 'dc';
-                }
-                break;
-            default:
-                eiteError('Unimplemented test format: '+format);
-                break;
-        }
-    }
+function docFromUrl(format, url, callback) {
+    urlLoadForCallback(url, function(responseArrayBuffer) { callback(createDocObj(format, responseArrayBuffer)); })
+}
 
-    // Set up dcData
-    dcData = [];
-    function dcDataAppendDataset(dataset) {
-        dcData[dataset] = [];
+function runEiteTest(format, name) {
+    urlPrefix='../tests/'+name+'.'+format+'/';
+    inFormatUrl='../tests/'+name+'.'+format+'/in-format';
+    switch (format) {
+        case 'ept': // Parser test
+            urlLoadForCallback(inFormatUrl, function(responseArrayBuffer) {})
+            break;
+        case 'comment':
+            if (isNewline(byteArray[i])) {
+                parserState = 'dc';
+            }
+            break;
+        default:
+            eiteError('Unimplemented test format: '+format);
+            break;
     }
-    function dcDataAppendLine(dataset, line) {
-        dcData[dataset].push(line);
-    }
-    function loadDatasets(callback) {
-        if (datasets.length > 0) {
-            let dataset = datasets[0];
-            dcDataAppendDataset(dataset);
-            loadCsv(
-                '../data/'+dataset+'.csv',
-                function(results,parser){
-                    dcDataAppendLine(dataset, results);
-                },
-                function(){
-                    datasets.shift();
-                    loadDatasets(callback);
-                },
-                function(){
-                    eiteError('Error reported while parsing '+dataset+'!')
-                }
-            );
-        }
-        else {
-            callback();
-        }
-    }
+}
 
-    datasets=[
-        'DcData',
-        'mappings/from/ascii',
-        'mappings/from/unicode',
-        'mappings/to/html'
-    ];
+// Set up dcData
+dcData = [];
+function dcDataAppendDataset(dataset) {
+    dcData[dataset] = [];
+}
+function dcDataAppendLine(dataset, line) {
+    dcData[dataset].push(line);
+}
+function loadDatasets(callback) {
+    if (datasets.length > 0) {
+        let dataset = datasets[0];
+        dcDataAppendDataset(dataset);
+        loadCsv(
+            '../data/'+dataset+'.csv',
+            function(results,parser){
+                dcDataAppendLine(dataset, results);
+            },
+            function(){
+                datasets.shift();
+                loadDatasets(callback);
+            },
+            function(){
+                eiteError('Error reported while parsing '+dataset+'!')
+            }
+        );
+    }
+    else {
+        callback();
+    }
+}
+
+datasets=[
+    'DcData',
+    'mappings/from/ascii',
+    'mappings/from/unicode',
+    'mappings/to/html'
+];
