922d807591ec860419fa3dd9250a94516cb820e3
ethus3h <kolubat@gmail.com>
2018-03-12 17:52:47 -0400

    Update Ember tree

--- web/eite.js
+++ web/eite.js
@@ -2,6 +2,7 @@
 // It also serves as an example implementation that should be written in a way that is easily ported to other platforms.
 // To serve these two goals, it should not use I/O, objects, first-class functions, or JavaScript-specific libraries.
 // Things that depend on I/O and JavaScript-specific libraries (e.g. logging using JSON.stringify) should be implemented in eite-[platform].js  (for platform-specific code) or eite-nonportable.js for JavaScript-specific code.
+// Those files should use clearly defined APIs that this file's code can call, so that they can be implemented as appropriate in other implementations.
 // dcData object must be available before calling these functions.
 
 function normalizeMessage(message) {
