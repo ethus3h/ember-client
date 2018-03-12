a23ebdaae0b561a07542918bb2179225124b95be
ethus3h <kolubat@gmail.com>
2018-03-12 17:54:35 -0400

    Update Ember tree

--- web/eite-browser.js
+++ web/eite-browser.js
@@ -153,9 +153,3 @@
         'mappings/from/unicode',
         'mappings/to/html'
     ];
-
-    loadDatasets(function() {
-        // This is where the actual commands to run (entry points) should go
-        runEiteTest('ept', 'idiomatic-hello-world-sems');
-        docFromUrl('sems', 'idiomatic-hello-world.sems', function (doc) { doc.run(); } );
-    });
