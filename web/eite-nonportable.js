70e20774d84d3f8124a3a0429506bd455e75b480
ethus3h <kolubat@gmail.com>
2018-03-14 01:06:31 -0400

    Update Ember tree

--- web/eite-nonportable.js
+++ web/eite-nonportable.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 // This file defines functions with implementations that are specific to the JavaScript implementation of EITE.
 // This is a library file, and should only initialize functions/variables, so that it can be loaded and run in parallel with other library files, and work regardless of the order they are loaded.
 
@@ -8,3 +9,4 @@ function normalizeMessage(message) {
         return message;
     }
 }
+// @license-end
