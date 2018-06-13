dd2d6733aec8d342af13b84aa36d6f396170f256
ethus3h <kolubat@gmail.com>
2018-06-13 17:25:21 -0400

    Update Git repository using egup-single

--- web/portable/type-conversion.js
+++ web/portable/type-conversion.js
@@ -2,28 +2,20 @@
 /* Depends:
     implStrFromByte
     implStrFromUnicodeHex
-    implIntDcarrLength
-    implIntBytearrayLength
     implIntFromStr
 */
 
-async function strFromByte(intInput) {
-    await assertIsInt(intInput);
-    return await implStrFromByte(intInput)
+async function intFromStr(str) {
+    await assertIsStr(str);
+    return await implIntFromStr(str);
+}
+
+async function strFromByte(int) {
+    await assertIsInt(int);
+    return await implStrFromByte(int)
 }
 
 async function strFromUnicodeHex(strInput) {
     await assertIsStr(strInput)
     return await implStrFromUnicodeHex(strInput);
-}
-
-async function intDcarrLength(dcarrInput) {
-    return await implIntDcarrLength(dcarrInput);
-}
-async function intBytearrayLength(bytearrayInput) {
-    return await implIntBytearrayLength(bytearrayInput);
-}
-async function intFromStr(str) {
-    await assertIsStr(str);
-    return await implIntFromStr(str);
 }
--- web/portable/type-tools.js
+++ web/portable/type-tools.js
@@ -7,23 +7,10 @@
     implIntFromStr
 */
 
-async function strFromByte(intInput) {
-    await assertIsInt(intInput);
-    return await implStrFromByte(intInput)
+async function intBytearrayLength(bytearray) {
+    return await implIntBytearrayLength(bytearray);
 }
 
-async function strFromUnicodeHex(strInput) {
-    await assertIsStr(strInput)
-    return await implStrFromUnicodeHex(strInput);
-}
-
-async function intDcarrLength(dcarrInput) {
-    return await implIntDcarrLength(dcarrInput);
-}
-async function intBytearrayLength(bytearrayInput) {
-    return await implIntBytearrayLength(bytearrayInput);
-}
-async function intFromStr(str) {
-    await assertIsStr(str);
-    return await implIntFromStr(str);
+async function intDcarrLength(dcarr) {
+    return await implIntDcarrLength(dcarr);
 }
