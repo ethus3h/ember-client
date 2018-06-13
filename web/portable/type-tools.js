f3676b4e6d807e2b974c0912434e0b17207d8113
ethus3h <kolubat@gmail.com>
2018-06-13 17:27:06 -0400

    Update Git repository using egup-single

--- web/portable/type-tools.js
+++ web/portable/type-tools.js
@@ -1,10 +1,7 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* Depends:
-    implStrFromByte
-    implStrFromUnicodeHex
     implIntDcarrLength
     implIntBytearrayLength
-    implIntFromStr
 */
 
 async function intBytearrayLength(bytearray) {
