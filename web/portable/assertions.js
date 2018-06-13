b7c5416e5eb7c6013e46d6ade2201806bb55fe9c
ethus3h <kolubat@gmail.com>
2018-06-13 17:18:14 -0400

    Update Git repository using egup-single

--- web/portable/assertions.js
+++ web/portable/assertions.js
@@ -1,5 +1,9 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
-// Depends: implAssertIsInt implAssertIsStr implAssertStrContainsOnlyInt
+/* Depends:
+    implAssertIsInt
+    implAssertIsStr
+    implAssertStrContainsOnlyInt
+*/
 
 async function assertIsInt(int) {
     return await implAssertIsInt(int);
