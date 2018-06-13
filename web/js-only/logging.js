ba2b7b2c8f883b2b9f0f7517e5ec57d2913248c4
ethus3h <kolubat@gmail.com>
2018-06-13 17:39:09 -0400

    Update Git repository using egup-single

--- web/js-only/logging.js
+++ web/js-only/logging.js
@@ -1,10 +1,15 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
-/* Depends:
+/* Provides:
     implDie
     implEiteLog
 */
 
+async function implDie(strMessage) {
+    throw strMessage;
+}
+
 async function implEiteLog(strMessage) {
-    // This function implements logging (which may differ between platforms).
-    console.log(implStrNormalizeMessage(strMessage));
+    // Log the provided message
+    await assertIsStr(strMessage);
+    console.log(await implStrNormalizeMessage(strMessage));
 };
--- web/web-only/eite-nonportable.js
+++ web/web-only/eite-nonportable.js
@@ -2,10 +2,6 @@
 // This file defines functions with implementations that are specific to the JavaScript implementation of EITE.
 // This is a library file, and should only initialize functions/variables, so that it can be loaded and run in parallel with other library files, and work regardless of the order they are loaded.
 
-async function die(strMessage) {
-    throw strMessage;
-}
-
 async function implStrNormalizeMessage(message) {
     if (typeof message == 'object') {
         return (JSON && JSON.stringify ? JSON.stringify(message) : message);
