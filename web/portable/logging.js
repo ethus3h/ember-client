569f66ee03ceb691bc751dc8af354d9880876f58
ethus3h <kolubat@gmail.com>
2018-06-13 18:20:20 -0400

    Update Git repository using egup-single

--- web/portable/logging.js
+++ web/portable/logging.js
@@ -6,26 +6,33 @@
 */
 
 async function die(strMessage) {
+    await assertIsStr(strMessage);
+
     await implDie(strMessage);
 }
 
 async function eiteLog(strMessage) {
     await assertIsStr(strMessage);
+
     await implEiteLog(strMessage);
 }
 
 async function eiteWarn(strMessage) {
     await assertIsStr(strMessage);
+
     await eiteLog('EITE reported warning: '+strMessages);
 }
 
 async function eiteError(strMessage) {
-    await assertIsStr(strMessage); // FIXME: recursive loop?
+    // Don't run await assertIsStr(strMessage); here since it can call eiteError, making a recursive loop
+
     await eiteLog('EITE reported error!: '+strMessage);
     await die('EITE reported error!: '+strMessage);
 }
 
 async function eiteFIXMEUnimplemented() {
+    await assertIsStr(strMessage);
+
     await implEiteFIXMEUnimplemented();
 }
 
