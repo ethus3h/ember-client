ddfc33583999c3d51ce79c39ef11606abfb2a681
ethus3h <kolubat@gmail.com>
2018-06-14 00:39:28 -0400

    Update Git repository using egup-single

--- web/portable/eite.js
+++ web/portable/eite.js
@@ -13,37 +13,6 @@
 // TODO: DcData and renderTraits shouldn't be used here, since they're JS-specific, complex objects. They should be provided by APIs and/or simple data types instead. This will also allow the "dc" ad-hoc data type to actually be an int, instead of a string containing an int (which is kind of stupid ­— it should just be an int).
 // TODO: Function parameters and return values should be type-checked to ensure their validity. Similarly, the string types that correspond to a set of possible values (format names, encoding names, etc.) should be checked against the set (this could also be reflected in more specific/meaningful identifier prefixes).
 
-async function dcarrParseSems(bytearrayContent) {
-    // Accepts an array of bytes of a SEMS format document. Returns an array of Dcs.
-    let dcarrParseResults = [];
-    let strParserState = "dc";
-    let strCurrentDc = "";
-    let intContentLength = await intBytearrayLength(bytearrayContent);
-    for (let intByteOffset = 0; intByteOffset < await intBytearrayLength(bytearrayContent); intByteOffset++) {
-        // do something with each byte in the array. bytearrayContent[intByteOffset] holds the decimal value of the given byte. These are Dcs encoded as ASCII text bytes, rather than an array of Dcs.
-        switch (strParserState) {
-            case "dc":
-                if (await boolAsciiIsDigit(bytearrayContent[intByteOffset])) {
-                    strCurrentDc = strCurrentDc + await strFromByte(bytearrayContent[intByteOffset]);
-                }
-                if (await boolAsciiIsSpace(bytearrayContent[intByteOffset])) {
-                    dcarrParseResults.push(strCurrentDc);
-                    strCurrentDc = "";
-                }
-                if (bytearrayContent[intByteOffset] == 35) { // pound sign: start comment
-                    strParserState = "comment";
-                }
-                break;
-            case "comment":
-                if (await boolAsciiIsNewline(bytearrayContent[intByteOffset])) {
-                    strParserState = "dc";
-                }
-                break;
-        }
-    }
-    return dcarrParseResults;
-}
-
 async function dcarrConvertDocument(dcarrInput, strTargetFormat, renderTraits) {
     dcarrOutput=[];
     // Build render output buffer for specified format
--- web/portable/parse-dc-documents.js
+++ web/portable/parse-dc-documents.js
@@ -16,4 +16,35 @@ async function dcarrParseDocument(strFormat, bytearrayContent) {
     await assertIsDcarr(dcarrReturn); return dcarrReturn;
 }
 
+async function dcarrParseSems(bytearrayContent) {
+    // Accepts an array of bytes of a SEMS format document. Returns an array of Dcs.
+    let dcarrParseResults = [];
+    let strParserState = "dc";
+    let strCurrentDc = "";
+    let intContentLength = await intBytearrayLength(bytearrayContent);
+    for (let intByteOffset = 0; intByteOffset < await intBytearrayLength(bytearrayContent); intByteOffset++) {
+        // do something with each byte in the array. bytearrayContent[intByteOffset] holds the decimal value of the given byte. These are Dcs encoded as ASCII text bytes, rather than an array of Dcs.
+        switch (strParserState) {
+            case "dc":
+                if (await boolAsciiIsDigit(bytearrayContent[intByteOffset])) {
+                    strCurrentDc = strCurrentDc + await strFromByte(bytearrayContent[intByteOffset]);
+                }
+                if (await boolAsciiIsSpace(bytearrayContent[intByteOffset])) {
+                    dcarrParseResults.push(strCurrentDc);
+                    strCurrentDc = "";
+                }
+                if (bytearrayContent[intByteOffset] == 35) { // pound sign: start comment
+                    strParserState = "comment";
+                }
+                break;
+            case "comment":
+                if (await boolAsciiIsNewline(bytearrayContent[intByteOffset])) {
+                    strParserState = "dc";
+                }
+                break;
+        }
+    }
+    return dcarrParseResults;
+}
+
 // @license-end
