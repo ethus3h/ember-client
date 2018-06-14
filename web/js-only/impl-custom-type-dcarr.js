b23101dcf8213b1960e9699769b9f02ff93dfe70
ethus3h <kolubat@gmail.com>
2018-06-14 00:54:08 -0400

    Update Git repository using egup-single

--- web/js-only/impl-custom-type-dcarr.js
+++ web/js-only/impl-custom-type-dcarr.js
@@ -10,12 +10,16 @@ async function implNewDcarr() {
     let dcarrReturn;
 
     Dcarrs[Dcarrs.length] = [];
+    console.log(Dcarrs);console.log(Dcarrs.length);
     dcarrReturn = Dcarrs.length; await assertIsDcarr(dcarrReturn); return dcarrReturn;
 }
 
 async function implCustomTypeDcarrPush(dcarr, dcToAdd) {
     await assertIsDcarr(dcarr); await assertIsDc(dcToAdd);
 
+    console.log(Dcarrs);
+    console.log(dcarr);
+    console.log(Dcarrs[1]);
     Dcarrs[dcarr].push(dcToAdd);
 }
 
