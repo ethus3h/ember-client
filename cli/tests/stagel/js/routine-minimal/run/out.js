2b62c513dbd50cccdd1bec0c9ee2a3c19db980e9
ethus3h <kolubat@gmail.com>
2018-10-31 18:00:10 -0400

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-minimal/run/out.js
+++ cli/tests/stagel/js/routine-minimal/run/out.js
@@ -1,4 +1,4 @@
-async function fbSwitcher(declare -a arr=([0]="ident-n")
+async function fbSwitcher(declare -a arr=([0]="ident-n,")
 )
 /*  Test comment */await (declare -a arr=([0]="literal-s,")
 )
