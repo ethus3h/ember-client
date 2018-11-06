086ed733875033fc1fc92de41fb1583e25ca3377
ethus3h <kolubat@gmail.com>
2018-11-05 21:06:54 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/run/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/run/out.js
@@ -8,3 +8,26 @@ await eq(declare -a arr=([0]="literal-n" [1]="")
 )
 await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\017')
 )
+    /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
+
+    rootReturn = 'FizzBuzz'; await assertIsRoot(rootReturn); return rootReturn;
+/* Or it could be written using brackets like this. Also the "test" and "do" commands are optional since if knows what it's about. */
+await if(declare -a arr=()
+)
+await eq(declare -a arr=([0]="literal-n" [1]="")
+)
+await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\005')
+)
+    /* This isn't very fun to read, though. */
+    /* Or, the brackets can be omitted since the number of things needed can be used to infer. The / between type and identifier maybe can also be omitted (have types be reserved words). */
+
+    rootReturn = 'Buzz'; await assertIsRoot(rootReturn); return rootReturn;
+await if(declare -a arr=()
+)
+await eq(declare -a arr=([0]="literal-n" [1]="")
+)
+await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\003')
+)
+    /* Even less fun to read. */
+
+    rootReturn = 'Fizz'; await assertIsRoot(rootReturn); return rootReturn;
