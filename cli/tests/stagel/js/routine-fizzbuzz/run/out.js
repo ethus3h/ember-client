1de8a379040b384117d26d7e70a95c88f00d827d
ethus3h <kolubat@gmail.com>
2018-11-06 11:11:50 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/run/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/run/out.js
@@ -3,32 +3,20 @@ async function fbSwitcher(intNum) {
 
     await if(declare -a arr=()
 )
-    await eq(declare -a arr=([0]="literal-n" [1]="")
-)
-    await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\017')
-)
-        /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
+await implEq(await implMod(        /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
 
         strReturn = 'FizzBuzz'; await assertIsStr(strReturn); return strReturn;
     /* Comment */
     await if(declare -a arr=()
 )
-    await eq(declare -a arr=([0]="literal-n" [1]="")
-)
-    await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\005')
-)
-        /* Comment 2 */
+await implEq(await implMod(        /* Comment 2 */
         /* Comment 3 */
 
         strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
     /* Could also write it like this, with the condition on another line */
     await if(declare -a arr=()
 )
-        await eq(declare -a arr=([0]="literal-n" [1]="")
-)
-        await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\003')
-)
-        /* Comment 4 */
+await implEq(await implMod(        /* Comment 4 */
 
         strReturn = 'Fizz'; await assertIsStr(strReturn); return strReturn;
 }
@@ -42,11 +30,7 @@ async function fbSwitcher(intNum) {
 )
     await until(declare -a arr=()
 )
-        await eq(declare -a arr=([0]="ident-n" [1]="counter" [2]="literal-n" [3]="d")
+await implEq(        await set(declare -a arr=([0]="ident-n" [1]="counter")
 )
-        await set(declare -a arr=([0]="ident-n" [1]="counter")
-)
-        await add(declare -a arr=([0]="ident-n" [1]="counter" [2]="literal-n" [3]=$'\001')
-)
-
+await implAdd(
         rootReturn = ; await assertIsRoot(rootReturn); return rootReturn;
