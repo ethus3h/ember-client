57363081af71a23ca4d0ff09d0a6186159f23a01
ethus3h <kolubat@gmail.com>
2018-11-05 21:53:01 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/run/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/run/out.js
@@ -10,29 +10,30 @@ async function fbSwitcher(intNum) {
         /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
 
         strReturn = 'FizzBuzz'; await assertIsStr(strReturn); return strReturn;
-    /* Or it could be written using brackets like this. Also the "test" and "do" commands are optional since if knows what it's about. */
+    /* Comment */
     await if(declare -a arr=()
 )
     await eq(declare -a arr=([0]="literal-n" [1]="")
 )
     await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\005')
 )
-        /* This isn't very fun to read, though. */
-        /* Or, the brackets can be omitted since the number of things needed can be used to infer. The / between type and identifier maybe can also be omitted (have types be reserved words). */
+        /* Comment 2 */
+        /* Comment 3 */
 
         strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
+    /* Could also write it like this, with the condition on another line */
     await if(declare -a arr=()
 )
-    await eq(declare -a arr=([0]="literal-n" [1]="")
+        await eq(declare -a arr=([0]="literal-n" [1]="")
 )
-    await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\003')
+        await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\003')
 )
-        /* Even less fun to read. */
+        /* Comment 4 */
 
         strReturn = 'Fizz'; await assertIsStr(strReturn); return strReturn;
 }
 /* A string is an array of 8-bit bytes. */
-    await output-assert(declare -a arr=()
+    await return-assert(declare -a arr=()
 )
     /* the output should be non-empty. Unless otherwise specified, would take and yield nothing. */
     await -n(declare -a arr=()
@@ -41,16 +42,7 @@ async function fbSwitcher(intNum) {
 )
     await until(declare -a arr=()
 )
-        await test(declare -a arr=()
+        await eq(declare -a arr=([0]="ident-n" [1]="counter" [2]="literal-n" [3]="d")
 )
-            await eq(declare -a arr=([0]="ident-n" [1]="counter" [2]="literal-n" [3]="d")
-)
-        await do(declare -a arr=()
-)
-            /* out is like return, but doesn't exit and more data can continue to be output */
-            await out(declare -a arr=()
-)
-                await run(declare -a arr=()
-)
-                await fbSwitcher(declare -a arr=([0]="ident-n" [1]="counter")
+        await run(declare -a arr=([0]="ident-r-s" [1]="fbSwitcher" [2]="ident-n" [3]="counter")
 )
