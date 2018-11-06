caf4bf872f83200355f5a2e5b6cec80c297a8cb9
ethus3h <kolubat@gmail.com>
2018-11-05 20:56:12 -0500

    [egup] Update repository metadata

--- cli/tests/stagel/js/routine-fizzbuzz/run/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/run/out.js
@@ -7,3 +7,14 @@ async function fbSwitcher(intNum) {
 )
     await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\017')
 )
+        /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
+
+        strReturn = 'FizzBuzz'; await assertIsStr(strReturn); return strReturn;
+    /* Or it could be written using brackets like this. Also the "test" and "do" commands are optional since if knows what it's about. */
+    await if(declare -a arr=()
+)
+    await eq(declare -a arr=([0]="literal-n" [1]="")
+)
+    await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\005')
+)
+        /* This isn't very fun to read, though. */
