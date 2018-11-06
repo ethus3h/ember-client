5474e111d26d18b7bec896197da6039047b65ec3
ethus3h <kolubat@gmail.com>
2018-11-05 22:04:25 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/out/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/out/out.js
@@ -1,21 +1,33 @@
-r/s/fbSwitcher n/num
-    if
-        test
-            eq 0
-                mod n/num 15
-        do
-            return "FizzBuzz" # just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL.
-    # Or it could be written using brackets like this. Also the "test" and "do" commands are optional since if knows what it's about.
-    if ( eq 0 ( mod n/num 5 ) ) return "Buzz" # This isn't very fun to read, though.
-    # Or, the brackets can be omitted since the number of things needed can be used to infer. The / between type and identifier maybe can also be omitted (have types be reserved words).
-    if eq 0 mod n num 3 return "Fizz" # Even less fun to read.
+async function fbSwitcher(intNum) {
+    await assertIsInt(intNum); let strReturn;
 
-r s fizzBuzz # A string is an array of 8-bit bytes.
-    output-assert -n # the output should be non-empty. Unless otherwise specified, would take and yield nothing.
-    new n/counter 1
-    until
-        test
-            eq n/counter 100
-        do
-            out # out is like return, but doesn't exit and more data can continue to be output
-                run fbSwitcher n/counter
+    if (implEq(0, implMod(intNum, 15))) {
+        /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
+
+        strReturn = 'FizzBuzz'; await assertIsStr(strReturn); return strReturn;
+    }
+    /* Comment */
+    if (implEq(0, implMod(intNum, 5))) {
+        /* Comment 2 */
+
+        strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
+        /* Comment 3 */
+    /* Could also write it like this, with the condition on another line */
+    if (implEq(0, implMod(intNum, 3))) {
+        /* Comment 4 */
+
+        strReturn = 'Fizz'; await assertIsStr(strReturn); return strReturn;
+    }
+}
+
+/* A string is an array of 8-bit bytes. */
+
+async function fizzBuzz() {
+    let strReturn;
+    /* the output should be non-empty. */
+    return-assert -n
+    let intCounter = 1;
+    while (! implEq(intCounter, 100)) {
+        eq n/counter 100
+        run r/s/fbSwitcher n/counter
+}
