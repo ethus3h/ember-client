762d851111e185cec15b76937966fc1149f8442b
ethus3h <kolubat@gmail.com>
2018-11-05 21:07:08 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/run/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/run/out.js
@@ -1,33 +1,30 @@
-async function fbSwitcher(intNum) {
-    await assertIsInt(intNum); let strReturn;
-
-}
-await if(declare -a arr=()
+    await if(declare -a arr=([0]="ident-n" [1]="num")
 )
-await eq(declare -a arr=([0]="literal-n" [1]="")
+    await eq(declare -a arr=([0]="literal-n" [1]="")
 )
-await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\017')
+        /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
+    /* Or it could be written using brackets like this. Also the "test" and "do" commands are optional since if knows what it's about. */
+    await if(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\017' [4]="literal-s" [5]="FizzBuzz")
 )
-    /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
-
-    rootReturn = 'FizzBuzz'; await assertIsRoot(rootReturn); return rootReturn;
-/* Or it could be written using brackets like this. Also the "test" and "do" commands are optional since if knows what it's about. */
-await if(declare -a arr=()
+    await eq(declare -a arr=([0]="literal-n" [1]="")
 )
-await eq(declare -a arr=([0]="literal-n" [1]="")
+        /* This isn't very fun to read, though. */
+        /* Or, the brackets can be omitted since the number of things needed can be used to infer. The / between type and identifier maybe can also be omitted (have types be reserved words). */
+    await if(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\005' [4]="literal-s" [5]="Buzz")
 )
-await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\005')
+    await eq(declare -a arr=([0]="literal-n" [1]="")
 )
-    /* This isn't very fun to read, though. */
-    /* Or, the brackets can be omitted since the number of things needed can be used to infer. The / between type and identifier maybe can also be omitted (have types be reserved words). */
-
-    rootReturn = 'Buzz'; await assertIsRoot(rootReturn); return rootReturn;
-await if(declare -a arr=()
+        /* Even less fun to read. */
+/* A string is an array of 8-bit bytes. */
+    await output-assert(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\003' [4]="literal-s" [5]="Fizz" [6]="ident-r-s" [7]="fizzBuzz")
 )
-await eq(declare -a arr=([0]="literal-n" [1]="")
+    /* the output should be non-empty. Unless otherwise specified, would take and yield nothing. */
+    await -n(declare -a arr=()
 )
-await mod(declare -a arr=([0]="ident-n" [1]="num" [2]="literal-n" [3]=$'\003')
+    await new(declare -a arr=([0]="ident-n" [1]="counter" [2]="literal-n" [3]=$'\001')
 )
-    /* Even less fun to read. */
-
-    rootReturn = 'Fizz'; await assertIsRoot(rootReturn); return rootReturn;
+            /* out is like return, but doesn't exit and more data can continue to be output */
+                await run(declare -a arr=([0]="ident-n" [1]="counter" [2]="literal-n" [3]="d")
+)
+await fbSwitcher(declare -a arr=([0]="ident-n" [1]="counter")
+)
