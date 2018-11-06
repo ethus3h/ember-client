f7b5629d9a8a9470deace9cce40f421474a9dda3
ethus3h <kolubat@gmail.com>
2018-11-06 11:11:39 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/out/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/out/out.js
@@ -1,19 +1,19 @@
 async function fbSwitcher(intNum) {
     await assertIsInt(intNum); let strReturn;
 
-    if (implEq(0, implMod(intNum, 15))) {
+    if (await implEq(0, await implMod(intNum, 15))) {
         /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
 
         strReturn = 'FizzBuzz'; await assertIsStr(strReturn); return strReturn;
     }
     /* Comment */
-    if (implEq(0, implMod(intNum, 5))) {
+    if (await implEq(0, await implMod(intNum, 5))) {
         /* Comment 2 */
 
         strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
         /* Comment 3 */
     /* Could also write it like this, with the condition on another line */
-    if (implEq(0, implMod(intNum, 3))) {
+    if (await implEq(0, await implMod(intNum, 3))) {
         /* Comment 4 */
 
         strReturn = 'Fizz'; await assertIsStr(strReturn); return strReturn;
@@ -27,8 +27,8 @@ async function fizzBuzz() {
     /* the output should be non-empty. */
     return-assert -n
     let intCounter = 1;
-    while (! implEq(intCounter, 100)) {
-        intCounter = implAdd(intCounter, 1);
+    while (! await implEq(intCounter, 100)) {
+        intCounter = await implAdd(intCounter, 1);
         await fbSwitcher(intCounter);
     }
 }
