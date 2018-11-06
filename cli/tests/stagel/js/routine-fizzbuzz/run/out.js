8aecf51ac6eec8df78f4fd0974dd4e399e6f3b9f
ethus3h <kolubat@gmail.com>
2018-11-06 11:23:08 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/run/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/run/out.js
@@ -1,36 +1,48 @@
 async function fbSwitcher(intNum) {
     await assertIsInt(intNum); let strReturn;
 
-    await if(declare -a arr=()
+await implMod(    await (declare -a arr=()
 )
-await implEq(await implMod(        /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
+    await (declare -a arr=()
+)
+        /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
 
         strReturn = 'FizzBuzz'; await assertIsStr(strReturn); return strReturn;
     /* Comment */
-    await if(declare -a arr=()
+await implMod(    await (declare -a arr=()
 )
-await implEq(await implMod(        /* Comment 2 */
+    await (declare -a arr=()
+)
+        /* Comment 2 */
         /* Comment 3 */
 
         strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
     /* Could also write it like this, with the condition on another line */
     await if(declare -a arr=()
 )
-await implEq(await implMod(        /* Comment 4 */
+        /* Comment 4 */
 
         strReturn = 'Fizz'; await assertIsStr(strReturn); return strReturn;
+        await (declare -a arr=()
+)
+        await (declare -a arr=()
+)
 }
 /* A string is an array of 8-bit bytes. */
-    await return-assert(declare -a arr=()
-)
     /* the output should be non-empty. */
-    await -n(declare -a arr=()
+    await until(declare -a arr=([0]="ident-n" [1]="counter" [2]="literal-n" [3]=$'\001')
 )
-    await new(declare -a arr=([0]="ident-n" [1]="counter" [2]="literal-n" [3]=$'\001')
+    await (declare -a arr=()
 )
-    await until(declare -a arr=()
+    await (declare -a arr=()
 )
-await implEq(        await set(declare -a arr=([0]="ident-n" [1]="counter")
+    await (declare -a arr=()
 )
-await implAdd(
+
         rootReturn = ; await assertIsRoot(rootReturn); return rootReturn;
+        await (declare -a arr=()
+)
+        await (declare -a arr=()
+)
+        await (declare -a arr=()
+)
