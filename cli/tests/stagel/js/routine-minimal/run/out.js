371a50d401d4e603fd10786b26e871be6845e5ce
ethus3h <kolubat@gmail.com>
2018-11-05 21:44:39 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-minimal/run/out.js
+++ cli/tests/stagel/js/routine-minimal/run/out.js
@@ -1,7 +1,7 @@
 async function fbSwitcher(intNum) {
     await assertIsInt(intNum); let strReturn;
 
-/* Test comment */
+    /* Test comment */
 
-strReturn = ''; await assertIsStr(strReturn); return strReturn;
+    strReturn = ''; await assertIsStr(strReturn); return strReturn;
 }
