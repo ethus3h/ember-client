1dc94e02049bb4284c67fa318589a3d7dbd9ea8e
ethus3h <kolubat@gmail.com>
2018-11-08 11:39:07 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/if-statement/run/out.js
+++ cli/tests/stagel/js/if-statement/run/out.js
@@ -1,6 +1,4 @@
 if (await implEq(0, await implMod(intNum, 15))) {
-let ,     await s/n(    );
-    );
-    await set(,     await s/n('FizzBuzz'    );
-    );
+    let strN    );
+    await set(strN, 'FizzBuzz'    );
 }
