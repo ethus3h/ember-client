b674d0e10bd38e9ab9012f05ed9fc35ec8ae4c3b
ethus3h <kolubat@gmail.com>
2018-11-08 10:24:28 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/run/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/run/out.js
@@ -1,23 +1,20 @@
 async function fbSwitcher(intNum) {
     await assertIsInt(intNum); let strReturn;
 
-    if (    await implMod(await assertIsLiteral-n(literal-n);  {
-    )    ) {
+    if (    await implMod(await assertIsLiteral-n(literal-n);     )    ) {
         /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
 
         strReturn = 'FizzBuzz'; await assertIsStr(strReturn); return strReturn;
     }
     /* Comment */
-    if (    await implMod(await assertIsLiteral-n(literal-n);  {
-    )    ) {
+    if (    await implMod(await assertIsLiteral-n(literal-n);     )    ) {
         /* Comment 2 */
 
         strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
         /* Comment 3 */
     }
     if (    /* Could also write it like this, with the condition on another line */
-        await implMod(await assertIsLiteral-n(literal-n);  {
-        )        /* Comment 4 */
+        await implMod(await assertIsLiteral-n(literal-n);         )        /* Comment 4 */
 
         strReturn = 'Fizz'; await assertIsStr(strReturn); return strReturn;
     }
@@ -26,15 +23,8 @@ async function fizzBuzz() {
     let strReturn;
 
     /* the output should be non-empty. */
-    ) {
-    ) {
-    ) {
-    ) {
- {
-await implEq( {
-await implAdd( {
-        ) {
-
+    )    )    )    ) {
+await implEq(await implAdd(        )
         strReturn = ; await assertIsStr(strReturn); return strReturn;
     }
 }
