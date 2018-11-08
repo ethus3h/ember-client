225daa6875493da8da17e4ee1ee1a6c6ca474f3a
ethus3h <kolubat@gmail.com>
2018-11-08 10:14:49 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/run/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/run/out.js
@@ -1,18 +1,4 @@
 async function fbSwitcher(intNum) {
     await assertIsInt(intNum); let strReturn;
 
-    if (await implMod(await assertIsLiteral-n(literal-n);     )    ) {
-        /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
-
-        strReturn = 'FizzBuzz'; await assertIsStr(strReturn); return strReturn;
-    }
-    /* Comment */
-    if (await implMod(await assertIsLiteral-n(literal-n);     )    ) {
-        /* Comment 2 */
-
-        strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
-        /* Comment 3 */
-    }
-    if (    /* Could also write it like this, with the condition on another line */
-    ) {
-        /* Comment 4 */
+    if (await implMod(await assertIsLiteral-n(literal-n);         /* just a literal sequence of bytes. Everything should be binary safe. 0x could be used to return data, too. 0x = data, not numbers, in StageL. */
