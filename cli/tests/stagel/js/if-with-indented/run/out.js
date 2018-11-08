b1dff8d839be5de7e51ad53968a2417ebee9b84b
ethus3h <kolubat@gmail.com>
2018-11-08 14:18:04 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/if-with-indented/run/out.js
+++ cli/tests/stagel/js/if-with-indented/run/out.js
@@ -1,3 +1,5 @@
-if (, await implEq(    0, await implMod(intNum, 15)),     let ;
-    strN),     =    strN, 'FizzBuzz')) {
+if (await implEq(0, await implMod(intNum, 15);
+);
+    let strN;
+    strN='FizzBuzz';
 }
