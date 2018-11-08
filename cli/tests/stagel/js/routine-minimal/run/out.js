225e760b6d0aa9aa8a33dc79ee44c4839006e703
ethus3h <kolubat@gmail.com>
2018-11-08 10:15:19 -0500

    [egup] Update repository metadata

---
+++ cli/tests/stagel/js/routine-minimal/run/out.js
@@ -0,0 +1,7 @@
+async function fbSwitcher(intNum) {
+    await assertIsInt(intNum); let strReturn;
+
+    /* Test comment; semicolon */
+
+    strReturn = ''; await assertIsStr(strReturn); return strReturn;
+}
