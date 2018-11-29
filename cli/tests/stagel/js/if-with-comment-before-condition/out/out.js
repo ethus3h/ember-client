367be0be092ca84b989ed760713169e76641f800
ethus3h <kolubat@gmail.com>
2018-11-29 14:56:34 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/if-with-comment-before-condition/out/out.js
+++ cli/tests/stagel/js/if-with-comment-before-condition/out/out.js
@@ -1,7 +1,5 @@
-
-    /* Could also write it like this, with the condition on another line */
-    if (await implEq(0, await implMod(intNum, 3))) {
-        /* Comment 4 */
+/* Could also write it like this, with the condition on another line */
+if (await implEq(0, await implMod(intNum, 3))) {
 
         strReturn = 'Fizz'; await assertIsStr(strReturn); return strReturn;
     }
