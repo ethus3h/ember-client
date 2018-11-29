a9071a7def0d86be915fd1328964074f1853f477
ethus3h <kolubat@gmail.com>
2018-11-29 14:27:41 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fbSwitcher-return-type/run/out.js
+++ cli/tests/stagel/js/routine-fbSwitcher-return-type/run/out.js
@@ -1,16 +1,8 @@
 async function fbSwitcher(intNum) {
     await assertIsInt(intNum); let strReturn;
 
-    /* Comment 2 */
+    if (await implEq(0, await implMod(intNum, 15))) {
 
-    strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
- {
-        /* Comment 3 */
+        testReturn = 'Buzz'; await assertIsTest(testReturn); return testReturn;
     }
-    if (    /* Could also write it like this, with the condition on another line */
-
-        await implEq(0, await implMod(intNum, 3))) {
-        /* Comment 4 */
-
-        testReturn = 'Fizz'; await assertIsTest(testReturn); return testReturn;
-    }
+}
