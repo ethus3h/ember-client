b9b70490b07b9d1caf1663dde3eef6903b7170fc
ethus3h <kolubat@gmail.com>
2018-11-29 15:02:40 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/if-with-comment-before-condition/run/out.js
+++ cli/tests/stagel/js/if-with-comment-before-condition/run/out.js
@@ -1,7 +1,9 @@
-if (/* comment */
+ {
+    if (    /* comment */
 
-    await implEq(0, await implMod(intNum, 3))) {
+        await implEq(0, await implMod(intNum, 3))) {
 await implEq(0, 0);
  {
+        }
     }
 }
