4f4ad10935e777f712ec3eec1d3d8f0ad9fd7bfc
ethus3h <kolubat@gmail.com>
2018-12-04 05:54:17 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/no-codegen/routine-with-if/run/out.js
+++
@@ -1,9 +0,0 @@
-async function fbSwitcher(intNum) {
-    await assertIsInt(intNum); let strReturn;
-
-    if (await implEq(0, 0)) {
-
-        strReturn = 'a'; await assertIsStr(strReturn); return strReturn;
-    }
-    }
-/* */
