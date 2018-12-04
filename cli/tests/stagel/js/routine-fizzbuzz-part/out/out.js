7739e0bd2c1269b70d981a4f86c303c6bd9193fc
ethus3h <kolubat@gmail.com>
2018-12-04 05:36:37 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz-part/out/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz-part/out/out.js
@@ -12,21 +12,23 @@ async function fbSwitcher(intNum) {
 
         strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
         /* Comment 3 */
-    }
-    if (/* Could also write it like this, with the condition on another line */
-
-        await implEq(0, await implMod(intNum, 3))) {
+    /* Could also write it like this, with the condition on another line */
+    if (await implEq(0, await implMod(intNum, 3))) {
         /* Comment 4 */
 
         strReturn = 'Fizz'; await assertIsStr(strReturn); return strReturn;
     }
-    }
+}
 
 /* A string is an array of 8-bit bytes. */
+
 async function fizzBuzz() {
     let strReturn;
 
     /* return-assert -n # the output should be non-empty. */
-    let int1;
-    while !() {
-        await implEq(intCounter, 100, 
+    let intCounter = 1;
+    while (! await implEq(intCounter, 100)) {
+        intCounter = await implAdd(intCounter, 1);
+        await fbSwitcher(intCounter);
+    }
+}
