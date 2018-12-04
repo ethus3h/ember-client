644d2b2b08090d12c280e3973992202d66bcdb44
ethus3h <kolubat@gmail.com>
2018-12-04 12:51:22 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/set-number-add/run/out.js
+++ cli/tests/stagel/js/set-number-add/run/out.js
@@ -1,9 +1,6 @@
-/* A string is an array of 8-bit bytes. */
-async function fizzBuzz() {
+async function f() {
     let strReturn;
 
-    /* return-assert -n # the output should be non-empty. */
     let int1;
-    while !(
-        await implEq(intCounter, 100)) {
-        intCounter = await implAdd(intCounter, 1, let 
+    intCounter = await implAdd(intCounter, 1);
+}
