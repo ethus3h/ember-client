16d1b72c41012e7c56e48ca0cc738e1e225362ae
ethus3h <kolubat@gmail.com>
2018-12-05 05:11:10 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/set-number-add-no-routine/run/out.js
+++ cli/tests/stagel/js/set-number-add-no-routine/run/out.js
@@ -1,5 +1,2 @@
-async function f() {
-    let strReturn;
-
-    let intCounter = 1;
-    intCounter = await implAdd(intCounter, 1, let 
+intCounter = await implAdd(intCounter, 1);
+let strTemp = '';
