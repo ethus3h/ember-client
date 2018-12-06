aa51dddc1d37c8f2e91c032da56a2a5c2240b956
ethus3h <kolubat@gmail.com>
2018-12-06 14:00:23 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/js/implementation/math.js
+++ cli/stagel-resources/js/implementation/math.js
@@ -1,11 +1,11 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* Provides:
-    implIntBytearrayLength
-    implIntDcarrLength
-    implStrSubstring
+    implAdd
+    implSub
+    implMod
 */
 
-async function implIntBytearrayLength(bytearray) {
+async function implAdd(num) {
     assertIsBytearray(bytearray); let intReturn;
 
     intReturn = bytearray.byteLength; await assertIsInt(intReturn); return intReturn;
