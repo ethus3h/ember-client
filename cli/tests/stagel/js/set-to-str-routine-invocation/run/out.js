cddccb568f3a3af710ebc2bc3cad3c5f68883047
ethus3h <kolubat@gmail.com>
2018-12-06 13:14:17 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/set-to-str-routine-invocation/run/out.js
+++ cli/tests/stagel/js/set-to-str-routine-invocation/run/out.js
@@ -1,1 +1,1 @@
-strTemp = strFbSwitcher, intCounter;
+strTemp = await  102 98 83 119 105 116 99 104 101 114(intCounter);
--- cli/tests/stagel/js/set-to-str-routine-with-new/run/out.js
+++ cli/tests/stagel/js/set-to-str-routine-with-new/run/out.js
@@ -1,2 +1,2 @@
-strTemp = strFbSwitcher, intCounter;
+strTemp = await  102 98 83 119 105 116 99 104 101 114(intCounter);
 let strTick = '';
