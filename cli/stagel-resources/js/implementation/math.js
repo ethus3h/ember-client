3d554f65b8f1c6cf0d94b060f464e2f408ed93ce
ethus3h <kolubat@gmail.com>
2018-12-06 13:58:08 -0500

    Update Git repository using egup-single

---
+++ cli/stagel-resources/js/implementation/math.js
@@ -0,0 +1,26 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+/* Provides:
+    implIntBytearrayLength
+    implIntDcarrLength
+    implStrSubstring
+*/
+
+async function implIntBytearrayLength(bytearray) {
+    assertIsBytearray(bytearray); let intReturn;
+
+    intReturn = bytearray.byteLength; await assertIsInt(intReturn); return intReturn;
+}
+
+async function implIntDcarrLength(dcarr) {
+    assertIsDcarr(dcarr); let intReturn;
+
+    intReturn = Dcarrs[await dcarr].length; await assertIsInt(intReturn); return intReturn;
+}
+
+async function implStrSubstring(str, intStart, intLength) {
+    assertIsStr(str); assertIsInt(intStart); assertIsInt(intLength); let strReturn;
+
+    strReturn = str.substring(intStart, intStart + intLength); await assertIsStr(strReturn); return strReturn;
+}
+
+// @license-end
