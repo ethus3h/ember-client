a1f55ef5fb3ee92d9c5309189b302fd9b1d2bd5a
ethus3h <kolubat@gmail.com>
2018-12-06 13:25:23 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/out/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/out/out.js
@@ -12,8 +12,10 @@ async function fbSwitcher(intNum) {
 
         strReturn = 'Buzz'; await assertIsStr(strReturn); return strReturn;
         /* Comment 3 */
-    /* Could also write it like this, with the condition on another line */
-    if (await implEq(0, await implMod(intNum, 3))) {
+    }
+    if (/* Could also write it like this, with the condition on another line */
+
+        await implEq(0, await implMod(intNum, 3))) {
         /* Comment 4 */
 
         strReturn = 'Fizz'; await assertIsStr(strReturn); return strReturn;
