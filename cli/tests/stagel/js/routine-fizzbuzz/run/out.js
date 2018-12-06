d0e359e04f399d2eb88e0352196bdf1cc51e297e
ethus3h <kolubat@gmail.com>
2018-12-06 13:18:38 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/routine-fizzbuzz/run/out.js
+++ cli/tests/stagel/js/routine-fizzbuzz/run/out.js
@@ -27,12 +27,13 @@ async function fizzBuzz() {
     let strReturn;
 
     /* return-assert -n # the output should be non-empty. */
-    let intCounter = 1, while !(await implEq(intCounter, 100));
+    let intCounter = 1;
+    while !(
+        await implEq(intCounter, 100)) {
         intCounter = await implAdd(intCounter, 1);
         let strTemp = '';
         strTemp = await fbSwitcher(intCounter);
 
         strReturn = ; await assertIsStr(strReturn); return strReturn;
     }
-}
 }
