a8898a079045f8319a9be2c31a099a6f40b5cc93
ethus3h <kolubat@gmail.com>
2018-12-09 15:43:55 -0500

    [egup] Update repository metadata

--- cli/tests/stagel/js/logical-or/run/out.js
+++ cli/tests/stagel/js/logical-or/run/out.js
@@ -2,8 +2,3 @@ async function or(boolA, boolB) {
     await assertIsBool(boolA); await assertIsBool(boolB); let boolReturn;
 
     let boolTemp;
-    boolTemp = await not(boolA);
-    boolTemp = await not(await and(boolTemp, await not(boolB)));
-
-    boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
-}
