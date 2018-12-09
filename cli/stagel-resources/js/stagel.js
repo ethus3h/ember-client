74e4562617f68458a1fa49199acc02ef9e356434
ethus3h <kolubat@gmail.com>
2018-12-09 18:54:18 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/js/stagel.js
+++ cli/stagel-resources/js/stagel.js
@@ -1,4 +1,5 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* Provides:
     implDie
@@ -101,6 +102,22 @@ async function implStrSubstring(str, intStart, intLength) {
 }
 
 // @license-end
+/* booleans.js, provides:
+    implAnd
+    implNot
+*/
+
+async function implAnd(boolA, boolB) {
+    assertIsBool(boolA); assertIsBool(boolB); let boolReturn;
+
+    boolReturn = boolA && boolB; await assertIsBool(boolReturn); return boolReturn;
+}
+
+async function implNot(boolA) {
+    assertIsBool(boolA); let boolReturn;
+
+    boolReturn = !boolA; await assertIsBool(boolReturn); return boolReturn;
+}
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* Provides:
     implAssertIsBool
@@ -171,26 +188,37 @@ async function implAssertStrContainsOnlyInt(str) {
 }
 
 // @license-end
-// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
-/* Provides:
-    implAnd
-    implNot
+/* comparison.js, provides:
+    implEq
+    implGt
+    implLt
+*/
+
+async function implEq(a, b) {
+    /* Can compare booleans, ints, or strings. */
+    let boolReturn;
+
+    boolReturn = a == b; await assertIsBool(boolReturn); return boolReturn;
+}
+
+async function implGt(a, b) {
+    /* Can compare booleans or ints */
+    let boolReturn;
+
+    boolReturn = a > b; await assertIsBool(boolReturn); return boolReturn;
+}
+
+async function implLt(a, b) {
+    /* Can compare booleans or ints */
+    let boolReturn;
+
+    boolReturn = a < b; await assertIsBool(boolReturn); return boolReturn;
+}
+/* math.js, provides:
     implAdd
     implSub
     implMod
 */
-
-async function implAnd(boolA, boolB) {
-    assertIsBool(boolA); assertIsBool(boolB); let boolReturn;
-
-    boolReturn = boolA && boolB; await assertIsBool(boolReturn); return boolReturn;
-}
-
-async function implNot(boolA) {
-    assertIsBool(boolA); let boolReturn;
-
-    boolReturn = !boolA; await assertIsBool(boolReturn); return boolReturn;
-}
 
 async function implAdd(intA, intB) {
     assertIsInt(intA); assertIsInt(intB); let intReturn;
@@ -209,8 +237,6 @@ async function implMod(intA, intB) {
 
     intReturn = intA % intB; await assertIsInt(intReturn); return intReturn;
 }
-
-// @license-end
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* Provides:
     implIntFromStr
@@ -236,158 +262,6 @@ async function implStrFromUnicodeHex(strCharacter) {
     strReturn = String.fromCharCode("0x" + strCharacter); await assertIsStr(strReturn); return strReturn;
 }
 
-// @license-end
-// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
-// This file defines functions with implementations that are specific to Web browser implementation of EITE in JavaScript.
-// This is a library file, and should only initialize functions/variables, so that it can be loaded and run in parallel with other library files, and work regardless of the order they are loaded.
-
-// Platform-specific overrides of routines available portably
-
-// Override error reporting method to show alert
-// TODO: Does this always work? Overrides aren't really possible when it's load-order-independent, I wouldn't think...
-async function eiteError(strMessage) {
-    console.trace();
-    await eiteLog("EITE reported error!: " + await implStrNormalizeMessage(strMessage));
-    alert("EITE reported error!: " + await implStrNormalizeMessage(strMessage));
-    throw "EITE reported error!: " + await implStrNormalizeMessage(strMessage);
-}
-async function eiteWarn(strMessage) {
-    console.trace();
-    await eiteLog("EITE reported warning: " + await implStrNormalizeMessage(strMessage));
-    alert("EITE reported warning: " + await implStrNormalizeMessage(strMessage));
-}
-
-// Fully platform-specific code
-
-async function implGetEnvironmentBestFormat() {
-    return "immutableCharacterCells";
-}
-
-async function implGetEnvironmentRenderTraits(targetFormat) {
-    if ( targetFormat === undefined ) {
-        await eiteError("implGetEnvironmentRenderTraits was called without any targetFormat!");
-    }
-    var traits = {};
-    switch (targetFormat) {
-        case "integerList":
-        case "immutableCharacterCells":
-            traits.cellTableWidth = -1; // unlimited
-            traits.cellTableHeight = -1; // unlimited
-            let cs = document.characterSet.toLowerCase();
-            switch(cs) {
-                case "utf-8":
-                    traits.characterEncoding = "UTF-8";
-                    break;
-                default:
-                    await eiteWarn("Unimplemented character set: " + cs + ". Falling back to ASCII-safe-subset.");
-                    traits.characterEncoding = "ASCII-safe-subset";
-                    break;
-            }
-            break;
-    }
-    return traits;
-}
-
-async function implLoadCsv(url, lineLoadedCallback, documentLoadedCallback, errorCallback) {
-    await Papa.parse(url, {
-        download: true,
-        encoding: "UTF-8",
-        newline: "\n",
-        delimiter: ",",
-        quoteChar: "\"",
-        step: async function(results, parser) {
-            await lineLoadedCallback(results, parser);
-        },
-        complete: async function(results, file) {
-            await documentLoadedCallback(results, file);
-        },
-        error: async function(results, file) {
-            await errorCallback(results, file);
-        }
-    });
-}
-
-async function implDoRenderIo(renderBuffer, targetFormat) {
-    switch (targetFormat) {
-        case "integerList":
-        case "immutableCharacterCells":
-            let immutableCharCellOutput = document.getElementById("log");
-            for (let i = 0; i < renderBuffer.length; i++) {
-                immutableCharCellOutput.innerHTML += await implStrNormalizeMessage(renderBuffer[i]) + "<br />";
-                immutableCharCellOutput.scrollTop = immutableCharCellOutput.scrollHeight;
-            }
-            break;
-        default:
-            await eiteError("Unimplemented render I/O format: " + targetFormat);
-            break;
-    }
-}
-
-async function implUrlLoadForCallback(url, callback) {
-    var oReq = new XMLHttpRequest();
-    oReq.open("GET", url, true);
-    oReq.responseType = "arraybuffer";
-    oReq.onload = async function(oEvent) {
-        callback(new Uint8Array(oReq.response)); // Note: not oReq.responseText
-    };
-    oReq.send(null);
-}
-
-async function implOperateOnDocFromUrl(strFormat, strUrl, callback) {
-    await implUrlLoadForCallback(strUrl, async function(bytearrayContent) { await callback(await dcarrParseDocument(strFormat, bytearrayContent)); });
-}
-
-async function implRunEiteTest(strTestFormat, strTestName) {
-    // TODO: Unfinished implementation
-    strTestUrlPrefix="../tests/" + strTestName + "." + strTestFormat + "/";
-    strTestInputFormatUrl="../tests/" + strTestName + "." + strTestFormat + "/in-format";
-    switch (strTestFormat) {
-        case "ept": // Parser test
-            await implUrlLoadForCallback(strTestInputFormatUrl, async function(bytearrayContent) {});
-            break;
-        default:
-            await eiteError("Unimplemented test format: " + strTestFormat);
-            break;
-    }
-}
-
-// Set up dcData
-dcData = [];
-async function implDcDataAppendDataset(dataset) {
-    dcData[dataset] = [];
-}
-async function implDcDataAppendLine(dataset, line) {
-    dcData[dataset].push(line);
-}
-async function implLoadDatasets(callback) {
-    if (datasets.length > 0) {
-        let dataset = datasets[0];
-        await implDcDataAppendDataset(dataset);
-        await implLoadCsv(
-            "../data/" + dataset + ".csv",
-            async function(results,parser){
-                await implDcDataAppendLine(dataset, results);
-            },
-            async function(){
-                datasets.shift();
-                await implLoadDatasets(callback);
-            },
-            async function(){
-                await eiteError("Error reported while parsing "+dataset+"!");
-            }
-        );
-    }
-    else {
-        await callback();
-    }
-}
-
-datasets=[
-    "DcData",
-    "mappings/from/ascii",
-    "mappings/from/unicode",
-    "mappings/to/html"
-];
 // @license-end
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* Provides:
@@ -423,4 +297,5 @@ async function implStrDcDataLookupByValue(strDataset, intFilterField, strFilterV
 }
 
 // @license-end
+
 // @license-end
--- cli/stagel-resources/stager/stager.js
+++ cli/stagel-resources/stager/stager.js
@@ -1,10 +1,11 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+
 async function or(boolA, boolB) {
     await assertIsBool(boolA); await assertIsBool(boolB); let boolReturn;
 
     let boolTemp;
-    boolTemp = await not(boolA);
-    boolTemp = await not(await and(boolTemp, await not(boolB)));
+    boolTemp = await implNot(boolA);
+    boolTemp = await implNot(await implAnd(boolTemp, await implNot(boolB)));
 
     boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
 }
@@ -13,7 +14,7 @@ async function nor(boolA, boolB) {
     await assertIsBool(boolA); await assertIsBool(boolB); let boolReturn;
 
     let boolTemp;
-    boolTemp = await not(await or(boolA, boolB));
+    boolTemp = await implNot(await or(boolA, boolB));
 
     boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
 }
@@ -22,7 +23,7 @@ async function nand(boolA, boolB) {
     await assertIsBool(boolA); await assertIsBool(boolB); let boolReturn;
 
     let boolTemp;
-    boolTemp = await not(await and(boolA, boolB));
+    boolTemp = await implNot(await implAnd(boolA, boolB));
 
     boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
 }
@@ -32,7 +33,7 @@ async function xor(boolA, boolB) {
 
     let boolTemp;
     boolTemp = await nand(boolA, boolB);
-    boolTemp = await and(boolTemp, await or(boolA, boolB));
+    boolTemp = await implAnd(boolTemp, await or(boolA, boolB));
 
     boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
 }
@@ -41,8 +42,11 @@ async function xnor(boolA, boolB) {
     await assertIsBool(boolA); await assertIsBool(boolB); let boolReturn;
 
     let boolTemp;
-    boolTemp = await not(await xor(boolA, boolB));
+    boolTemp = await implNot(await xor(boolA, boolB));
 
     boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
 }
+/* Calling a comparison with different types is an error. All types must be same type. */
+
+
 // @license-end
