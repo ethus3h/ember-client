8bb6f3d6392f2149ed68dc2f39ef15b316d165b1
ethus3h <kolubat@gmail.com>
2018-12-09 15:41:07 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/logical-or/run/out.js
+++ cli/tests/stagel/js/logical-or/run/out.js
@@ -1,1 +1,8 @@
-let intCounter = 1;
+async function or(boolA, boolB) {
+    await assertIsBool(boolA); await assertIsBool(boolB); let boolReturn;
+
+    let boolTemp;
+    boolTemp = await not(await and(await not(boolA, await not(boolB))));
+
+    boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
+}
