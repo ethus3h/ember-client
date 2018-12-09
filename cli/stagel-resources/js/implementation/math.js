d30be3891b3a6a6d5c0d821883152d3354ad00af
ethus3h <kolubat@gmail.com>
2018-12-09 15:35:36 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/js/implementation/math.js
+++ cli/stagel-resources/js/implementation/math.js
@@ -1,9 +1,23 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* Provides:
+    implAnd
+    implNot
     implAdd
     implSub
     implMod
 */
+
+async function implAnd(boolA, boolB) {
+    assertIsBool(boolA); assertIsBool(boolB); let boolReturn;
+
+    boolReturn = boolA && boolB; await assertIsBool(boolReturn); return boolReturn;
+}
+
+async function implNot(boolA) {
+    assertIsBool(boolA); let boolReturn;
+
+    boolReturn = !boolA; await assertIsBool(boolReturn); return boolReturn;
+}
 
 async function implAdd(intA, intB) {
     assertIsInt(intA); assertIsInt(intB); let intReturn;
