003a1a9ba4415a55f8eab5e926ec46c5a119a784
ethus3h <kolubat@gmail.com>
2018-12-09 18:36:48 -0500

    Update Git repository using egup-single

---
+++ cli/stagel-resources/js/implementation/booleans.js
@@ -0,0 +1,37 @@
+/* math.js, provides:
+    implAnd
+    implNot
+    implAdd
+    implSub
+    implMod
+*/
+
+async function implAnd(boolA, boolB) {
+    assertIsBool(boolA); assertIsBool(boolB); let boolReturn;
+
+    boolReturn = boolA && boolB; await assertIsBool(boolReturn); return boolReturn;
+}
+
+async function implNot(boolA) {
+    assertIsBool(boolA); let boolReturn;
+
+    boolReturn = !boolA; await assertIsBool(boolReturn); return boolReturn;
+}
+
+async function implAdd(intA, intB) {
+    assertIsInt(intA); assertIsInt(intB); let intReturn;
+
+    intReturn = intA + intB; await assertIsInt(intReturn); return intReturn;
+}
+
+async function implSub(intA, intB) {
+    assertIsInt(intA); assertIsInt(intB); let intReturn;
+
+    intReturn = intA - intB; await assertIsInt(intReturn); return intReturn;
+}
+
+async function implMod(intA, intB) {
+    assertIsInt(intA); assertIsInt(intB); let intReturn;
+
+    intReturn = intA % intB; await assertIsInt(intReturn); return intReturn;
+}
