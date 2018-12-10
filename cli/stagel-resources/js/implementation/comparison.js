2a372af6a8e0c9b3232ea560ba3ee011e0d571fb
ethus3h <kolubat@gmail.com>
2018-12-09 19:15:32 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/js/implementation/comparison.js
+++ cli/stagel-resources/js/implementation/comparison.js
@@ -10,14 +10,14 @@ async function implEq(genericA, genericB) {
     boolReturn = genericA == genericB; await assertIsBool(boolReturn); return boolReturn;
 }
 
-async function implGt(genericA, genericB) {
-    await assertIsGeneric(genericA); await assertIsGeneric(genericB); let boolReturn;
+async function implGt(intA, intB) {
+    await assertIsInt(intA); await assertIsInt(intB); let boolReturn;
 
-    boolReturn = genericA > genericB; await assertIsBool(boolReturn); return boolReturn;
+    boolReturn = intA > intB; await assertIsBool(boolReturn); return boolReturn;
 }
 
-async function implLt(genericA, genericB) {
-    await assertIsGeneric(genericA); await assertIsGeneric(genericB); let boolReturn;
+async function implLt(intA, intB) {
+    await assertIsInt(intA); await assertIsInt(intB); let boolReturn;
 
-    boolReturn = genericA < genericB; await assertIsBool(boolReturn); return boolReturn;
+    boolReturn = intA < intB; await assertIsBool(boolReturn); return boolReturn;
 }
