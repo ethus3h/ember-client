e13fb2e23c19ccfa6b175cd1f7b6f3188475ae6a
ethus3h <kolubat@gmail.com>
2018-12-09 19:09:11 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/generic-parameter/run/out.js
+++ cli/tests/stagel/js/generic-parameter/run/out.js
@@ -1,9 +1,8 @@
-async function or(boolA, boolB) {
-    await assertIsBool(boolA); await assertIsBool(boolB); let boolReturn;
+async function ne(intA, intB) {
+    await assertIsInt(intA); await assertIsInt(intB); let boolReturn;
 
     let boolTemp;
-    boolTemp = await not(boolA);
-    boolTemp = await not(await and(boolTemp, await not(boolB)));
+    boolTemp = await implNot(await implEq(intA, intB));
 
     boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
 }
