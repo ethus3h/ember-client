934dc4d0ac600b8a5d190453eb1c544a35330ad9
ethus3h <kolubat@gmail.com>
2018-12-10 13:33:15 -0500

    Update Git repository using egup-single

---
+++ cli/tests/stagel/js/array-s/run/out.js
@@ -0,0 +1,8 @@
+async function ne(genericA, genericB) {
+    await assertIsGeneric(genericA); await assertIsGeneric(genericB); let boolReturn;
+
+    let boolTemp;
+    boolTemp = await implNot(await implEq(genericA, genericB));
+
+    boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
+}
