4fba5200cf23d6aa45bf86281b4a13f550f90944
ethus3h <kolubat@gmail.com>
2018-12-12 15:21:29 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/array-s/out/out.js
+++ cli/tests/stagel/js/array-s/out/out.js
@@ -1,5 +1,11 @@
 async function strFromByte(intIn) {
     await assertIsInt(intIn); let strReturn;
 
-    let strArrayChars = ();
-    strArrayChars = 
+    let strArrayChars = [];
+    strArrayChars = [ '', '', '', '', '', '', '' ];
+    /* 0 through 6 */
+    let strArrayTemp = [];
+    strArrayTemp = [ 'd', 'b', 'l', 
+    'c', 'b', 'a' ];
+    await append(strArrayChars, strArrayTemp);
+}
