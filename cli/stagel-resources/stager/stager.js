6b70846a35d9b57f9b2cb433870419e9155ea119
ethus3h <kolubat@gmail.com>
2019-01-06 15:13:14 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/stager/stager.js
+++ cli/stagel-resources/stager/stager.js
@@ -1,5 +1,13 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
+async function strCharAtPos(strStr, intIndex) {
+    await assertIsStr(strStr); await assertIsInt(intIndex); let strReturn;
+
+    let strTemp = '';
+    strTemp = await substring(strStr, intIndex, 1);
+
+    strReturn = strTemp; await assertIsStr(strReturn); return strReturn;
+}
 async function or(boolA, boolB) {
     await assertIsBool(boolA); await assertIsBool(boolB); let boolReturn;
 
