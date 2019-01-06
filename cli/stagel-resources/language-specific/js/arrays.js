06635bcadb835a280b78e0909c3b3404917699a5
ethus3h <kolubat@gmail.com>
2019-01-06 15:21:39 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/arrays.js
+++ cli/stagel-resources/language-specific/js/arrays.js
@@ -5,5 +5,5 @@
 async function append(array1, array2) {
     await assertIsArray(array1); await assertIsArray(array2); let arrayReturn;
 /*FIXME*/
-    arrayReturn=array1.push(array2); await assertIsArray(arrayReturn); return arrayReturn;
+    arrayReturn=array1.concat(array2); await assertIsArray(arrayReturn); return arrayReturn;
 }
