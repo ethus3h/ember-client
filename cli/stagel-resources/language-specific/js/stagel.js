9767468060b417f766190f270ffa0a1f0b26cfab
ethus3h <kolubat@gmail.com>
2019-01-06 14:27:07 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/stagel.js
+++ cli/stagel-resources/language-specific/js/stagel.js
@@ -183,8 +183,8 @@ async function assertIsDcarr(dcarr) {
     await assertIsTrue(dcarr < Dcarrs.length);
 }
 
-async function implAssertIsBytearray(bytearray) {
-    await implFIXMEUnimplemented("implAssertIsBytearray");
+async function assertIsBytearray(bytearray) {
+    await FIXMEUnimplemented("implAssertIsBytearray");
 }
 
 
