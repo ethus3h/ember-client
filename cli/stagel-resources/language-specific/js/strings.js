c6fe7f2f0a56808122181202c1df3aa6678d1cca
ethus3h <kolubat@gmail.com>
2019-01-06 14:41:22 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/strings.js
+++ cli/stagel-resources/language-specific/js/strings.js
@@ -1,9 +1,16 @@
 /* strings, provides:
     implCat
+    implSubstr
 */
 
 async function implCat(strA, strB) {
     assertIsStr(strA); assertIsStr(strB); let strReturn;
 
     strReturn = concat(strA, strB); await assertIsStr(strReturn); return strReturn;
+}
+
+async function strSubstring(str, intStart, intLength) {
+    assertIsStr(str); assertIsInt(intStart); assertIsInt(intLength); let strReturn;
+
+    strReturn = await implStrSubstring(str, intStart, intLength); await assertIsStr(strReturn); return strReturn;
 }
