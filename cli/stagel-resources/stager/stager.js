6320ad7476cbf58cd9e5a914c40129e4f38b0472
ethus3h <kolubat@gmail.com>
2019-01-06 15:29:24 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/stager/stager.js
+++ cli/stagel-resources/stager/stager.js
@@ -146,18 +146,5 @@ async function intIsBetween(intN, intA, intB) {
 
     boolReturn = boolTemp; await assertIsBool(boolReturn); return boolReturn;
 }
-async function strFromByte(intIn) {
-    await assertIsInt(intIn); let strReturn;
-
-    let strArrayChars = [];
-    strArrayChars = [ '', '', '', '', '', '', '' ];
-    /* 0 through 6 */
-    let strArrayTemp = [];
-    strArrayTemp = [ 'd', 'b', 'l', 
-    'c', 'b', 'a' ];
-    await append(strArrayChars, strArrayTemp);
-
-    strReturn = ''; await assertIsStr(strReturn); return strReturn;
-}
 
 // @license-end
