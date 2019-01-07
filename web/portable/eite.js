db8983d88f1ac05a72a59895a632a76b08250375
ethus3h <kolubat@gmail.com>
2019-01-07 11:35:38 -0500

    Update Git repository using egup-single

--- web/portable/eite.js
+++ web/portable/eite.js
@@ -41,6 +41,14 @@ async function dcarrConvertDocument(dcarrInput, strTargetFormat, renderTraits) {
                 }
             }
             break;
+        case "HTML":
+            strReturn = await strFromUnicodeHex(await strDcDataLookupByValue("mappings/from/unicode", 1, dc, 0));
+            if (strReturn === "\u0000") {
+                /* No mapping was found by reversing Unicode, so look for a simple character mapping from the HTML mappings */
+                strReturn = await strDcDataLookupByValue("mappings/to/html", 0, dc, 1);
+            }
+            assertIsStr(strReturn); return strReturn;
+            break;
         default:
             await eiteError("Unimplemented document render target format: " + strTargetFormat);
             break;
