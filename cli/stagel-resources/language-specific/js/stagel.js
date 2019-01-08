14b77324bb48d538014dd7a9e43e00de0f7aca83
ethus3h <kolubat@gmail.com>
2019-01-08 14:09:58 -0500

    [egup] Update repository metadata

--- cli/stagel-resources/language-specific/js/stagel.js
+++ cli/stagel-resources/language-specific/js/stagel.js
@@ -180,7 +180,7 @@ async function internalDebugCollect(strMessageFragment) {
 }
 
 async function internalDebugFlush(strMessageFragment) {
-    await implDebug("Flushing debug message fragment collector, which contains: " + strMessageFragment, 3);
+    console.log("Flushing debug message fragment collector, which contains: " + strMessageFragment);
     let temp;
     temp = stagelDebugCollection;
     stagelDebugCollection = "";
