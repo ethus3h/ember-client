a600234d3bddd841b2b0e093730e75a497643e9a
ethus3h <kolubat@gmail.com>
2019-01-08 15:16:28 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/assertions.js
+++ cli/stagel-resources/language-specific/js/assertions.js
@@ -13,7 +13,7 @@
 
 async function assertIsBool(bool) {
     if (typeof bool !== "boolean" || typeof bool === "undefined" || bool === null) {
-        await implError("Assertion failed: "+bool+" is not a boolean.");
+        await implDie("Assertion failed: "+bool+" is not a boolean.");
     }
 }
 
@@ -21,7 +21,7 @@ async function assertIsTrue(bool) {
     await assertIsBool(bool);
 
     if (bool !== true) {
-        await implError("Assertion failed: "+bool+" is not true.");
+        await implDie("Assertion failed: "+bool+" is not true.");
     }
 }
 
@@ -29,38 +29,38 @@ async function assertIsFalse(bool) {
     await assertIsBool(bool);
 
     if (bool !== false) {
-        await implError("Assertion failed: "+bool+" is not false.");
+        await implDie("Assertion failed: "+bool+" is not false.");
     }
 }
 
 async function assertIsInt(int) {
     return;
     if ((! Number.isInteger(int)) || typeof int === "undefined" || int === null || int < -2147483648 || int > 2147483647) {
-        await implError("Assertion failed: "+int+" is not an int, or is outside the currently allowed range of 32 bit signed (-2,147,483,648 to 2,147,483,647).");
+        await implDie("Assertion failed: "+int+" is not an int, or is outside the currently allowed range of 32 bit signed (-2,147,483,648 to 2,147,483,647).");
     }
 }
 
 async function assertIsStr(str) {
     if (typeof str !== "string" || typeof str === "undefined" || str === null) {
-        await implError("Assertion failed: "+str+" is not a string.");
+        await implDie("Assertion failed: "+str+" is not a string.");
     }
 }
 
 async function assertIsGeneric(val) {
     if ((typeof val !== "string" && typeof val !== "boolean" && ! Number.isInteger(val)) || typeof val === "undefined" || val === null) {
-        await implError("Assertion failed: "+val+" cannot be used as a generic.");
+        await implDie("Assertion failed: "+val+" cannot be used as a generic.");
     }
 }
 
 async function assertIsGenericArray(arr) {
     if ((! Array.isArray(arr)) || typeof arr === "undefined" || arr === null) {
-        await implError("Assertion failed: "+arr+" is not an array.");
+        await implDie("Assertion failed: "+arr+" is not an array.");
     }
 }
 
 async function assertIsGenericItem(arr) {
     if ((! Array.isArray(arr)) || typeof arr === "undefined" || arr === null || (typeof val !== "string" && typeof val !== "boolean" && ! Number.isInteger(val)) || typeof val === "undefined" || val === null) {
-        await implError("Assertion failed: "+arr+" is not a generic item.");
+        await implDie("Assertion failed: "+arr+" is not a generic item.");
     }
 }
 
