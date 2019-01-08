acbffee03fda277c457c28d6660b059090e107cd
ethus3h <kolubat@gmail.com>
2019-01-08 14:45:51 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/arrays.js
+++ cli/stagel-resources/language-specific/js/arrays.js
@@ -13,7 +13,14 @@ async function append(array1, array2) {
 async function get(array, index) {
     await assertIsArray(array); await assertIsInt(index); let returnVal;
 
-    returnVal=array[index]; await assertIsGeneric(returnVal); return returnVal;
+    if(index < 0) {
+        /* JavaScript arrays don't allow negative indices without doing it this way */
+        returnVal = array.slice(index)[0];
+    }
+    else {
+        returnVal=array[index];
+    }
+    await assertIsGeneric(returnVal); return returnVal;
 }
 
 async function count(array) {
