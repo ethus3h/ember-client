11b06cb6bc54de1a308f11f6e8b48d05812606d4
ethus3h <kolubat@gmail.com>
2019-01-09 15:38:25 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/bits.js
+++ cli/stagel-resources/language-specific/js/bits.js
@@ -6,18 +6,31 @@
 // Note that bitwise operations in StageL operate on bytes rather than uint32s. Consequently, C-style 8-bit bitwise operations must be emulated for the Javascript implementation.
 
 async function bitAnd(byteA, byteB) {
-    assertIsByte(byteA); assertIsByte(byteB); let byteReturn;
+    await assertIsByte(byteA); await assertIsByte(byteB); let byteReturn;
 
     byteReturn = await internalBitwiseMask(byteA & byteB);
+
     await assertIsByte(byteReturn); return byteReturn;
 }
 
 async function bitNot(byteA) {
-    assertIsByte(byteA); let byteReturn;
+    await assertIsByte(byteA); let byteReturn;
 
     byteReturn = await internalBitwiseMask(~byteA);
     await assertIsByte(byteReturn); return byteReturn;
 }
+
+async function bitLshift(byteA, intPlaces) {
+    await assertIsByte(byteA); await assertIsInt(intPlaces); let byteReturn;
+
+    await assertIsBetween(intPlaces, 0, 31);
+
+    byteReturn = await internalBitwiseMask(byteA << intPlaces);
+
+    await assertIsByte(byteReturn); return byteReturn;
+}
+
+// Internal function
 
 async function internalBitwiseMask(uint32input) {
     let byteReturn;
