41bfd85807c5df12041a9d0e1f0074dbcd6144d7
ethus3h <kolubat@gmail.com>
2019-01-09 15:28:57 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/bits.js
+++ cli/stagel-resources/language-specific/js/bits.js
@@ -3,11 +3,12 @@
     bitNot
 */
 
+// Note that bitwise operations in StageL operate on bytes rather than ints. Consequently, C-style bitwise operations must be emulated for the Javascript implementation.
+
 async function bitAnd(byteA, byteB) {
     assertIsByte(byteA); assertIsByte(byteB); let byteReturn;
-    // Note that bitwise operations in StageL operate on bytes rather than ints. Consequently, C-style bitwise operations must be emulated for the Javascript implementation.
 
-    byteReturn = byteA & byteB;
+    byteReturn = await internalBitwiseMask(byteA & byteB);
     let byteMask;
     byteMask = 255;
     byteReturn = byteReturn & byteMask; /* zero out all but the least significant bits, which are what we want */
@@ -19,6 +20,14 @@ async function bitNot(byteA) {
     // Note that bitwise operations in StageL operate on bytes rather than ints. Consequently, C-style bitwise operations must be emulated for the Javascript implementation.
 
     byteReturn = ~byteA;
+    let byteMask;
+    byteMask = 255;
+    byteReturn = byteReturn & byteMask; /* zero out all but the least significant bits, which are what we want */
+    await assertIsByte(byteReturn); return byteReturn;
+}
+
+async function internalBitwiseMask(uint32input) {
+    let byteReturn;
     let byteMask;
     byteMask = 255;
     byteReturn = byteReturn & byteMask; /* zero out all but the least significant bits, which are what we want */
