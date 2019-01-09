dc7d916bbbb07239328b5202424077a65fc4298b
ethus3h <kolubat@gmail.com>
2019-01-09 15:25:55 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/bits.js
+++ cli/stagel-resources/language-specific/js/bits.js
@@ -10,9 +10,13 @@ async function bitAnd(byteA, byteB) {
     byteReturn = byteA && byteB; await assertIsByte(byteReturn); return byteReturn;
 }
 
-async function bitNot(boolA) {
+async function bitNot(byteA) {
     assertIsByte(byteA); let byteReturn;
     // Note that bitwise operations in StageL operate on bytes rather than ints. Consequently, C-style bitwise operations must be emulated for the Javascript implementation.
 
-    byteReturn = !byteA; await assertIsByte(byteReturn); return byteReturn;
+    byteReturn = ~byteA;
+    let byteMask;
+    byteMask = 255;
+    byteReturn = byteReturn & byteMask; /* zero out all but the least significant bits, which are what we want */
+    await assertIsByte(byteReturn); return byteReturn;
 }
