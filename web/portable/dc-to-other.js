68fd4703ff37a6415dfeb19fd56ed4750ecf27aa
ethus3h <kolubat@gmail.com>
2019-01-09 10:46:18 -0500

    Update Git repository using egup-single

--- web/portable/dc-to-other.js
+++ web/portable/dc-to-other.js
@@ -8,11 +8,11 @@ async function strPrintableDcToChar(dc, strCharacterEncoding) {
     switch (strCharacterEncoding) {
         case "ASCII-safe-subset":
         case "UTF-8":
-            strReturn = await strFromUnicodeHex(await strDcDataLookupByValue("mappings/from/unicode", 1, dc, 0));
+            strReturn = await strFromHex(await strDcDataLookupByValue("mappings/from/unicode", 1, dc, 0));
             assertIsStr(strReturn); return strReturn;
             break;
         case "HTML":
-            strReturn = await strFromUnicodeHex(await strDcDataLookupByValue("mappings/from/unicode", 1, dc, 0));
+            strReturn = await strFromHex(await strDcDataLookupByValue("mappings/from/unicode", 1, dc, 0));
             if (strReturn === "\u0000") {
                 /* No mapping was found by reversing Unicode, so look for a simple character mapping from the HTML mappings */
                 strReturn = await strDcDataLookupByValue("mappings/to/html", 0, dc, 1);
