15062336da822dd3a8f2efba441bc2d85f02d6d4
ethus3h <kolubat@gmail.com>
2019-01-09 15:15:58 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/bits.js
+++ cli/stagel-resources/language-specific/js/bits.js
@@ -1,16 +1,18 @@
 /* bits, provides:
-    implBitAnd
-    implBitNot
+    bitAnd
+    bitNot
 */
 
-async function implBitAnd(byteA, byteB) {
+async function bitAnd(byteA, byteB) {
     assertIsByte(byteA); assertIsByte(byteB); let byteReturn;
+    // Note that bitwise operations in StageL operate on bytes rather than ints. Consequently, C-style bitwise operations must be emulated for the Javascript implementation.
 
     byteReturn = byteA && byteB; await assertIsByte(byteReturn); return byteReturn;
 }
 
-async function implBitNot(boolA) {
+async function bitNot(boolA) {
     assertIsByte(byteA); let byteReturn;
+    // Note that bitwise operations in StageL operate on bytes rather than ints. Consequently, C-style bitwise operations must be emulated for the Javascript implementation.
 
     byteReturn = !byteA; await assertIsByte(byteReturn); return byteReturn;
 }
