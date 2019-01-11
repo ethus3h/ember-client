e006a38abb2065051cb02d3e239313fd7bdd5164
ethus3h <kolubat@gmail.com>
2019-01-11 13:26:04 -0500

    Update Git repository using egup-single

--- cli/tests/stagel/js/set-number-add/run/out.js
+++ cli/tests/stagel/js/set-number-add/run/out.js
@@ -1,7 +1,5 @@
 async function f() {
-    await internalDebugStackEnter('f:in');  let voidReturn;
-
-    let intCounter = 1;
+    await internalDebugStackEnter('f:in');let intCounter = 1;
     intCounter = await implAdd(intCounter, 1);
     let strTemp = '';
     await internalDebugStackExit();
