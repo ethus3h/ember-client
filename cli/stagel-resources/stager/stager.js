3a7291c09954024c7f197b64a12f4b1a19f1c71f
ethus3h <kolubat@gmail.com>
2019-01-11 15:43:50 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/stager/stager.js
+++ cli/stagel-resources/stager/stager.js
@@ -469,6 +469,7 @@ async function strFromHex(strCharacter) {
     await internalDebugCollect('str Character = ' + strCharacter + '; '); await internalDebugStackEnter('strFromHex:type-conversion'); await assertIsStr(strCharacter); let strReturn;
 
     /* Bear in mind that StageL doesn't attempt to support Unicode. */
+    await implLog(strCharacter);
     let strRes = '';
     strRes = await strFromByte(await intFromBaseStr(strCharacter, 16));
 
