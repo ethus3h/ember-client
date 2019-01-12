a8dbdf24717ef1b5e4faaf2cdcba4937431d0e46
ethus3h <kolubat@gmail.com>
2019-01-11 19:06:54 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/stager/stager.js
+++ cli/stagel-resources/stager/stager.js
@@ -552,7 +552,7 @@ async function intFromBase36Char(strN) {
 async function intFromBaseStr(strN, intB) {
     await internalDebugCollect('str N = ' + strN + '; '); await internalDebugCollect('int B = ' + intB + '; '); await internalDebugStackEnter('intFromBaseStr:math'); await assertIsStr(strN);await assertIsInt(intB); let intReturn;
 
-    debugger);
+    debugger;
     /* Returns the integer represented by n in the requested base. Strategy based on https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/ */
     await assertIsBaseStr(strN, intB);
     let strUc = '';
