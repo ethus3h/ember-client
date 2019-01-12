5cfe14c6ecdf38dee60068e89432dc27dba4b37a
ethus3h <kolubat@gmail.com>
2019-01-12 16:19:10 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/language-specific/js/assertions.js
+++ cli/stagel-resources/language-specific/js/assertions.js
@@ -44,14 +44,28 @@ async function assertIsFalse(bool) {
     }
 }
 
-async function assertIsInt(int) {
+async function isInt(int) {
     if ((! Number.isInteger(int)) || typeof int === "undefined" || int === null || int < -2147483648 || int > 2147483647) {
+        return false;
+    }
+    return true;
+}
+
+async function assertIsInt(int) {
+    if (!await isInt(int)) {
         await assertionFailed(int+" is not an int, or is outside the currently allowed range of 32 bit signed (-2,147,483,648 to 2,147,483,647).");
     }
 }
 
-async function assertIsStr(str) {
+async function isStr(str) {
     if (typeof str !== "string" || typeof str === "undefined" || str === null) {
+        return false;
+    }
+    return true;
+}
+
+async function assertIsStr(str) {
+    if (!await isStr(str)) {
         await assertionFailed(str+" is not a string.");
     }
 }
