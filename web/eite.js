3a605be6063d3f009c3eccc9186461e394c2a748
ethus3h <kolubat@gmail.com>
2019-01-14 17:04:35 -0500

    Update Git repository using egup-single

--- web/eite.js
+++ web/eite.js
@@ -1,5 +1,518 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
+/* math, provides:
+    implAdd
+    implSub
+    implMul
+    implDiv
+    implMod
+*/
+
+async function implAdd(intA, intB) {
+    assertIsInt(intA); assertIsInt(intB); let intReturn;
+
+    intReturn = intA + intB; await assertIsInt(intReturn); return intReturn;
+}
+
+async function implSub(intA, intB) {
+    assertIsInt(intA); assertIsInt(intB); let intReturn;
+
+    intReturn = intA - intB; await assertIsInt(intReturn); return intReturn;
+}
+
+async function implMul(intA, intB) {
+    assertIsInt(intA); assertIsInt(intB); let intReturn;
+
+    intReturn = intA * intB; await assertIsInt(intReturn); return intReturn;
+}
+
+async function implDiv(intA, intB) {
+    assertIsInt(intA); assertIsInt(intB); let intReturn;
+
+    // Should round towards zero. Note a portability gotcha: before C99, rounding was different. See https://stackoverflow.com/questions/17795421/bug-in-glibc-div-code
+    // It may be preferable to implement it in StageL directly at some point, but I can't be bothered to figure out how right now, and it would probably be slower than relying on native implementations.
+
+    floatReturn = intA / intB;
+    if (floatReturn < 0) {
+        intReturn = Math.ceil(floatReturn);
+    }
+    else {
+        intReturn = Math.floor(floatReturn);
+    }
+    assertIsInt(intReturn); return intReturn;
+}
+
+async function implMod(intA, intB) {
+    assertIsInt(intA); assertIsInt(intB); let intReturn;
+
+    intReturn = intA % intB; await assertIsInt(intReturn); return intReturn;
+}
+/* type-conversion, provides:
+    intFromIntStr
+    strFrom
+    charFromByte
+    byteFromChar
+*/
+
+async function intFromIntStr(str) {
+    await assertStrContainsOnlyInt(str); let intReturn;
+
+    intReturn = parseInt(str, 10); await assertIsInt(intReturn); return intReturn;
+}
+
+async function strFrom(input) {
+    await assertIsGeneric(input); let strReturn;
+
+    strReturn = String(input); await assertIsStr(strReturn); return strReturn;
+}
+
+async function charFromByte(intInput) {
+    await assertIsInt(intInput); let strReturn;
+
+    // Expects a decimal byte as input. Bear in mind that StageL doesn't attempt to support Unicode.
+
+    strReturn = String.fromCharCode(intInput); await assertIsStr(strReturn); return strReturn;
+}
+
+async function byteFromChar(strInput) {
+    await assertIsStr(strInput);
+    // Bear in mind that StageL doesn't attempt to support Unicode.
+    // We can't use assertIsChar here, because it depends on byteFromChar.
+    let intReturn;
+    intReturn = strInput.charCodeAt(0);
+
+    await assertIsTrue(intReturn > 31);
+    await assertIsTrue(intReturn < 127);
+
+    await assertIsInt(intReturn); return intReturn;
+}
+/* arrays, provides:
+    append
+    get
+    count
+*/
+
+async function append(array1, array2) {
+    await assertIsArray(array1); await assertIsArray(array2); let arrayReturn;
+
+    arrayReturn=array1.concat(array2); await assertIsArray(arrayReturn); return arrayReturn;
+}
+
+async function push(array1, array2) {
+    return append(array1, array2);
+}
+
+async function get(array, index) {
+    await assertIsArray(array); await assertIsInt(index); let returnVal;
+
+    if(index < 0) {
+        /* JavaScript arrays don't allow negative indices without doing it this way */
+        returnVal = array.slice(index)[0];
+    }
+    else {
+        returnVal=array[index];
+    }
+    await assertIsGeneric(returnVal); return returnVal;
+}
+
+async function count(array) {
+    await assertIsArray(array); let intReturn;
+
+    intReturn=Object.keys(array).length; await assertIsInt(intReturn); return intReturn;
+}
+/* strings, provides:
+    implCat
+    substring
+*/
+
+async function implCat(strA, strB) {
+    assertIsStr(strA); assertIsStr(strB); let strReturn;
+
+    strReturn = strA + "" + strB; await assertIsStr(strReturn); return strReturn;
+}
+
+async function substring(str, intStart, intLength) {
+    assertIsStr(str); assertIsInt(intStart); assertIsInt(intLength); let strReturn;
+
+    strReturn = str.substring(intStart, intStart + intLength); await assertIsStr(strReturn); return strReturn;
+}
+
+async function len(str) {
+    assertIsStr(str); let intReturn;
+
+    intReturn = str.length; await assertIsInt(intReturn); return intReturn;
+}
+
+/* logging, provides:
+    implDie
+    implWarn
+    implLog
+    implDebug
+    setDebugLevel
+    FIXMEUnimplemented
+*/
+
+var STAGEL_DEBUG;
+if (STAGEL_DEBUG === undefined) {
+    STAGEL_DEBUG = 0;
+}
+let stagelDebugCallstack = [];
+let stagelDebugCollection = "";
+//alert("Setting up logging");
+
+async function implDie(strMessage) {
+    // Don't call await assertIsStr(strMessage); here since it can call implDie and cause a recursive loop
+
+    await implError(strMessage);
+
+    throw strMessage;
+}
+
+async function implError(strMessage) {
+    if(typeof strMessage !== "string") {
+        throw "Nonstring error message";
+    }
+    // Don't call await assertIsStr(strMessage); here since it can call implDie and cause a recursive loop — maybe??
+
+    //await FIXMEUnimplemented("implError");
+    await implWarn(strMessage);
+}
+
+async function implWarn(strMessage) {
+    if(typeof strMessage !== "string") {
+        throw "Nonstring error message";
+    }
+    await assertIsStr(strMessage);
+    // Log the provided message
+
+    await FIXMEUnimplemented("implWarn");
+
+    await implLog(strMessage);
+}
+
+async function implLog(strMessage) {
+    if(typeof strMessage !== "string") {
+        throw "Nonstring error message";
+    }
+    await assertIsStr(strMessage);
+    // Log the provided message
+    await console.log(strMessage);
+    if(await Object.keys(stagelDebugCallstack).length > 0) {
+        await console.log("Previous message sent at: " + await internalDebugPrintStack());
+    }
+    else {
+        if (2 <= STAGEL_DEBUG) {
+            await console.log("(Previous message sent from non-StageL code.)");
+        }
+    }
+}
+
+async function implDebug(strMessage, intLevel) {
+    if(typeof strMessage !== "string") {
+        throw "Nonstring error message";
+    }
+    if ((! Number.isInteger(int)) || typeof int === "undefined" || int === null || int < -2147483648 || int > 2147483647) {
+        throw "Non-integer debug level";
+    }
+    await assertIsStr(strMessage); await assertIsInt(intLevel);
+    // Log the provided message
+
+    if (intLevel <= STAGEL_DEBUG) {
+        await implLog(strMessage);
+    }
+}
+
+async function setDebugLevel(intLevel) {
+    await assertIsInt(intLevel);
+    // Set the debug level to the level specified. Int from 0 to 2 inclusive. Default 0. 0 = no debug messages printed; 1 = normal debug messages printed; 2 = block entry printed; 3 = verbose printing
+
+    STAGEL_DEBUG=intLevel;
+}
+
+async function FIXMEUnimplemented(strLocation) {
+    await assertIsStr(strLocation);
+
+    await implLog("FIXME: Unimplemented in " + strLocation);
+}
+
+// Internal functions
+
+async function internalDebugQuiet(strMessage, intLevel) {
+    await assertIsStr(strMessage); await assertIsInt(intLevel);
+    // Log the provided message, but don't print a trace for it
+
+    if (intLevel <= STAGEL_DEBUG) {
+        // await implLog(strMessage);
+        console.log(strMessage);
+    }
+}
+
+async function internalDebugCollect(strMessageFragment) {
+    stagelDebugCollection = stagelDebugCollection + strMessageFragment;
+}
+
+async function internalDebugFlush() {
+    /* console.log("Flushing debug message fragment collector, which contains: " + stagelDebugCollection); */
+    let temp;
+    temp = stagelDebugCollection;
+    stagelDebugCollection = "";
+    return temp;
+}
+
+async function internalDebugStackEnter(strBlockName) {
+    //alert("DbugStackEtner");
+    if (strBlockName === undefined) {
+        await implDie("Block entry specified but no block name given");
+    }
+
+    await stagelDebugCallstack.push(strBlockName + " (" + await internalDebugFlush() + ")");
+
+    await internalDebugQuiet("Entered block: " + await stagelDebugCallstack.slice(-1)[0], 2);
+}
+
+async function internalDebugStackExit() {
+    //alert("Dbgstackext");
+    if (await stagelDebugCallstack.slice(-1)[0] === undefined) {
+        await implDie("Exited block, but no block on stack");
+    }
+    await internalDebugQuiet("Exited block: " + await stagelDebugCallstack.pop(), 3);
+}
+
+async function internalDebugPrintStack() {
+    let i;
+    i = await Object.keys(stagelDebugCallstack).length - 1;
+    let result="";
+    let arrow=" < "
+    while (i>=0) {
+        /* FIXME: This could probably be optimized if it's problematically slow. */
+        if (i==0) {
+            arrow=""
+        }
+        result = result + stagelDebugCallstack.slice(i)[0] + arrow;
+        i = i - 1;
+    }
+    return result;
+}
+/* booleans, provides:
+    implAnd
+    implNot
+*/
+
+async function implAnd(boolA, boolB) {
+    assertIsBool(boolA); assertIsBool(boolB); let boolReturn;
+
+    boolReturn = boolA && boolB; await assertIsBool(boolReturn); return boolReturn;
+}
+
+async function implNot(boolA) {
+    assertIsBool(boolA); let boolReturn;
+
+    boolReturn = !boolA; await assertIsBool(boolReturn); return boolReturn;
+}
+/* comparison, provides:
+    implEq
+    implGt
+    implLt
+*/
+
+async function implEq(genericA, genericB) {
+    await assertIsGeneric(genericA); await assertIsGeneric(genericB); let boolReturn;
+
+    boolReturn = genericA == genericB; await assertIsBool(boolReturn); return boolReturn;
+}
+
+async function implGt(intA, intB) {
+    await assertIsInt(intA); await assertIsInt(intB); let boolReturn;
+
+    boolReturn = intA > intB; await assertIsBool(boolReturn); return boolReturn;
+}
+
+async function implLt(intA, intB) {
+    await assertIsInt(intA); await assertIsInt(intB); let boolReturn;
+
+    boolReturn = intA < intB; await assertIsBool(boolReturn); return boolReturn;
+}
+/* assertions, provides:
+    assertIsBool
+    assertIsTrue
+    assertIsFalse
+    assertIsInt
+    assertIsStr
+    assertIsGeneric
+    assertIsGenericArray
+    assertIsGenericItem
+    assertionFailed
+*/
+
+// Assertions that something is a given type
+
+async function isBool(bool) {
+    if (typeof bool !== "boolean" || typeof bool === "undefined" || bool === null) {
+        return false;
+    }
+    return true;
+}
+
+async function assertIsBool(bool) {
+    if (!await isBool(bool)) {
+        await assertionFailed(bool+" is not a boolean.");
+    }
+}
+
+/* TODO: move assertIsTrue/assertIsFalse to StageR once bool literals are available */
+async function assertIsTrue(bool) {
+    await assertIsBool(bool);
+
+    if (bool !== true) {
+        await assertionFailed(bool+" is not true.");
+    }
+}
+
+async function assertIsFalse(bool) {
+    await assertIsBool(bool);
+
+    if (bool !== false) {
+        await assertionFailed(bool+" is not false.");
+    }
+}
+
+async function isInt(int) {
+    if ((! Number.isInteger(int)) || typeof int === "undefined" || int === null || int < -2147483648 || int > 2147483647) {
+        return false;
+    }
+    return true;
+}
+
+async function assertIsInt(int) {
+    if (!await isInt(int)) {
+        await assertionFailed(int+" is not an int, or is outside the currently allowed range of 32 bit signed (-2,147,483,648 to 2,147,483,647).");
+    }
+}
+
+async function isStr(str) {
+    if (typeof str !== "string" || typeof str === "undefined" || str === null) {
+        return false;
+    }
+    return true;
+}
+
+async function assertIsStr(str) {
+    if (!await isStr(str)) {
+        await assertionFailed(str+" is not a string.");
+    }
+}
+
+async function isGeneric(val) {
+    // We have to do isGeneric in native code because otherwise the assertion at the start of the function would call it.
+    if (! (await isStr(val) || await isInt(val) || await isBool(val))) {
+        return false;
+    }
+    return true;
+}
+
+async function assertIsGeneric(val) {
+    if (!await isGeneric(val)) {
+        await assertionFailed(val+" cannot be used as a generic.");
+    }
+}
+
+async function isGenericArray(val) {
+    // Can't use count() here because it depends on this function.
+    let intCount = Object.keys(val).length;
+    let genericElem;
+    while (intCount > 0) {
+        intCount = intCount - 1;
+        genericElem = val.slice(intCount)[0];
+        if (!await isGeneric(genericElem)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+async function assertIsGenericArray(val) {
+    if (!await isGenericArray(val)) {
+        await assertionFailed(val+" cannot be used as a generic array.");
+    }
+}
+
+async function isGenericItem(val) {
+    if (! (await isGeneric(val) || await isGenericArray(val))) {
+        return false;
+    }
+    return true;
+}
+
+async function assertIsGenericItem(val) {
+    if (!await isGenericItem(val)) {
+        await assertionFailed(val+" cannot be used as a generic item.");
+    }
+}
+
+async function assertionFailed(message) {
+    await implDie("Assertion failed: "+message);
+}
+/* bits, provides:
+    bitAnd
+    bitNot
+*/
+
+// Note that bitwise operations in StageL operate on bytes rather than int32s. Consequently, C-style 8-bit bitwise operations must be emulated for the Javascript implementation.
+
+async function bitAnd(byteA, byteB) {
+    await assertIsByte(byteA); await assertIsByte(byteB); let byteReturn;
+
+    byteReturn = await internalBitwiseMask(byteA & byteB);
+
+    await assertIsByte(byteReturn); return byteReturn;
+}
+
+async function bitNot(byteA) {
+    await assertIsByte(byteA); let byteReturn;
+
+    byteReturn = await internalBitwiseMask(~byteA);
+    await assertIsByte(byteReturn); return byteReturn;
+}
+
+async function bitLshift(byteA, intPlaces) {
+    await assertIsByte(byteA); await assertIsInt(intPlaces); let byteReturn;
+
+    await assertIsBetween(intPlaces, 0, 8);
+
+    byteReturn = await internalBitwiseMask(byteA << intPlaces);
+
+    await assertIsByte(byteReturn); return byteReturn;
+}
+
+async function bitRshift(byteA, intPlaces) {
+    await assertIsByte(byteA); await assertIsInt(intPlaces); let byteReturn;
+
+    await assertIsBetween(intPlaces, 0, 8);
+
+    byteReturn = await internalBitwiseMask(byteA >>> intPlaces); /* >>> is needed in JavaScript to make it fill zeroes behind it. >> does something else. */
+
+    await assertIsByte(byteReturn); return byteReturn;
+}
+
+// Internal function
+
+async function internalBitwiseMask(int32input) {
+    let byteReturn;
+    let byteMask;
+    byteMask = 255;
+    byteReturn = int32input & byteMask; /* zero out all but the least significant bits, which are what we want */
+    return byteReturn;
+}
+/* type-tools, provides:
+    implIntBytearrayLength
+    implIntDcarrLength
+*/
+
+async function intBytearrayLength(bytearray) {
+    assertIsBytearray(bytearray); let intReturn;
+
+    intReturn = bytearray.byteLength; await assertIsInt(intReturn); return intReturn;
+}
 
 // @license-end
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
