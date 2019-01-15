a5a3f3e81431c73d990f421aa9287812565f376f
ethus3h <kolubat@gmail.com>
2019-01-15 16:11:12 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/assertions.js
+++ cli/stagel-resources/core-implementations/js/assertions.js
@@ -92,7 +92,7 @@ async function assertIsGenericArray(val) {
     function isGenericSync(v) {
         return (typeof v === 'boolean' || typeof v === 'string' || (Number.isInteger(v) && v >= -2147483648 && v <= 2147483647));
     }
-    if val.every(isGenericSync) {
+    if (val.every(isGenericSync)) {
         return;
     }
     else {
@@ -111,7 +111,7 @@ async function isGenericItem(val) {
     function isGenericSync(v) {
         return (typeof v === 'boolean' || typeof v === 'string' || (Number.isInteger(v) && v >= -2147483648 && v <= 2147483647));
     }
-    if val.every(isGenericSync) {
+    if (val.every(isGenericSync)) {
         return true;
     }
     else {
@@ -129,7 +129,7 @@ async function assertIsGenericItem(val) {
     function isGenericSync(v) {
         return (typeof v === 'boolean' || typeof v === 'string' || (Number.isInteger(v) && v >= -2147483648 && v <= 2147483647));
     }
-    if val.every(isGenericSync) {
+    if (val.every(isGenericSync)) {
         return true;
     }
     else {
