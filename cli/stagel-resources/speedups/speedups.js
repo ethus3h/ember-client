6618cb55cac6e4531125f86ce9df208ef8fc817d
ethus3h <kolubat@gmail.com>
2019-01-15 15:47:19 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/speedups/speedups.js
+++ cli/stagel-resources/speedups/speedups.js
@@ -2,3 +2,19 @@ async function or(a,b) {
     await assertIsBool(a); await assertIsBool(b);
     return a || b;
 }
+/* TODO: move assertIsTrue/assertIsFalse to StageR once bool literals are available */
+async function assertIsTrue(bool) {
+    await assertIsBool(bool);
+
+    if (bool !== true) {
+        await assertionFailed(bool+' is not true.');
+    }
+}
+
+async function assertIsFalse(bool) {
+    await assertIsBool(bool);
+
+    if (bool !== false) {
+        await assertionFailed(bool+' is not false.');
+    }
+}
