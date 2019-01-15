48f5e5db044ba0e07de4fb5867a9f173befdec2f
ethus3h <kolubat@gmail.com>
2019-01-14 23:25:05 -0500

    [egup] Update repository metadata

--- cli/stagel-resources/stager/stager.js
+++ cli/stagel-resources/stager/stager.js
@@ -416,4 +416,46 @@ async function bitXnor(intByte1, intByte2) {
     intTemp = await bitNot(await bitXor(intByte1, intByte2));
     await assertIsByte(intTemp);
 
-    
+    intReturn = intTemp; await assertIsInt(intReturn); await internalDebugStackExit(); return intReturn;
+}
+async function or(boolA, boolB) {
+    await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugCollect('bool B = ' + boolB + '; '); await internalDebugStackEnter('or:booleans'); await assertIsBool(boolA);await assertIsBool(boolB); let boolReturn;
+
+    let boolTemp = false;
+    boolTemp = await implNot(boolA);
+    boolTemp = await implNot(await implAnd(boolTemp, await implNot(boolB)));
+
+    boolReturn = boolTemp; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
+}
+
+async function nor(boolA, boolB) {
+    await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugCollect('bool B = ' + boolB + '; '); await internalDebugStackEnter('nor:booleans'); await assertIsBool(boolA);await assertIsBool(boolB); let boolReturn;
+
+    let boolTemp = false;
+    boolTemp = await implNot(await or(boolA, boolB));
+
+    boolReturn = boolTemp; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
+}
+
+async function nand(boolA, boolB) {
+    await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugCollect('bool B = ' + boolB + '; '); await internalDebugStackEnter('nand:booleans'); await assertIsBool(boolA);await assertIsBool(boolB); let boolReturn;
+
+    let boolTemp = false;
+    boolTemp = await implNot(await implAnd(boolA, boolB));
+
+    boolReturn = boolTemp; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
+}
+
+async function xor(boolA, boolB) {
+    await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugCollect('bool B = ' + boolB + '; '); await internalDebugStackEnter('xor:booleans'); await assertIsBool(boolA);await assertIsBool(boolB); let boolReturn;
+
+    let boolTemp = false;
+    boolTemp = await nand(boolA, boolB);
+    boolTemp = await implAnd(boolTemp, await or(boolA, boolB));
+
+    boolReturn = boolTemp; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
+}
+
+async function xnor(boolA, boolB) {
+    await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugCollect('bool B = ' + boolB + '; '); await internalDebugStackEnter('xnor:booleans'); await assertIsBool(boolA);await assertIsBool(boolB); let boolReturn;
+
