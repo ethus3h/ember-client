e8f214f920943e357bf5726b467e8ce48adc45b5
ethus3h <kolubat@gmail.com>
2019-01-15 22:56:02 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/stagel.js
+++ cli/stagel-resources/core-implementations/js/stagel.js
@@ -334,30 +334,30 @@ async function implLt(intA, intB) {
 
     boolReturn = intA < intB; await assertIsBool(boolReturn); return boolReturn;
 }
-async function dcDatasetLength(strDataset) {
-    assertIsStr(strDataset); let intReturn;
+async function dcDatasetLength(dataset) {
+    assertIsDcDataset(dataset); let intReturn;
 
-    intReturn = await dcData[strDataset].length; await assertIsInt(intReturn); return intReturn;
+    intReturn = await dcData[dataset].length; await assertIsInt(intReturn); return intReturn;
 }
 
-async function dcDataLookupById(strDataset, intRowNumber, intFieldNumber) {
-    await assertIsStr(strDataset); await assertIsInt(intRowNumber); await assertIsInt(intFieldNumber); let strReturn;
+async function dcDataLookupById(dataset, rowNum, fieldNum) {
+    await assertIsStr(dataset); await assertIsInt(rowNum); await assertIsInt(fieldNum); let strReturn;
 
-    strReturn = dcData[strDataset][intRowNumber].data[0][intFieldNumber]; await assertIsStr(strReturn); return strReturn;
+    strReturn = dcData[dataset][rowNum].data[0][fieldNum]; await assertIsStr(strReturn); return strReturn;
 }
 
-async function dcDataLookupByValue(strDataset, intFilterField, genericFilterValue, intDesiredField) {
-    await assertIsStr(strDataset); await assertIsInt(intFilterField); await assertIsGeneric(genericFilterValue); await assertIsInt(intDesiredField); let strReturn;
+async function dcDataLookupByValue(dataset, filterField, filterValue, desiredField) {
+    await assertIsDcDataset(dataset); await assertIsInt(filterField); await assertIsGeneric(filterValue); await assertIsInt(desiredField); let strReturn;
 
-    let intLength = await intDcDataDatasetLength(strDataset);
+    let intLength = await intDcDataDatasetLength(dataset);
     // start at 1 to skip header row
     let strFilterValue = await strFrom(genericFilterValue);
     for (let intRow = 1; intRow < intLength; intRow++) {
-        if(dcData[strDataset][intRow].data[0][intFilterField] === strFilterValue) {
-            strReturn = dcData[strDataset][intRow].data[0][intDesiredField]; await assertIsStr(strReturn); return strReturn;
+        if(dcData[dataset][intRow].data[0][intFilterField] === strFilterValue) {
+            strReturn = dcData[dataset][intRow].data[0][intDesiredField]; await assertIsStr(strReturn); return strReturn;
         }
     }
-    //await console.log("SEARCHING", strDataset, intFilterField, genericFilterValue, intDesiredField, dcData);
+    //await console.log("SEARCHING", dataset, intFilterField, genericFilterValue, intDesiredField, dcData);
     // If nothing was found, return this UUID.
     strReturn="89315802-d53d-4d11-ba5d-bf505e8ed454"; await assertIsStr(strReturn); return strReturn;
 }
