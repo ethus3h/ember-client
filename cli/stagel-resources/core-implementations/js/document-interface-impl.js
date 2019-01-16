de8c7bed28e37212f2d916dd14618e9944e6fff7
ethus3h <kolubat@gmail.com>
2019-01-16 17:16:31 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/document-interface-impl.js
+++ cli/stagel-resources/core-implementations/js/document-interface-impl.js
@@ -6,3 +6,18 @@ async function internalRunDocument(document) {
     let events = [];
     events = await getDesiredEventNotifications(doc);
 }
+
+
+async function implUrlLoadForCallback(url, callback) {
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", url, true);
+    oReq.responseType = "arraybuffer";
+    oReq.onload = async function(oEvent) {
+        callback(new Uint8Array(oReq.response)); // Note: not oReq.responseText
+    };
+    oReq.send(null);
+}
+
+async function implOperateOnDocFromUrl(strFormat, strUrl, callback) {
+    await implUrlLoadForCallback(strUrl, async function(bytearrayContent) { await callback(await dcarrParseDocument(strFormat, bytearrayContent)); });
+}
