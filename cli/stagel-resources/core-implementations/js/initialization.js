95fb4f3bacf74eb19cb2f5a57cdf822fbe35dba3
ethus3h <kolubat@gmail.com>
2019-01-16 16:13:26 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/initialization.js
+++ cli/stagel-resources/core-implementations/js/initialization.js
@@ -1,6 +1,11 @@
+// Global variables
+
 let haveDom = false;
+let datasets = [];
+let dcData = [];
 
 async function internalSetup() {
+    // Detect if we can create DOM nodes (otherwise we'll output to a terminal)
     if (typeof window !== 'undefined') {
         haveDom = true;
     }
