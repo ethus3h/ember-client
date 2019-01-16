b2d5fb3ec22b5de87f25f050ea05fda93cb13375
ethus3h <kolubat@gmail.com>
2019-01-16 16:09:41 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/initialization.js
+++ cli/stagel-resources/core-implementations/js/initialization.js
@@ -1,1 +1,5 @@
- 
+async function internalSetup() {
+    assertIsDcDataset(dataset); let intReturn;
+
+    intReturn = await dcData[dataset].length; await assertIsInt(intReturn); return intReturn;
+}
