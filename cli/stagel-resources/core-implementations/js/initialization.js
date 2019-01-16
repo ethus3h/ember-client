095e63631042769eb7961c07d0da79ee4e637162
ethus3h <kolubat@gmail.com>
2019-01-16 18:06:28 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/initialization.js
+++ cli/stagel-resources/core-implementations/js/initialization.js
@@ -26,58 +26,57 @@ async function internalSetup() {
     await internalLoadDatasets();
     if (haveDom) {
         // Override error reporting method to show alert
-// TODO: Does this always work? Overrides aren't really possible when it's load-order-independent, I wouldn't think...
-async function implDie(strMessage) {
-    // Don't call await assertIsStr(strMessage); here since it can call implDie and cause a recursive loop
+        // TODO: Does this always work? Overrides aren't really possible when it's load-order-independent, I wouldn't think...
+        async function implDie(strMessage) {
+            // Don't call await assertIsStr(strMessage); here since it can call implDie and cause a recursive loop
 
-    await implError(strMessage);
+            await implError(strMessage);
 
-    throw strMessage;
-}
+            throw strMessage;
+        }
 
-async function implError(strMessage) {
-    if(typeof strMessage !== "string") {
-        throw "Nonstring error message";
-    }
-    // Don't call await assertIsStr(strMessage); here since it can call implDie and cause a recursive loop — maybe??
+        async function implError(strMessage) {
+            if(typeof strMessage !== "string") {
+                throw "Nonstring error message";
+            }
+            // Don't call await assertIsStr(strMessage); here since it can call implDie and cause a recursive loop — maybe??
 
-    //await FIXMEUnimplemented("implError");
-    await implWarn(strMessage);
+            //await FIXMEUnimplemented("implError");
+            await implWarn(strMessage);
 
-    await console.trace();
-    alert("EITE reported error!: " + strMessage);
-}
+            await console.trace();
+            alert("EITE reported error!: " + strMessage);
+        }
 
-async function implWarn(strMessage) {
-    await assertIsStr(strMessage);
-    // Log the provided message
+        async function implWarn(strMessage) {
+            await assertIsStr(strMessage);
+            // Log the provided message
 
-    await FIXMEUnimplemented("implWarn");
-    await implLog(strMessage);
+            await FIXMEUnimplemented("implWarn");
+            await implLog(strMessage);
 
-    await console.trace();
-}
-
-async function implLog(strMessage) {
-    await assertIsStr(strMessage);
-    // Log the provided message
-
-    await console.log(strMessage);
-    //await console.trace();
-    if(await Object.keys(stagelDebugCallstack).length > 0) {
-        await console.log("Previous message sent at: " + await internalDebugPrintStack());
-    }
-    else {
-        if (2 <= STAGEL_DEBUG && 3 > STAGEL_DEBUG) {
-            await console.log("(Previous message sent from non-StageL code.)");
             await console.trace();
         }
-    }
-    if (3 <= STAGEL_DEBUG) {
-        await console.trace();
-    }
-}
 
+        async function implLog(strMessage) {
+            await assertIsStr(strMessage);
+            // Log the provided message
+
+            await console.log(strMessage);
+            //await console.trace();
+            if(await Object.keys(stagelDebugCallstack).length > 0) {
+                await console.log("Previous message sent at: " + await internalDebugPrintStack());
+            }
+            else {
+                if (2 <= STAGEL_DEBUG && 3 > STAGEL_DEBUG) {
+                    await console.log("(Previous message sent from non-StageL code.)");
+                    await console.trace();
+                }
+            }
+            if (3 <= STAGEL_DEBUG) {
+                await console.trace();
+            }
+        }
     }
     setupFinished = true;
 }
