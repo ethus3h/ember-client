828ad66b8e3e6f1b378807be2bf66ae1c6bf213f
ethus3h <kolubat@gmail.com>
2019-01-16 18:06:07 -0500

    Update Git repository using egup-single

--- cli/eite-implementation/language-specific/js/browser/eite-browser.js
+++ cli/eite-implementation/language-specific/js/browser/eite-browser.js
@@ -2,59 +2,6 @@
 // This file defines functions with implementations that are specific to Web browser implementation of EITE in JavaScript.
 // This is a library file, and should only initialize functions/variables, so that it can be loaded and run in parallel with other library files, and work regardless of the order they are loaded.
 
-// Override error reporting method to show alert
-// TODO: Does this always work? Overrides aren't really possible when it's load-order-independent, I wouldn't think...
-async function implDie(strMessage) {
-    // Don't call await assertIsStr(strMessage); here since it can call implDie and cause a recursive loop
-
-    await implError(strMessage);
-
-    throw strMessage;
-}
-
-async function implError(strMessage) {
-    if(typeof strMessage !== "string") {
-        throw "Nonstring error message";
-    }
-    // Don't call await assertIsStr(strMessage); here since it can call implDie and cause a recursive loop — maybe??
-
-    //await FIXMEUnimplemented("implError");
-    await implWarn(strMessage);
-
-    await console.trace();
-    alert("EITE reported error!: " + strMessage);
-}
-
-async function implWarn(strMessage) {
-    await assertIsStr(strMessage);
-    // Log the provided message
-
-    await FIXMEUnimplemented("implWarn");
-    await implLog(strMessage);
-
-    await console.trace();
-}
-
-async function implLog(strMessage) {
-    await assertIsStr(strMessage);
-    // Log the provided message
-
-    await console.log(strMessage);
-    //await console.trace();
-    if(await Object.keys(stagelDebugCallstack).length > 0) {
-        await console.log("Previous message sent at: " + await internalDebugPrintStack());
-    }
-    else {
-        if (2 <= STAGEL_DEBUG && 3 > STAGEL_DEBUG) {
-            await console.log("(Previous message sent from non-StageL code.)");
-            await console.trace();
-        }
-    }
-    if (3 <= STAGEL_DEBUG) {
-        await console.trace();
-    }
-}
-
 // Fully platform-specific code
 
 async function implGetEnvironmentRenderTraits(targetFormat) {
