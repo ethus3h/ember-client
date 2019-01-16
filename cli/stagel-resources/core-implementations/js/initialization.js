3f48420bb12a26f9ce424f373713e82ba64c95d6
ethus3h <kolubat@gmail.com>
2019-01-16 16:11:31 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/initialization.js
+++ cli/stagel-resources/core-implementations/js/initialization.js
@@ -1,6 +1,8 @@
+let haveDom = false;
+
 async function internalSetup() {
-    let haveDom = false;
     if (typeof window !== 'undefined') {
-        https://web.archive.org/web/20190116201150/https://stackoverflow.com/questions/34550890/how-to-detect-if-script-is-running-in-browser-or-in-node-js
+        haveDom = true;
     }
+    
 }
