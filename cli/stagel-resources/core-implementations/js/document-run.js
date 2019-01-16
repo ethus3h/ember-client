029932e1c4a0f561e9281ab4d2776d36cad91dbf
ethus3h <kolubat@gmail.com>
2019-01-16 17:11:00 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/document-run.js
+++ cli/stagel-resources/core-implementations/js/document-run.js
@@ -1,13 +1,8 @@
-// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
-
-//        implRunEiteTest("ept", "idiomatic-hello-world-sems");
-
 async function internalRunDocument(document) {
     await assertIsDcArray(document);
+
     let doc = '';
     doc = await startDocument(document);
     let events = [];
     events = await getDesiredEventNotifications(doc);
-};
-
-// @license-end
+}
