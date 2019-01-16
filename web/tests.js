367a69bf157fabd21d481cea0556fc995e4f65b2
ethus3h <kolubat@gmail.com>
2019-01-16 17:00:02 -0500

    Update Git repository using egup-single

--- web/tests.js
+++ web/tests.js
@@ -1,7 +1,7 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
 window.onload = function() {
-    startDocument(async function())
+    startDocument(async function() { return await loadDocument('sems', 'idiomatic-hello-world.sems'); });
     implLoadDatasets(function() {
         // This is where the actual commands to run (entry points) should go
         implRunEiteTest("ept", "idiomatic-hello-world-sems");
