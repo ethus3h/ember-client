732018de54cad326a1228277028bc8c62bb6a6d0
ethus3h <kolubat@gmail.com>
2019-01-15 22:58:31 -0500

    Update Git repository using egup-single

--- web/portable/dc-data.js
+++ web/portable/dc-data.js
@@ -1,28 +1,3 @@
-// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
-/* Depends:
-    implIntDcDataDatasetLength
-    implDcDataLookupById
-    implDcDataLookupByValue
-*/
-
-async function intDcDataDatasetLength(strDataset) {
-    assertIsStr(strDataset); let intReturn;
-
-    intReturn = await implIntDcDataDatasetLength(strDataset); await assertIsInt(intReturn); return intReturn;
-}
-
-async function dcDataLookupById(strDataset, intRowNumber, intFieldNumber) {
-    await assertIsStr(strDataset); await assertIsInt(intRowNumber); await assertIsInt(intFieldNumber); let strReturn;
-
-    strReturn = await implDcDataLookupById(strDataset, intRowNumber, intFieldNumber); await assertIsStr(strReturn); return strReturn;
-}
-
-async function dcDataLookupByValue(strDataset, intFilterField, genericFilterValue, intDesiredField) {
-    await assertIsStr(strDataset); await assertIsInt(intFilterField); await assertIsGeneric(genericFilterValue); await assertIsInt(intDesiredField); let strReturn;
-
-    strReturn = await implDcDataLookupByValue(strDataset, intFilterField, genericFilterValue, intDesiredField); await assertIsStr(strReturn); return strReturn;
-}
-
 async function dcGetField(dc, intFieldNumber) {
     await assertIsDc(dc); await assertIsInt(intFieldNumber); let strReturn;
 
