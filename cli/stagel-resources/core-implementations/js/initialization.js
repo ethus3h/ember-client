f0cc5210321b4a8718541ae5ba280fab9e3ffd92
ethus3h <kolubat@gmail.com>
2019-01-16 16:10:30 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/initialization.js
+++ cli/stagel-resources/core-implementations/js/initialization.js
@@ -1,5 +1,6 @@
 async function internalSetup() {
-    assertIsDcDataset(dataset); let intReturn;
-
-    intReturn = await dcData[dataset].length; await assertIsInt(intReturn); return intReturn;
+    let haveDom = false;
+    if (typeof window !== 'undefined') {
+        https://web.archive.org/web/20190116201150/https://stackoverflow.com/questions/34550890/how-to-detect-if-script-is-running-in-browser-or-in-node-js
+    }
 }
