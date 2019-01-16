b62f2b969f98c4d66cd330ff85008ad451ee8479
ethus3h <kolubat@gmail.com>
2019-01-16 18:03:21 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/environment.js
+++ cli/stagel-resources/core-implementations/js/environment.js
@@ -1,4 +1,5 @@
 async function getEnvironmentPreferredFormat() {
+    // Note that this routine will produce different outputs on different StageL target platforms, and that's not a problem since that's what it's for.
     if (haveDom) {
         return 'HTML';
     }
