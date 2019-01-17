f5e2b143388d2d4138a7568f2025896cd2187363
ethus3h <kolubat@gmail.com>
2019-01-16 20:22:50 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/dc-data.js
+++ cli/stagel-resources/core-implementations/js/dc-data.js
@@ -7,6 +7,11 @@ async function dcDatasetLength(dataset) {
 async function dcDataLookupById(dataset, rowNum, fieldNum) {
     await assertIsDcDataset(dataset); await assertIsInt(rowNum); await assertIsInt(fieldNum); let strReturn;
 
+    if (dcData[dataset] === undefined) {
+        console.log(dcData);
+        assertFailed('DcData for the dataset '+dataset+' does not seem to be available. Setup finished: '+setupFinished);
+    }
+
     strReturn = dcData[dataset][rowNum].data[0][fieldNum]; await assertIsStr(strReturn); return strReturn;
 }
 
