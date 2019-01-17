1542ee5393330288266b53d660b5fd5378ce7e9c
ethus3h <kolubat@gmail.com>
2019-01-17 15:17:38 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/dc-data.js
+++ cli/stagel-resources/core-implementations/js/dc-data.js
@@ -13,7 +13,7 @@ async function dcDataLookupById(dataset, rowNum, fieldNum) {
         await implDie('dcDataLookupById called, but dataset '+dataset+' does not appear to be available.');
     }
 
-    strReturn = dcData[dataset][rowNum].data[0][fieldNum]; await assertIsStr(strReturn); return strReturn;
+    strReturn = dcData[dataset][rowNum][fieldNum]; await assertIsStr(strReturn); return strReturn;
 }
 
 async function dcDataLookupByValue(dataset, filterField, genericFilterValue, desiredField) {
@@ -23,8 +23,8 @@ async function dcDataLookupByValue(dataset, filterField, genericFilterValue, des
     // start at 1 to skip header row
     let filterValue = await strFrom(genericFilterValue);
     for (let row = 1; row < intLength; row++) {
-        if(dcData[dataset][row].data[0][filterField] === filterValue) {
-            strReturn = dcData[dataset][row].data[0][desiredField]; await assertIsStr(strReturn); return strReturn;
+        if(dcData[dataset][row][filterField] === filterValue) {
+            strReturn = dcData[dataset][row][desiredField]; await assertIsStr(strReturn); return strReturn;
         }
     }
     //await console.log("SEARCHING", dataset, filterField, genericFilterValue, desiredField, dcData);
