82c1e0720120d8edfb9965bf11bcec2ed69ceafa
ethus3h <kolubat@gmail.com>
2019-01-17 00:23:32 -0500

    Update Git repository using egup-single

--- web/eite.js
+++ web/eite.js
@@ -221,7 +221,6 @@ async function internalLoadDatasets() {
         dataset = datasets[count];
         dcData[dataset] = [];
         // I guess the anonymous functions defined as parameters to the Papa.parse call inherit the value of dataset from the environment where they were defined (i.e., here)??
-    await alert('wtf 2');
         await Papa.parse('../data/' + dataset + '.csv', {
             download: true,
             encoding: "UTF-8",
@@ -233,10 +232,10 @@ async function internalLoadDatasets() {
             },
             complete: async function(results, file) {
                 return;
-            },
+            }/*,
             error: async function(results, file) {
                 await implError("Error reported while parsing "+dataset+"!");
-            }
+            }*/
         });
     }
     datasetsLoaded = true;
