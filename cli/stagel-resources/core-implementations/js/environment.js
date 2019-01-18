e7bb5c976e7fbacff96e7aa9b2ff4f36aada05e8
ethus3h <kolubat@gmail.com>
2019-01-17 20:03:26 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/environment.js
+++ cli/stagel-resources/core-implementations/js/environment.js
@@ -4,7 +4,7 @@ async function getEnvPreferredFormat() {
 }
 
 async function getEnvResolutionW() {
-    // Result for this is either in pixels or characters. For immutableCharacterCells, it's just the number of columns available, defaulting to 80 if we can't tell, and says 1 line available.
+    // Result for this is either in pixels or characters. For immutableCharacterCells, it's just the number of columns available, defaulting to 80 if we can't tell, and says 1 line available. If it's -1, it's unlimited (probably this would only occur if explicitly configured as such).
     return envResolutionW;
 }
 
