1c2b0638eff84b5cf2d8760f706c33cf830685c3
ethus3h <kolubat@gmail.com>
2019-01-17 19:53:35 -0500

    Update Git repository using egup-single

--- cli/stagel-resources/core-implementations/js/environment.js
+++ cli/stagel-resources/core-implementations/js/environment.js
@@ -1,9 +1,6 @@
 async function getEnvironmentPreferredFormat() {
     // Note that this routine will produce different outputs on different StageL target platforms, and that's not a problem since that's what it's for.
-    if (haveDom) {
-        return 'HTML';
-    }
-    return 'immutableCharacterCells';
+    return environmentPreferredFormat;
 }
 
 async function getEnvironmentResolutionW() {
