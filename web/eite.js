36c55db5351fb481c387b4d63857f793155a43d0
ethus3h <kolubat@gmail.com>
2019-01-19 18:15:33 -0500

    Update Git repository using egup-single

--- web/eite.js
+++ web/eite.js
@@ -1988,7 +1988,7 @@ async function dcToFormat(strOutFormat, intDc) {
             intArrayRes = await push(intArrayRes, await hexToDec(strRes));
         }
         else {
-            intArrayRes = await push(intArrayRes, await strToBytearray(await dcDataLookupByValue('mappings/to/html', 0, intDc, 1)));
+            intArrayRes = await push(intArrayRes, await strToByteArray(await dcDataLookupByValue('mappings/to/html', 0, intDc, 1)));
         }
     }
     else {
