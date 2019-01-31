f0c1ff887f2d20cdffb50d44ad686c32f755450e
ethus3h <kolubat@gmail.com>
2019-01-31 02:51:29 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/dc-data.js
+++ implementation/core-implementations/js/dc-data.js
@@ -8,18 +8,19 @@ async function dcDataLookupById(dataset, rowNum, fieldNum) {
     await assertIsDcDataset(dataset); await assertIsInt(rowNum); await assertIsInt(fieldNum); let strReturn;
 
     if (dcData[dataset] === undefined) {
-        console.log(dcData);
-        console.log(setupFinished);
         await implDie('dcDataLookupById called, but dataset '+dataset+' does not appear to be available.');
     }
 
+    if (rowNum >= dcData[dataset].length) {
+        await implDie('The requested row '+rowNum+' is greater than the number of entries in the ' + dataset + ' dataset ('+dcData[dataset].length+').');
+    }
     strReturn = dcData[dataset][rowNum][fieldNum]; await assertIsStr(strReturn); return strReturn;
 }
 
 async function dcDataLookupByValue(dataset, filterField, genericFilterValue, desiredField) {
     await assertIsDcDataset(dataset); await assertIsInt(filterField); await assertIsGeneric(genericFilterValue); await assertIsInt(desiredField); let strReturn;
 
-    let intLength = await dcDatasetLength(dataset);
+    let intLength = dcData[dataset].length;
     // start at 1 to skip header row
     let filterValue = await strFrom(genericFilterValue);
     for (let row = 1; row < intLength; row++) {
