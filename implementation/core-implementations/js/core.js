cab29d38f28cbe87922b0a7af4fbaa81f60c69ab
ethus3h <kolubat@gmail.com>
2019-01-31 02:47:05 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/core.js
+++ implementation/core-implementations/js/core.js
@@ -205,7 +205,7 @@ async function internalSetup() {
 
         window.implError = async function (strMessage) {
             if(typeof strMessage !== "string") {
-                alert("EITE reported error: Nonstring error message!");
+                alert("EITE reported an error! You may want to reload the page. The error was: Nonstring error message!");
                 throw "Nonstring error message";
             }
             // Don't call await assertIsStr(strMessage); here since it can call implDie and cause a recursive loop — maybe??
@@ -214,7 +214,7 @@ async function internalSetup() {
             await implWarn(strMessage);
 
             await console.trace();
-            alert("EITE reported error!: " + strMessage);
+            alert("EITE reported an error! You may want to reload the page. The error was: " + strMessage);
         }
 
         window.implWarn = async function (strMessage) {
