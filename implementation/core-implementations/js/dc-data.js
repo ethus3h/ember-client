2ac7ee54eebe8bfb507ad6c39dccdb9b59098fae
ethus3h <kolubat@gmail.com>
2019-02-02 04:15:26 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/dc-data.js
+++ implementation/core-implementations/js/dc-data.js
@@ -40,3 +40,39 @@ async function dcDataLookupByValue(dataset, filterField, genericFilterValue, des
     // If nothing was found, return this UUID.
     strReturn="89315802-d53d-4d11-ba5d-bf505e8ed454"; await assertIsStr(strReturn); return strReturn;
 }
+
+async function dcDataLookupById(dataset, rowNum, fieldNum) {
+    await assertIsDcDataset(dataset); await assertIsInt(rowNum); await assertIsInt(fieldNum); let strReturn;
+
+    // This routine returns the value of the specified cell of the nth row in the dataset (zero-indexed, such that the 0th row is the first content row, and the header row is not available (would be -1 but isn't available from this routine)).
+    if (dcData[dataset] === undefined) {
+        await implDie('dcDataLookupById called, but dataset '+dataset+' does not appear to be available.');
+    }
+
+    // Add 1 to account for header row
+    rowNum = rowNum + 1;
+
+    if (rowNum >= dcData[dataset].length) {
+        await implDie('The requested row '+rowNum+' is greater than the number of entries in the ' + dataset + ' dataset ('+dcData[dataset].length+').');
+    }
+
+    strReturn = dcData[dataset][rowNum][fieldNum]; await assertIsStr(strReturn); return strReturn;
+}
+
+async function dcDataLookupByValue(dataset, filterField, genericFilterValue, desiredField) {
+    await assertIsDcDataset(dataset); await assertIsInt(filterField); await assertIsGeneric(genericFilterValue); await assertIsInt(desiredField); let strReturn;
+
+    let intLength = dcData[dataset].length - 2;
+    // start at 1 to skip header row
+    let filterValue = await strFrom(genericFilterValue);
+    for (let row = 1; row <= intLength; row++) {
+        if(dcData[dataset][row][filterField] === filterValue) {
+            strReturn = dcData[dataset][row][desiredField]; await assertIsStr(strReturn); return strReturn;
+        }
+    }
+    //await console.log("SEARCHING", dataset, filterField, genericFilterValue, desiredField, dcData);
+    //await implDie('Could not find required dataset entry by value (parameters: '+dataset+'/'+filterField+'/'+genericFilterValue+'/'+desiredField+').');
+    // TODO: this should be available as a "lookupbyvalue" and a "lookupbyvalueForgiving" versions that do and don't die on this; the forgiving would return the exception UUID.
+    // If nothing was found, return this UUID.
+    strReturn="89315802-d53d-4d11-ba5d-bf505e8ed454"; await assertIsStr(strReturn); return strReturn;
+}
