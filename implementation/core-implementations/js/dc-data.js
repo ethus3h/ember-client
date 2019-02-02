c5d0cd93ddb73fa12dcb7509df4f1592c96036ea
ethus3h <kolubat@gmail.com>
2019-02-02 04:20:33 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/dc-data.js
+++ implementation/core-implementations/js/dc-data.js
@@ -44,19 +44,17 @@ async function dcDataLookupByValue(dataset, filterField, genericFilterValue, des
 async function dcDataFilterByValue(dataset, filterField, genericFilterValue, desiredField) {
     await assertIsDcDataset(dataset); await assertIsInt(filterField); await assertIsGeneric(genericFilterValue); await assertIsInt(desiredField); let asReturn;
 
-    // This routine returns an array of values of the desired column when the filter field matches the filter value. While dcDataLookupByValue gives a single (the first) result, this returns all available results.
+    // This routine returns an array of values of the desired column when the filter field matches the filter value. While dcDataLookupByValue gives a single (the first) result, this returns all matching results.
+
+    asReturn = [];
 
     let intLength = dcData[dataset].length - 2;
     // start at 1 to skip header row
     let filterValue = await strFrom(genericFilterValue);
     for (let row = 1; row <= intLength; row++) {
         if(dcData[dataset][row][filterField] === filterValue) {
-            strReturn = dcData[dataset][row][desiredField]; await assertIsStrArray(asReturn); return asReturn;
+            asReturn = asReturn.concat(dcData[dataset][row][desiredField];
         }
     }
-    //await console.log("SEARCHING", dataset, filterField, genericFilterValue, desiredField, dcData);
-    //await implDie('Could not find required dataset entry by value (parameters: '+dataset+'/'+filterField+'/'+genericFilterValue+'/'+desiredField+').');
-    // TODO: this should be available as a "lookupbyvalue" and a "lookupbyvalueForgiving" versions that do and don't die on this; the forgiving would return the exception UUID.
-    // If nothing was found, return this UUID.
-    strReturn="89315802-d53d-4d11-ba5d-bf505e8ed454"; await assertIsStr(strReturn); return strReturn;
+    await assertIsStrArray(asReturn); return asReturn;
 }
