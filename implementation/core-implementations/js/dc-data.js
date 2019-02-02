7078b15e6e4dff28bf5672d92aa12b32a3f1ec5b
ethus3h <kolubat@gmail.com>
2019-02-02 04:17:03 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/dc-data.js
+++ implementation/core-implementations/js/dc-data.js
@@ -41,24 +41,6 @@ async function dcDataLookupByValue(dataset, filterField, genericFilterValue, des
     strReturn="89315802-d53d-4d11-ba5d-bf505e8ed454"; await assertIsStr(strReturn); return strReturn;
 }
 
-async function dcDataGetRowById(dataset, rowNum) {
-    await assertIsDcDataset(dataset); await assertIsInt(rowNum); let asReturn;
-
-    // This routine returns the value of the specified cell of the nth row in the dataset (zero-indexed, such that the 0th row is the first content row, and the header row is not available (would be -1 but isn't available from this routine)).
-    if (dcData[dataset] === undefined) {
-        await implDie('dcDataLookupById called, but dataset '+dataset+' does not appear to be available.');
-    }
-
-    // Add 1 to account for header row
-    rowNum = rowNum + 1;
-
-    if (rowNum >= dcData[dataset].length) {
-        await implDie('The requested row '+rowNum+' is greater than the number of entries in the ' + dataset + ' dataset ('+dcData[dataset].length+').');
-    }
-
-    asReturn = dcData[dataset][rowNum]; await assertIsStrArray(asReturn); return asReturn;
-}
-
 async function dcDataFilterByValue(dataset, filterField, genericFilterValue, desiredField) {
     await assertIsDcDataset(dataset); await assertIsInt(filterField); await assertIsGeneric(genericFilterValue); await assertIsInt(desiredField); let strReturn;
 
