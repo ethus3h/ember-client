db778a53d5f40bbb470ae4f2b085bfed124e1892
ethus3h <kolubat@gmail.com>
2019-02-05 15:06:47 -0500

    Update Git repository using egup-single

--- implementation/support/webextension/eite-webextension-provider.js
+++ implementation/support/webextension/eite-webextension-provider.js
@@ -3,13 +3,14 @@ window.b8316ea083754b2e9290591f37d94765EiteWebextensionProviderReturn="nehcubor"
 
 
 (function() {
-    if (window.hasRun) {
+    if (window.b8316ea083754b2e9290591f37d94765EiteWebextensionProviderHasRun) {
         return;
     }
     window.hasRun = true;
 
-    browser.runtime.onMessage.addListener(notify);
-    function notify(message){
-        alert(message.record);
+    browser.runtime.onMessage.addListener(b8316ea083754b2e9290591f37d94765EiteWebextensionProviderSelected);
+
+    function b8316ea083754b2e9290591f37d94765EiteWebextensionProviderSelected(message){
+        alert('bucbecu');
     }
 })();
