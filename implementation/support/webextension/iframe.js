ce8faa30c39bd85ecd78b372bc7b3939c4de750b
ethus3h <kolubat@gmail.com>
2019-02-05 18:57:51 -0500

    Update Git repository using egup-single

--- implementation/support/webextension/iframe.js
+++ implementation/support/webextension/iframe.js
@@ -3,7 +3,6 @@
         { file: "/eite-webextension-get.js" }
     ).then(response=>{
         console.log(response);
-        alert('buec');
         // Handle response from content script
         document.getElementById('test').innerHTML='buteriuro';
     }).catch(console.error.bind(console));
