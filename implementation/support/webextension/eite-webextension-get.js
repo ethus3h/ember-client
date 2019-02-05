55a0a5f6247538e33118bfd23d49079d2f568eb4
ethus3h <kolubat@gmail.com>
2019-02-05 18:56:14 -0500

    Update Git repository using egup-single

--- implementation/support/webextension/eite-webextension-get.js
+++ implementation/support/webextension/eite-webextension-get.js
@@ -1,4 +1,3 @@
-    alert(',');
 console.log('Bubecicucecice');
 b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetResponse=[];
 b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetResponse=document.activeElement.innerHTML;
