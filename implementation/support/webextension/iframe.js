84a191e2624cc6404d62bdc12e15747afb999acc
ethus3h <kolubat@gmail.com>
2019-02-05 17:27:16 -0500

    Update Git repository using egup-single

--- implementation/support/webextension/iframe.js
+++ implementation/support/webextension/iframe.js
@@ -1,5 +1,5 @@
 browser.tabs.executeScript(
-    { file: "eite-webextension-provider.js"}
+    { file: "/eite-webextension-provider.js"}
 ).then(function () {
     browser.tabs.sendMessage({record: "start"}) 
 }).catch(console.error.bind(console));
