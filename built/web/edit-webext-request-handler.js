2c3320013d60f4a610a7faffeae8094068f1beee
ethus3h <kolubat@gmail.com>
2019-02-05 20:42:13 -0500

    Update Git repository using egup-single

--- built/web/edit-webext-request-handler.js
+++ built/web/edit-webext-request-handler.js
@@ -1,17 +1,16 @@
 window.addEventListener('message', async function(message) {
     let mutobs=new MutationObserver(function(e){
         if (e[0].removedNodes) {
-            
+            if (message.data[0] === 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessage') {
+                canEdit=message.data[1];
+                contents=message.data[2];
+                console.log(message.data);
+                if (!canEdit) {
+                    openAlertDialog('The requested content is read-only.');
+                }
+                let utf8encoder = new TextEncoder();
+                document.getElementById('inputarea').value = await strFromByteArray(await importAndExport('ascii', 'integerList', new Uint8Array(utf8encoder.encode(contents))));
+            }
         }
     })
-    if (message.data[0] === 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessage') {
-        canEdit=message.data[1];
-        contents=message.data[2];
-        console.log(message.data);
-        if (!canEdit) {
-            openAlertDialog('The requested content is read-only.');
-        }
-        let utf8encoder = new TextEncoder();
-        document.getElementById('inputarea').value = await strFromByteArray(await importAndExport('ascii', 'integerList', new Uint8Array(utf8encoder.encode(contents))));
-    }
 });
