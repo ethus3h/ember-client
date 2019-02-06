a188b3e93ecbd039287ffb4c55a75e906eac17ba
ethus3h <kolubat@gmail.com>
2019-02-05 21:42:49 -0500

    Update Git repository using egup-single

--- built/edit-webextension/edit-webext-request-handler.js
+++ built/edit-webextension/edit-webext-request-handler.js
@@ -1,32 +1,32 @@
 window.addEventListener('message', function(message) {
     function onRemove(element, onDetachCallback) {
         // https://stackoverflow.com/questions/31798816/simple-mutationobserver-version-of-domnoderemovedfromdocument
-        const observer = new MutationObserver(function () {
-            function isDetached(el) {
-                console.log(el);
-                if (el.parentNode === document) {
-                    return false;
-                } else if (el.parentNode === null) {
-                    return true;
-                } else {
-                    isDetached(el.parentNode);
+        if (element !== null) {
+            const observer = new MutationObserver(function () {
+                function isDetached(el) {
+                    console.log(el);
+                    if (el.parentNode === document) {
+                        return false;
+                    } else if (el.parentNode === null) {
+                        return true;
+                    } else {
+                        isDetached(el.parentNode);
+                    }
                 }
-            }
-            if (isDetached(element)) {
-                console.log('bubeiccecuc');
-                observer.disconnect();
-                onDetachCallback();
-            }
-        });
+                if (isDetached(element)) {
+                    observer.disconnect();
+                    onDetachCallback();
+                }
+            });
 
-        observer.observe(document, {
-            childList: true,
-            subtree: true
-        });
+            observer.observe(document, {
+                childList: true,
+                subtree: true
+            });
+        }
     }
 
     async function eiteReadyCallback(message) {
-        console.log(message);
         if (message.data[0] === 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessage') {
             startSpinner();
             window.setTimeout(async function(){
@@ -39,11 +39,10 @@ window.addEventListener('message', function(message) {
                 let utf8encoder = new TextEncoder();
                 document.getElementById('inputarea').value = await strFromByteArray(await importAndExport('ascii', 'integerList', new Uint8Array(utf8encoder.encode(contents))));
                 removeSpinner();
+                RunDocumentHandler();
             }, 500);
         }
     };
-
-    console.log(message);
 
     onRemove(document.getElementById('overlay'), function() {
         eiteReadyCallback(message);
