f17a949aae1bf8f6a5f3d445ea519cceef904ea1
ethus3h <kolubat@gmail.com>
2019-02-05 20:30:10 -0500

    Update Git repository using egup-single

--- implementation/platform-support/web/edit-webext-request-handler.js
+++ implementation/platform-support/web/edit-webext-request-handler.js
@@ -1,5 +1,8 @@
 window.addEventListener('message', function(message) {
     canEdit=message[0];
     contents=message[1];
-    console.log(message);
+    if (!canEdit) {
+        openAlertDialog('The requested content is read-only.');
+    }
+    document.getElementById('inputarea').value = await strFromByteArray(await importAndExport('ascii', 'integerList', new Uint8Array(fr.result)));
 });
