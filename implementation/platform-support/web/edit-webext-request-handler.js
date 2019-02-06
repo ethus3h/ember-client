d306bf5fc10cce0ca0aeb5969a790ecbc0fbc9eb
ethus3h <kolubat@gmail.com>
2019-02-05 20:41:34 -0500

    Update Git repository using egup-single

--- implementation/platform-support/web/edit-webext-request-handler.js
+++ implementation/platform-support/web/edit-webext-request-handler.js
@@ -1,4 +1,9 @@
 window.addEventListener('message', async function(message) {
+    let mutobs=new MutationObserver(function(e){
+        if (e[0].removedNodes) {
+            
+        }
+    })
     if (message.data[0] === 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessage') {
         canEdit=message.data[1];
         contents=message.data[2];
