9f1c1686305fddc56e77af4f2726f2c774e35f44
ethus3h <kolubat@gmail.com>
2019-02-05 20:27:58 -0500

    Update Git repository using egup-single

--- built/edit-webextension/iframe.js
+++ built/edit-webextension/iframe.js
@@ -1,6 +1,7 @@
 window.onload=(async function(){
     if (typeof browser !== 'undefined') {
         // Running as a WebExtension
+        await setupIfNeeded();
         browser.tabs.executeScript(
             { file: "/eite-webextension-get.js" }
         ).then(response=>{
