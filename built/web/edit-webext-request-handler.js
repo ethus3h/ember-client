8840c4714b4a5a80e3cf9bebd910f4d8b6e990c3
ethus3h <kolubat@gmail.com>
2019-02-05 20:09:01 -0500

    Update Git repository using egup-single

--- built/web/edit-webext-request-handler.js
+++ built/web/edit-webext-request-handler.js
@@ -1,4 +1,5 @@
 window.addEventListener('message', function(message) {
-    canEdit=response[0];
-    contents=response[1];
+    canEdit=message[0];
+    contents=message[1];
+    console.log(message);
 });
