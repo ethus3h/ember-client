4eb6a1b358af84fddc09d8fc7598ef1b019f64ce
ethus3h <kolubat@gmail.com>
2019-02-05 19:51:28 -0500

    Update Git repository using egup-single

---
+++ built/edit-webextension/iframe-nonframed.js
@@ -0,0 +1,12 @@
+(async function(){
+    browser.tabs.executeScript(
+        { file: "/eite-webextension-get.js" }
+    ).then(response=>{
+        // Handle response from content script
+        canEdit=response[0];
+        contents=response[1];
+        iframe=Document.getElementById('eiteEditToolFrame');
+        iframe.contentWindow.postMessage(response);
+        console.log(response);
+    }).catch(console.error.bind(console));
+})();
