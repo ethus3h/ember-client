016ce85b731e0506a6f52d0bb8c1f2c82b9305e3
ethus3h <kolubat@gmail.com>
2019-02-05 19:32:19 -0500

    Update Git repository using egup-single

--- built/edit-webextension/iframe.js
+++ built/edit-webextension/iframe.js
@@ -5,6 +5,7 @@
         // Handle response from content script
         canEdit=response[0];
         contents=response[1];
+        iframe=Document.getElementById('eiteEditToolFrame');
         console.log(response);
     }).catch(console.error.bind(console));
 })();
