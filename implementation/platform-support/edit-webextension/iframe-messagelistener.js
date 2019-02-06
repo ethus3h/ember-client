ebcc19ccf34b4adab2f9667b61bba04f3b5ef20d
ethus3h <kolubat@gmail.com>
2019-02-05 23:03:48 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/iframe-messagelistener.js
+++ implementation/platform-support/edit-webextension/iframe-messagelistener.js
@@ -7,13 +7,6 @@ window.addEventListener('message', function(message) {
                     iframe=Document.getElementById('eiteEditToolFrame');
                     iframe.contentWindow.postMessage(response);
                 }).catch(console.error.bind(console));
-
-        })
-    }
+            }
+    )}
 );
-    browser.tabs.sendMessage(message).then(response=>{
-        // Handle response from content script
-        iframe=Document.getElementById('eiteEditToolFrame');
-        iframe.contentWindow.postMessage(response);
-    }).catch(console.error.bind(console));
-});
