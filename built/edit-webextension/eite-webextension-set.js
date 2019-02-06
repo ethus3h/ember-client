fb4a681d4b1200e87bfd517f1059b02d03ea2fcf
ethus3h <kolubat@gmail.com>
2019-02-05 19:25:43 -0500

    Update Git repository using egup-single

---
+++ built/edit-webextension/eite-webextension-set.js
@@ -0,0 +1,8 @@
+(function() {
+    browser.runtime.onMessage.addListener(b8316ea083754b2e9290591f37d94765EiteWebextensionProviderMessage => { b8316ea083754b2e9290591f37d94765EiteWebextensionProvider(b8316ea083754b2e9290591f37d94765EiteWebextensionProviderMessage) });
+
+    function b8316ea083754b2e9290591f37d94765EiteWebextensionProvider(message){
+        b8316ea083754b2e9290591f37d94765EiteWebextensionProviderResponse='Bebibeci';
+    }
+})();
+b8316ea083754b2e9290591f37d94765EiteWebextensionProviderResponse;
