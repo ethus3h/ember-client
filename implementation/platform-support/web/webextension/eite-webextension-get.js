2f1e18850d6408aaff9f869563591c4283dc583b
ethus3h <kolubat@gmail.com>
2019-02-05 19:21:11 -0500

    Update Git repository using egup-single

--- implementation/platform-support/web/webextension/eite-webextension-get.js
+++
@@ -1,8 +0,0 @@
-b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetResponse=[];
-b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem=document.activeElement;
-if ((b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem instanceof HTMLInputElement && (b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem.type == 'text' || b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem.type == 'search')) || (b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem instanceof HTMLTextAreaElement)) {
-    b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetResponse=[true, document.activeElement.value];
-}
-else {
-    b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetResponse=[false, document.activeElement.innerHTML];
-}b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetResponse;
