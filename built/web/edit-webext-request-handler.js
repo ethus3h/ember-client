ae7f464fed6351c49195f21a6502135c2b5d1443
ethus3h <kolubat@gmail.com>
2019-02-05 21:01:31 -0500

    Update Git repository using egup-single

--- built/web/edit-webext-request-handler.js
+++ built/web/edit-webext-request-handler.js
@@ -41,5 +41,5 @@ window.addEventListener('message', function(message) {
 
     console.log(message);
 
-    onRemove(document.getElementById('overlay'), function(){ alert('d');eiteReadyCallback(message); });
+    onRemove(document.getElementById('overlay'), function() {alert('d')}, function(){ alert('d');eiteReadyCallback(message); });
 });
