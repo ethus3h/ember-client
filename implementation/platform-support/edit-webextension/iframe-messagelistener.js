39af9a4f157939604a3daa88433ce06a2ffa0f60
ethus3h <kolubat@gmail.com>
2019-02-05 23:02:22 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/iframe-messagelistener.js
+++ implementation/platform-support/edit-webextension/iframe-messagelistener.js
@@ -1,11 +1,15 @@
 window.addEventListener('message', function(message) {
-    chrome.tabs.query(
+    browser.tabs.query(
     { currentWindow: true, active: true },
     function (tabArray) {
-        chrome.tabs.executeScript(tabArray[0].id, {
+        browser.tabs.executeScript(, {
             file: 'src/js/scripts/extractCSS.js'
          }, function() {
-            chrome.tabs.sendMessage(this.props.source);
+            browser.tabs.sendMessage(tabArray[0].id, message).then(response=>{
+        // Handle response from content script
+        iframe=Document.getElementById('eiteEditToolFrame');
+        iframe.contentWindow.postMessage(response);
+    }).catch(console.error.bind(console));;
 
         })
     }
