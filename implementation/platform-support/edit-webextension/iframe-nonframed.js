64a720e2f7b8f87d5971512a61b9e6226379f51d
ethus3h <kolubat@gmail.com>
2019-02-06 00:48:28 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/iframe-nonframed.js
+++ implementation/platform-support/edit-webextension/iframe-nonframed.js
@@ -1,7 +1,7 @@
 window.onload=(async function(){
     if (typeof browser === 'undefined') {
         // Not running as a WebExtension
-        response=['b8316ea083754b2e9290591f37d94765EiteWebextensionMessage', true, " <input class=\"nav-trigger\" id=\"nav-trigmain>  "]
+        response=['b8316ea083754b2e9290591f37d94765EiteWebextensionMessage', true, " <input class=\"nav-trigger\" id=\"nav-trigmain>  ", document.baseURI]
         iframe=document.getElementById('eiteEditToolFrame');
         iframe.contentWindow.postMessage(response, iframe.src);
     }
