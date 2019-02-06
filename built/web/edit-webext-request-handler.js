c9e21a37eae7488ecc453817a55ac277cedac7ad
ethus3h <kolubat@gmail.com>
2019-02-05 21:40:40 -0500

    Update Git repository using egup-single

--- built/web/edit-webext-request-handler.js
+++ built/web/edit-webext-request-handler.js
@@ -28,14 +28,18 @@ window.addEventListener('message', function(message) {
     async function eiteReadyCallback(message) {
         console.log(message);
         if (message.data[0] === 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessage') {
-            canEdit=message.data[1];
-            contents=message.data[2];
-            console.log(message.data);
-            if (!canEdit) {
-                openAlertDialog('The requested content is read-only.');
-            }
-            let utf8encoder = new TextEncoder();
-            document.getElementById('inputarea').value = await strFromByteArray(await importAndExport('ascii', 'integerList', new Uint8Array(utf8encoder.encode(contents))));
+            startSpinner();
+            window.setTimeout(async function(){
+                canEdit=message.data[1];
+                contents=message.data[2];
+                console.log(message.data);
+                if (!canEdit) {
+                    openAlertDialog('The requested content is read-only.');
+                }
+                let utf8encoder = new TextEncoder();
+                document.getElementById('inputarea').value = await strFromByteArray(await importAndExport('ascii', 'integerList', new Uint8Array(utf8encoder.encode(contents))));
+                removeSpinner();
+            }, 500);
         }
     };
 
