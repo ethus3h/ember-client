f2991dee16e25ef184481f2e745b29e5ee7342dd
ethus3h <kolubat@gmail.com>
2019-02-06 02:17:53 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/eite-webextension-get.js
+++ implementation/platform-support/edit-webextension/eite-webextension-get.js
@@ -1,3 +1,5 @@
+// I swear this file isn't obfuscated. Replace b8316ea083754b2e9290591f37d94765EiteWebextensionProvider with empty string to make it more readable. Doing that would cause problems though since all this gets dumped into the window namespace of random Web pages.
+
 // Don't use let since it will persist between executions of this script.
 b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetResponse=[];
 function b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetSelectionText() {
@@ -18,6 +20,7 @@ function b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetSelectionTex
 }
 
 b8316ea083754b2e9290591f37d94765EiteWebextensionProviderSelectionText=b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetSelectionText();
+b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem=document.activeElement;
 
 if (b8316ea083754b2e9290591f37d94765EiteWebextensionProviderSelectionText.length > 0) {
     if ((b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem instanceof HTMLInputElement && (b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem.type == 'text' || b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem.type == 'search')) || (b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem instanceof HTMLTextAreaElement)) {
@@ -27,8 +30,6 @@ if (b8316ea083754b2e9290591f37d94765EiteWebextensionProviderSelectionText.length
         b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetResponse=['b8316ea083754b2e9290591f37d94765EiteWebextensionMessage', false, b8316ea083754b2e9290591f37d94765EiteWebextensionProviderSelectionText];
     }
 }
-
-b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem=document.activeElement;
 
 if ((b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem instanceof HTMLInputElement && (b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem.type == 'text' || b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem.type == 'search')) || (b8316ea083754b2e9290591f37d94765EiteWebextensionProviderTempElem instanceof HTMLTextAreaElement)) {
     b8316ea083754b2e9290591f37d94765EiteWebextensionProviderGetResponse=['b8316ea083754b2e9290591f37d94765EiteWebextensionMessage', true, document.activeElement.value];
