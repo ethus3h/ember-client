59938d28719230fe5f9fb6112fb8cd3f0266bf07
ethus3h <kolubat@gmail.com>
2019-02-06 01:31:54 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/framehost.js
+++ implementation/platform-support/edit-webextension/framehost.js
@@ -1,6 +1,8 @@
+        console.log('makewinnonload');
 window.onload=(async function(){
     if (typeof browser === 'undefined') {
         // Not running as a WebExtension
+        console.log('setupnwe');
         response=['b8316ea083754b2e9290591f37d94765EiteWebextensionMessage', true, " <input class=\"nav-trigger\" id=\"nav-trigmain>  ", document.baseURI]
         iframe=document.getElementById('eiteEditToolFrame');
         iframe.contentWindow.postMessage(response, iframe.src);
