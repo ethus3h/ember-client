7e843767dd1ab5880f08908586bd329dd8ea35b2
ethus3h <kolubat@gmail.com>
2019-02-05 20:45:10 -0500

    Update Git repository using egup-single

--- built/web/edit-webext-request-handler.js
+++ built/web/edit-webext-request-handler.js
@@ -1,4 +1,29 @@
 window.addEventListener('message', async function(message) {
+    
+https://stackoverflow.com/questions/31798816/simple-mutationobserver-version-of-domnoderemovedfromdocument
+function onRemove(element, onDetachCallback) {
+    const observer = new MutationObserver(function () {
+        function isDetached(el) {
+            if (el.parentNode === document) {
+                false;
+            } else if (el.parentNode === null) {
+                true;
+            } else {
+                isDetached(el.parentNode);
+            }
+        }
+
+        if (isDetached(element)) {
+            observer.disconnect();
+            onDetachCallback();
+        }
+    })
+
+    observer.observe(document, {
+         childList: true,
+         subtree: true
+    })
+}
     let mutobs=new MutationObserver(function(e){
         if (e[0].removedNodes) {
             if (message.data[0] === 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessage') {
@@ -12,5 +37,6 @@ window.addEventListener('message', async function(message) {
                 document.getElementById('inputarea').value = await strFromByteArray(await importAndExport('ascii', 'integerList', new Uint8Array(utf8encoder.encode(contents))));
             }
         }
-    })
+    });
+    mutobs.observe(document.getElementById('overlay'))
 });
