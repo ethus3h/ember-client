530444e6d4a07d63ef3e05947df2aef701a25976
ethus3h <kolubat@gmail.com>
2019-02-06 03:08:00 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/framehost-message-listener.js
+++ implementation/platform-support/edit-webextension/framehost-message-listener.js
@@ -1,10 +1,11 @@
 window.addEventListener('message', function(message) {
     if (message.data[0] === 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessage') {
+        window.b8316ea083754b2e9290591f37d94765EiteWebextensionMessageData=message.data;
         // Pass edited data back to content script
         browser.tabs.query(
             { currentWindow: true, active: true },
             function (tabArray) {
-                browser.tabs.sendMessage(tabArray[0].id, message.data).then(response=>{
+                browser.tabs.sendMessage(tabArray[0].id, window.b8316ea083754b2e9290591f37d94765EiteWebextensionMessageData).then(response=>{
                     // Handle response from content script
                     window.close();
                 }).catch(console.error.bind(console));
