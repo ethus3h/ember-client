accec65ec9909e66bd007332555046024f23bd95
ethus3h <kolubat@gmail.com>
2019-02-06 01:20:49 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/iframe-nonframed.js
+++ implementation/platform-support/edit-webextension/iframe-nonframed.js
@@ -5,4 +5,18 @@ window.onload=(async function(){
         iframe=document.getElementById('eiteEditToolFrame');
         iframe.contentWindow.postMessage(response, iframe.src);
     }
-});
+else {
+            // Running as a WebExtension
+        await setupIfNeeded();
+        console.log('setup');
+        browser.tabs.executeScript(
+            { file: "/eite-webextension-get.js" }
+        ).then(response=>{
+            // Handle response from content script
+            response[3]=document.getElementById('eiteEditToolFrame').src;
+            iframe=Document.getElementById('eiteEditToolFrame');
+            console.log(response);
+            iframe.contentWindow.postMessage(response);
+        }).catch(console.error.bind(console));
+
+}});
