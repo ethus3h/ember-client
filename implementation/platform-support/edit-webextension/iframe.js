c33ae6e8fc799be151e1d9ad14577fda0d7baae2
ethus3h <kolubat@gmail.com>
2019-02-05 20:07:29 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/iframe.js
+++ implementation/platform-support/edit-webextension/iframe.js
@@ -1,14 +1,16 @@
-(async function(){
-    if (typeof browser !== 'undefined') {
-        // Running as a WebExtension
-        browser.tabs.executeScript(
-            { file: "/eite-webextension-get.js" }
-        ).then(response=>{
-            // Handle response from content script
-            canEdit=response[0];
-            contents=response[1];
-            iframe=Document.getElementById('eiteEditToolFrame');
-            iframe.contentWindow.postMessage(response);
-        }).catch(console.error.bind(console));
-    }
-})();
+window.onload=(function(){
+    (async function(){
+        if (typeof browser !== 'undefined') {
+            // Running as a WebExtension
+            browser.tabs.executeScript(
+                { file: "/eite-webextension-get.js" }
+            ).then(response=>{
+                // Handle response from content script
+                canEdit=response[0];
+                contents=response[1];
+                iframe=Document.getElementById('eiteEditToolFrame');
+                iframe.contentWindow.postMessage(response);
+            }).catch(console.error.bind(console));
+        }
+    })();
+});
