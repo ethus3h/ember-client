dbf3dafb69124665fabab23f347455b2a03b6984
ethus3h <kolubat@gmail.com>
2019-02-07 14:34:17 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/bits.js
+++ implementation/core-implementations/js/bits.js
@@ -5,7 +5,7 @@
 
 // Note that bitwise operations in StageL operate on bytes rather than int32s. Consequently, C-style 8-bit bitwise operations must be emulated for the Javascript implementation. That said, C-style bitwise operators depend on the types being operated upon. So, there should probably be a set of functions like bitLshift8, bitLshift32, etc. maybe. Do these really make much sense in StageL, which is mostly higher-level? How would one implement these in languages that don't provide them natively? Mmh.
 
-async function bitAnd(byteA, byteB) {
+async function bitAnd8(byteA, byteB) {
     await assertIsByte(byteA); await assertIsByte(byteB); let byteReturn;
 
     byteReturn = await internalBitwiseMask(byteA & byteB);
@@ -13,14 +13,14 @@ async function bitAnd(byteA, byteB) {
     await assertIsByte(byteReturn); return byteReturn;
 }
 
-async function bitNot(byteA) {
+async function bitNot8(byteA) {
     await assertIsByte(byteA); let byteReturn;
 
     byteReturn = await internalBitwiseMask(~byteA);
     await assertIsByte(byteReturn); return byteReturn;
 }
 
-async function bitLshift(byteA, intPlaces) {
+async function bitLshift8(byteA, intPlaces) {
     await assertIsByte(byteA); await assertIsInt(intPlaces); let byteReturn;
 
     await assertIsBetween(intPlaces, 0, 8);
@@ -30,7 +30,7 @@ async function bitLshift(byteA, intPlaces) {
     await assertIsByte(byteReturn); return byteReturn;
 }
 
-async function bitRshift(byteA, intPlaces) {
+async function bitRshift8(byteA, intPlaces) {
     await assertIsByte(byteA); await assertIsInt(intPlaces); let byteReturn;
 
     await assertIsBetween(intPlaces, 0, 8);
