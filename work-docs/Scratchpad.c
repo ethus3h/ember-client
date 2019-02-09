f50eed5135fcdc791c01bb7eceefd1a60e061cf1
ethus3h <kolubat@gmail.com>
2019-02-09 05:39:52 -0500

    Update Git repository using egup-single

--- work-docs/Scratchpad.c
+++ work-docs/Scratchpad.c
@@ -1,18 +1,21 @@
+#include <stdio.h>
 
-    #include <stdio.h>
 unsigned long pwrOf2(int n) {
     return 1 << n;
 }
-void printBits(unsigned int num){
+
+void printBits(unsigned int num) {
     unsigned int size = sizeof(unsigned int);
     unsigned int maxPow = 1<<(size*8-1);
     printf("MAX POW : %u\n",maxPow);
-    int i=0,j;for(;i<size*8;++i){
-    // print last bit and shift left.
-    printf("%u ",num&maxPow ? 1 : 0);
-    num = num<<1;
+    int i=0,j;
+    for(;i<size*8;++i){
+        // print last bit and shift left.
+        printf("%u ",num&maxPow ? 1 : 0);
+        num = num<<1;
+    }
 }
-}
+
 int main() {
   unsigned char a;
   a=255;
