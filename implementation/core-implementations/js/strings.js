16a5b2bcf9e7efd99f103e7588ccf97b716834f9
ethus3h <kolubat@gmail.com>
2019-02-10 13:28:15 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/strings.js
+++ implementation/core-implementations/js/strings.js
@@ -6,18 +6,18 @@
 async function implCat(strA, strB) {
     assertIsStr(strA); assertIsStr(strB); let strReturn;
 
-    strReturn = strA + "" + strB; await assertIsStr(strReturn); return strReturn;
+    strReturn = strA + "" + strB; return strReturn;
 }
 
 async function substring(str, intStart, intLength) {
     assertIsStr(str); assertIsInt(intStart); assertIsInt(intLength); let strReturn;
 
-    strReturn = str.substring(intStart, intStart + intLength); await assertIsStr(strReturn); return strReturn;
+    strReturn = str.substring(intStart, intStart + intLength); return strReturn;
 }
 
 async function len(str) {
     assertIsStr(str); let intReturn;
 
-    intReturn = str.length; await assertIsInt(intReturn); return intReturn;
+    intReturn = str.length; return intReturn;
 }
 
