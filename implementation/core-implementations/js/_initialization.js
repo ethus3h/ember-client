27f0ccb58bcab34f2e440bbc902958db62d9f08a
ethus3h <kolubat@gmail.com>
2019-02-11 13:12:27 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/_initialization.js
+++ implementation/core-implementations/js/_initialization.js
@@ -228,6 +228,7 @@ if (typeof window !== 'undefined') {
         };
         window.eiteWorker.onmessage = function(message) {
             const {uuid, msgid, res} = message.data;
+            if (uuid === 'b8316ea083754b2e9290591f37d94765EiteWebworkerResponse') {
             if (res || res === null) {
                 let resolveCallback;
                 resolveCallback = window.eiteWorkerResolveCallbacks[id];
@@ -292,10 +293,10 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
             args=[];
         }
         self.eiteWorkerHostCallID = self.eiteWorkerHostCallID + 1;
-        let thisCallId=self.eiteWorkerCallID;
+        let thisCallId=self.eiteWorkerHostCallID;
         let thisCall={uuid: 'b8316ea083754b2e9290591f37d94765EiteWebworkerHostRequest', msgid: thisCallId, args: [funcName, args]};
         return new Promise(function(resolve) {
-            self.eiteWorkerResolveCallbacks[thisCallId]=resolve;
+            self.eiteWorkerHostResolveCallbacks[thisCallId]=resolve;
             self.postMessage(thisCall);
         });
     };
