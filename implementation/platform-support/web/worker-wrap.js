32fd00614b218fbb95e4451ee99fd65e6e398af9
ethus3h <kolubat@gmail.com>
2019-02-11 10:30:39 -0500

    Update Git repository using egup-single

--- implementation/platform-support/web/worker-wrap.js
+++ implementation/platform-support/web/worker-wrap.js
@@ -1,9 +1,14 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
 if (window.Worker) {
-    let eiteWorker = new Worker('eite.js');
-    window.eiteCall = async function() {
-        return;
+    let window.eiteWorker = new Worker('eite.js');
+    window.eiteCall = async function(funcName, args) {
+        await new Promise(resolve => {
+                fr.onload=function (){
+                    resolve(undefined);
+                };
+                fr.readAsArrayBuffer(file);
+            });
     }
 }
 else {
