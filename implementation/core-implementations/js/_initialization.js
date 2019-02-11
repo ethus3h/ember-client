73b47d5e18845e2918fdbe8b2b26b6952380aae4
ethus3h <kolubat@gmail.com>
2019-02-11 11:56:15 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/_initialization.js
+++ implementation/core-implementations/js/_initialization.js
@@ -208,43 +208,45 @@ async function internalLoadDatasets() {
 
 // Promise-wrapped worker strategy is inspired by Gilad Dayagi's implementation described at https://codeburst.io/promises-for-the-web-worker-9311b7831733
 
-if (window.Worker) {
-    window.eiteWorker = new Worker('eite.js');
-    window.eiteWorkerResolveCallbacks = {};
-    window.eiteWorkerCallID = 0;
-    window.eiteCall = async function(funcName, args) {
-        window.eiteWorkerCallID = window.eiteWorkerCallID + 1;
-        let thisCallId=window.eiteWorkerCallID;
-        let thisCall={uuid: 'b8316ea083754b2e9290591f37d94765EiteWebworkerRequest', msgid: thisCallId, args: [funcName, args]};
-        return new Promise(function(resolve) {
-            window.eiteWorkerResolveCallbacks[thisCallId]=resolve;
-            window.eiteWorker.postMessage(thisCall);
-        });
-    };
-    window.eiteWorker.onmessage = function(message) {
-        const {uuid, msgid, res} = message.data;
-        if (res) {
-            let resolveCallback;
-            resolveCallback = window.eiteWorkerResolveCallbacks[id];
-            if (resolveCallback) {
-                resolveCallback(res);
-                delete window.eiteWorkerResolveCallbacks[id];
+if (window) {
+    // Not running as a Web worker
+    if (window.Worker) {
+        window.eiteWorker = new Worker('eite.js');
+        window.eiteWorkerResolveCallbacks = {};
+        window.eiteWorkerCallID = 0;
+        window.eiteCall = async function(funcName, args) {
+            window.eiteWorkerCallID = window.eiteWorkerCallID + 1;
+            let thisCallId=window.eiteWorkerCallID;
+            let thisCall={uuid: 'b8316ea083754b2e9290591f37d94765EiteWebworkerRequest', msgid: thisCallId, args: [funcName, args]};
+            return new Promise(function(resolve) {
+                window.eiteWorkerResolveCallbacks[thisCallId]=resolve;
+                window.eiteWorker.postMessage(thisCall);
+            });
+        };
+        window.eiteWorker.onmessage = function(message) {
+            const {uuid, msgid, res} = message.data;
+            if (res) {
+                let resolveCallback;
+                resolveCallback = window.eiteWorkerResolveCallbacks[id];
+                if (resolveCallback) {
+                    resolveCallback(res);
+                    delete window.eiteWorkerResolveCallbacks[id];
+                }
+                else {
+                    implDie('Web worker returned invalid message ID.');
+                    throw 'Web worker returned invalid message ID.';
+                }
             }
             else {
-                implDie('Web worker returned invalid message ID.');
-                throw 'Web worker returned invalid message ID.';
+                implDie('Web worker encountered an error.');
+                throw 'Web worker encountered an error.';
             }
+        };
+    }
+    else {
+        window.eiteCall = async function(funcName, args) {
+            return await window[funcName]( ...args );
         }
-        else {
-            implDie('Web worker encountered an error.');
-            throw 'Web worker encountered an error.';
-        }
-    };
-}
-
-else {
-    window.eiteCall = async function(funcName, args) {
-        return await window[funcName]( ...args );
     }
 }
 
