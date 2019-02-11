223a970896e24ed87ef2a458d31f773ec2aaf0f7
ethus3h <kolubat@gmail.com>
2019-02-11 16:21:44 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/_initialization.js
+++ implementation/core-implementations/js/_initialization.js
@@ -286,7 +286,6 @@ if (typeof window !== 'undefined') {
                 window.eiteHostRequestInternalOnMessage(message);
             }
             else if (uuid === 'b8316ea083754b2e9290591f37d94765EiteWebworkerError') {
-                console.log(message);
                 implDie('Web worker encountered an error: '+res+'.');
                 throw 'Web worker encountered an error: '+res+'.';
             }
@@ -321,7 +320,7 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
             res = await self[args[0]]( ...args[1] );
         }
         catch(error) {
-            self.postMessage({uuid: 'b8316ea083754b2e9290591f37d94765EiteWebworkerError', msgid: msgid, val: error.message});
+            self.postMessage({uuid: 'b8316ea083754b2e9290591f37d94765EiteWebworkerError', msgid: msgid, res: error.message});
             throw error;
         }
         if (!res) {
