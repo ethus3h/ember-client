4964917a5bac22ecccbd4987cc113ed56a8b5c1f
ethus3h <kolubat@gmail.com>
2019-02-11 16:10:33 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -348,6 +348,7 @@ if (typeof window !== 'undefined') {
         }
         window.eiteWorker.onmessage = function(message) {
             const {uuid, msgid, res} = message.data;
+            console.log(message);
             if (uuid === 'b8316ea083754b2e9290591f37d94765EiteWebworkerResponse') {
                 if (res || res === null) {
                     let resolveCallback;
