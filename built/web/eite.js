9904795a91a02f38bd5f402deed88303c7565f6d
ethus3h <kolubat@gmail.com>
2019-02-11 12:15:39 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -3431,7 +3431,7 @@ async function dcaFromSems(intArrayContent) {
 
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
-registerSpeedup(name, func) {
+function registerSpeedup(name, func) {
     if (typeof window !== 'undefined') {
         window[name] = func;
     }
