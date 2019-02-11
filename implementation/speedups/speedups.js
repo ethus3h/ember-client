b7b581bc0ab528bfd6b1b3609b0e776db4159a5c
ethus3h <kolubat@gmail.com>
2019-02-11 12:07:11 -0500

    Update Git repository using egup-single

--- implementation/speedups/speedups.js
+++ implementation/speedups/speedups.js
@@ -14,35 +14,35 @@ registerSpeedup('assertIsFalse', async function (bool) {
     await assertionFailed(bool+' is true, but should be false.');
 });
 
-window.assertIsTrue', async function (bool) {
+registerSpeedup('assertIsTrue', async function (bool) {
     if (bool === true) {
         return;
     }
     await assertionFailed(bool+' is not true.');
 });
 
-window.assertIsDc', async function (v) {
+registerSpeedup('assertIsDc', async function (v) {
     if (await Number.isInteger(v) && v >= 0 && v <= 2147483647) {
         return true;
     }
     await assertIsTrue(false);
 });
 
-window.assertIsDcDataset', async function (str) {
+registerSpeedup('assertIsDcDataset', async function (str) {
     if (datasets.includes(str)) {
         return;
     }
     await assertIsTrue(false);
 });
 
-window.or', async function (a,b) {
+registerSpeedup('or', async function (a,b) {
     if (typeof a === 'boolean' && typeof b === 'boolean') {
         return a || b;
     }
     await assertIsBool(a); await assertIsBool(b);
 });
 
-window.isTrue', async function (bool) {
+registerSpeedup('isTrue', async function (bool) {
     if (bool === true) {
         // Can't simplify to if(bool) because non-bools might evaluate to true and give wrong result
         return true;
@@ -50,14 +50,14 @@ window.isTrue', async function (bool) {
     return false;
 });
 
-window.isFalse', async function (bool) {
+registerSpeedup('isFalse', async function (bool) {
     if (bool === false) {
         return true;
     }
     return false;
 });
 
-window.isIntArray', async function (val) {
+registerSpeedup('isIntArray', async function (val) {
     if (val === undefined) {
         await assertionFailed('isGenericArray called with non-StageL-supported argument type.'); /* Claim to fail the isGenericArray assertion here, because that's what would get called in the portable implementation. */
     }
@@ -73,7 +73,7 @@ window.isIntArray', async function (val) {
     return val.every(isIntSync);
 });
 
-window.assertIsIntArray', async function (val) {
+registerSpeedup('assertIsIntArray', async function (val) {
     if (val === undefined) {
         await assertionFailed('isGenericArray called with non-StageL-supported argument type.'); /* Claim to fail the isGenericArray assertion here, because that's what would get called in the portable implementation. */
     }
@@ -94,19 +94,19 @@ window.assertIsIntArray', async function (val) {
     }
 });
 
-window.ne', async function (genericA, genericB) {
+registerSpeedup('ne', async function (genericA, genericB) {
     await assertIsGeneric(genericA); await assertIsGeneric(genericB); let boolReturn;
 
     return genericA !== genericB;
 });
 
-window.le', async function (intA, intB) {
+registerSpeedup('le', async function (intA, intB) {
     await assertIsInt(intA); await assertIsInt(intB); let boolReturn;
 
     return intA <= intB;
 });
 
-window.ge', async function (intA, intB) {
+registerSpeedup('ge', async function (intA, intB) {
     await assertIsInt(intA); await assertIsInt(intB); let boolReturn;
 
     return intA >= intB;
