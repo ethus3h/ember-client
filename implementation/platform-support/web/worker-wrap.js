1cea1f0b1d0646d668345eec7542f5035414fa6d
ethus3h <kolubat@gmail.com>
2019-02-11 11:03:37 -0500

    Update Git repository using egup-single

--- implementation/platform-support/web/worker-wrap.js
+++ implementation/platform-support/web/worker-wrap.js
@@ -1,6 +1,6 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
-// Promise-wrapped worker API is inspired by Gilad Dayagi's implementation described at https://codeburst.io/promises-for-the-web-worker-9311b7831733
+// Promise-wrapped worker strategy is inspired by Gilad Dayagi's implementation described at https://codeburst.io/promises-for-the-web-worker-9311b7831733
 
 if (window.Worker) {
     let window.eiteWorker = new Worker('eite.js');
@@ -10,8 +10,9 @@ if (window.Worker) {
     window.eiteCall = async function(funcName, args) {
         window.eiteWorkerCallID = window.eiteWorkerCallID + 1;
         let thisCallId=window.eiteWorkerCallID;
+        let thisCall={id: thisCallId, args: args};
         return new Promise(function(resolve, reject) {
-            
+            window.eiteWorkerResolves[thisCallId];
         });
     }
     window.eiteWorker.onmessage = function(event) {
