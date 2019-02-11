348eb84b2f654078e851c391e5db2354c326e3da
ethus3h <kolubat@gmail.com>
2019-02-11 16:07:31 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -404,6 +404,7 @@ if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScop
             res = await self[args[0]]( ...args[1] );
         }
         catch(error) {
+            console.log('burerererer');
             self.postMessage({uuid: 'b8316ea083754b2e9290591f37d94765EiteWebworkerError', msgid: msgid, val: error});
         }
         if (!res) {
