d18652ae81c04c72c40892085d66d589f0bb507b
ethus3h <kolubat@gmail.com>
2019-02-13 16:22:35 -0500

    Update Git repository using egup-single

--- built/wasm-common/simple.c
+++ built/wasm-common/simple.c
@@ -1,8 +1,7 @@
-int main() {
+void _start() {
     // This is library code, so cannot be run by itself. But, we need an entry point so the functions don't get eaten by the linker's --gc-sections.
     utf8enc();
     utf9enc();
-    return 1;
 }
 int utf8enc() {
     return 42;
--- built/wasm-common/simple.c.wat
+++ built/wasm-common/simple.c.wat
@@ -1,6 +1,7 @@
 (module
   (type $t0 (func))
   (func $__wasm_call_ctors (type $t0))
+  (func $_start (export "_start") (type $t0))
   (table $T0 1 1 anyfunc)
   (memory $memory (export "memory") 2)
   (global $g0 (mut i32) (i32.const 66560))
