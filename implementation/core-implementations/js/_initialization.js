f741ac519ecd10f1dbfb4abed352f21fbf8a4aff
ethus3h <kolubat@gmail.com>
2019-02-13 13:02:42 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/_initialization.js
+++ implementation/core-implementations/js/_initialization.js
@@ -22,6 +22,7 @@ let intFailedTests = 0;
 let intTotalTests = 0;
 let intArrayFrameBuffer = []; // an
 let intArrayTestFrameBuffer = []; // an
+let eiteWasmModule;
 
 // Global environment
 let haveDom = false;
@@ -67,7 +68,7 @@ async function setupIfNeeded() {
 async function internalSetup() {
     // Load WebAssembly components.
     // https://developer.mozilla.org/en-US/docs/WebAssembly/Loading_and_running
-    await WebAssembly.instantiate(await getFileFromPath('wasm-common/simple.c.wat'), importObject));
+    eiteWasmModule = await WebAssembly.instantiate(await getFileFromPath('wasm-common/simple.c.wat'), importObject));
 
     // Set up environment variables.
 
