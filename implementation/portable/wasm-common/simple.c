9074c3418ecd6f421a56bbaeed96fa6f2ca861a5
ethus3h <kolubat@gmail.com>
2019-02-13 16:13:28 -0500

    Update Git repository using egup-single

--- implementation/portable/wasm-common/simple.c
+++ implementation/portable/wasm-common/simple.c
@@ -1,3 +1,9 @@
+int main() {
+    // This is library code, so cannot be run by itself. But, we need an entry point so the functions don't get eaten by the linker's --gc-sections.
+    utf8enc();
+    utf9enc();
+    return 1;
+}
 int utf8enc() {
     return 42;
 }
