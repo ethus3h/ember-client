b1761ad22a36d33131c6d049c7bff214cdf37e88
ethus3h <kolubat@gmail.com>
2019-02-13 16:53:07 -0500

    [egup] Update repository metadata

--- built/wasm-common/simple.c
+++ built/wasm-common/simple.c
@@ -1,5 +1,4 @@
 void _start() {
-    /* This is library code, so cannot be run by itself. But, we need an entry point so the functions don't get eaten by the linker's --gc-sections. */
     utf8enc();
     utf9enc();
 }
