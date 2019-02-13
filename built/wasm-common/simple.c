aecde84b7c1998432828423fd385e5b07562847c
ethus3h <kolubat@gmail.com>
2019-02-13 16:25:51 -0500

    Update Git repository using egup-single

--- built/wasm-common/simple.c
+++ built/wasm-common/simple.c
@@ -1,5 +1,9 @@
 void _start() {
-    // This is library code, so cannot be run by itself. But, we need an entry point so the functions don't get eaten by the linker's --gc-sections.
+    int ret = main(); /* https://web.archive.org/web/20190213211745/https://stackoverflow.com/questions/29694564/what-is-the-use-of-start-in-c */
+    exit ret;
+}
+int main() {
+    /* This is library code, so cannot be run by itself. But, we need an entry point so the functions don't get eaten by the linker's --gc-sections. */
     utf8enc();
     utf9enc();
 }
