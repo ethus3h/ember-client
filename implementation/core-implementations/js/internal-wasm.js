7d5523dc162bc14e7b22e1598dc4fc9edae515f6
ethus3h <kolubat@gmail.com>
2019-02-14 08:54:09 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/internal-wasm.js
+++ implementation/core-implementations/js/internal-wasm.js
@@ -1,6 +1,6 @@
 // Eventually the WASM stuff should all be available in pure StageL (+ getFileFromPath to load it), and this file's contents used only as speedups.
 
-async function wasmCall(strRoutine, intVal) {
+async function internalWasmCall(strRoutine, intVal) {
     await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('int Val = ' + intVal + '; '); await internalDebugStackEnter('wasmCall:wasm'); await assertIsStr(strRoutine);await assertIsInt(intVal); let intReturn;
 
     let intRes = 0;
@@ -10,7 +10,7 @@ async function wasmCall(strRoutine, intVal) {
     await nan/res();
 }
 
-async function wasmCallArrIn(strRoutine, intArrayVals) {
+async function internalWasmCallArrIn(strRoutine, intArrayVals) {
     await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('intArray Vals = ' + intArrayVals + '; '); await internalDebugStackEnter('wasmCallArrIn:wasm'); await assertIsStr(strRoutine);await assertIsIntArray(intArrayVals); let intReturn;
 
     let intRes = 0;
@@ -20,7 +20,7 @@ async function wasmCallArrIn(strRoutine, intArrayVals) {
     await nan/res();
 }
 
-async function wasmCallArrOut(strRoutine, intVal) {
+async function internalWasmCallArrOut(strRoutine, intVal) {
     await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('int Val = ' + intVal + '; '); await internalDebugStackEnter('wasmCallArrOut:wasm'); await assertIsStr(strRoutine);await assertIsInt(intVal); let intArrayReturn;
 
     let intArrayRes = [];
@@ -29,7 +29,7 @@ async function wasmCallArrOut(strRoutine, intVal) {
     intArrayReturn = intArrayRes; await assertIsIntArray(intArrayReturn); await internalDebugStackExit(); return intArrayReturn;
 }
 
-async function wasmCallArrInOut(strRoutine, intArrayVals) {
+async function internalWasmCallArrInOut(strRoutine, intArrayVals) {
     await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('intArray Vals = ' + intArrayVals + '; '); await internalDebugStackEnter('wasmCallArrInOut:wasm'); await assertIsStr(strRoutine);await assertIsIntArray(intArrayVals); let intArrayReturn;
 
     let intArrayRes = [];
