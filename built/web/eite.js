925b282fb91794c60a7554dab95abf724797c06c
ethus3h <kolubat@gmail.com>
2019-02-14 09:09:36 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -1534,36 +1534,7 @@ async function wasmCall(strRoutine, intVal) {
     let intRes = 0;
     intRes = await internalWasmCall(strRoutine, intVal);
 
-    intReturn = ; await assertIsInt(intReturn); await internalDebugStackExit(); return intReturn;
-    await nan/res();
-}
-
-async function wasmCallArrIn(strRoutine, intArrayVals) {
-    await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('intArray Vals = ' + intArrayVals + '; '); await internalDebugStackEnter('wasmCallArrIn:wasm'); await assertIsStr(strRoutine);await assertIsIntArray(intArrayVals); let intReturn;
-
-    let intRes = 0;
-    intRes = await internalWasmCallArrIn(strRoutine, intVal);
-
-    intReturn = ; await assertIsInt(intReturn); await internalDebugStackExit(); return intReturn;
-    await nan/res();
-}
-
-async function wasmCallArrOut(strRoutine, intVal) {
-    await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('int Val = ' + intVal + '; '); await internalDebugStackEnter('wasmCallArrOut:wasm'); await assertIsStr(strRoutine);await assertIsInt(intVal); let intArrayReturn;
-
-    let intArrayRes = [];
-    intRes = await internalWasmCallArrOut(strRoutine, intVal);
-
-    intArrayReturn = intArrayRes; await assertIsIntArray(intArrayReturn); await internalDebugStackExit(); return intArrayReturn;
-}
-
-async function wasmCallArrInOut(strRoutine, intArrayVals) {
-    await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('intArray Vals = ' + intArrayVals + '; '); await internalDebugStackEnter('wasmCallArrInOut:wasm'); await assertIsStr(strRoutine);await assertIsIntArray(intArrayVals); let intArrayReturn;
-
-    let intArrayRes = [];
-    intRes = await internalWasmCallArrInOut(strRoutine, intVal);
-
-    intArrayReturn = intArrayRes; await assertIsIntArray(intArrayReturn); await internalDebugStackExit(); return intArrayReturn;
+    intReturn = intRes; await assertIsInt(intReturn); await internalDebugStackExit(); return intReturn;
 }
 
 async function dcaFromAsciiSafeSubset(intArrayContent) {
