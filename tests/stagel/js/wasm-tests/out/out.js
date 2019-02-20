d13dcea2d8a738fc560745f9b0acf9b62158598c
ethus3h <kolubat@gmail.com>
2019-02-20 18:49:31 -0500

    Update Git repository using egup-single

--- tests/stagel/js/wasm-tests/out/out.js
+++ tests/stagel/js/wasm-tests/out/out.js
@@ -1,7 +1,7 @@
 async function runTestsWasm(boolV) {
     await internalDebugCollect('bool V = ' + boolV + '; '); await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugStackEnter('runTestsWasm:wasm-tests'); await assertIsBool(boolV); await assertIsBool(boolA);
 
-    await runTest(boolV, await implEq(42, await wasmCall('fortytwo', 0)));
+    await runTest(boolV, await implEq(42, await wasmCall('fortytwo', [ true, false ])));
     await runTest(boolV, await implEq(4, await wasmCallArrIn('add', [ 2, 2 ])));
 
     await internalDebugStackExit();
