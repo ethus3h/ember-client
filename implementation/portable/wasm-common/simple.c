99a7bbd58261f88d19699840a57d3123434ec0f1
ethus3h <kolubat@gmail.com>
2019-02-20 17:20:01 -0500

    Update Git repository using egup-single

--- implementation/portable/wasm-common/simple.c
+++ implementation/portable/wasm-common/simple.c
@@ -3,7 +3,16 @@
     utf9enc();
 }
 extern int utf8enc(); */
-int fortytwo() {
+int errorStatus;
+int checkForError() {
+    int errorTemp = errorStatus;
+    errorStatus = 0;
+    return errorTemp;
+}
+int die() {
+    errorStatus = 1; /* All calls need to call checkForError exactly once afterwards. */
+}
+int fortytwo(int a) {
     return 42;
 }
 int add(int a, int b) {
