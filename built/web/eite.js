a248f8df991228b298dd9347038c1f814d254f3b
ethus3h <kolubat@gmail.com>
2019-02-20 14:17:58 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -326,9 +326,9 @@ WabtModule().then(async function(wabt) {
         var binaryOutput = wabtWasmObject.toBinary({log: true, write_debug_names:true});
         binaryBuffer = binaryOutput.buffer;
         wasmArray = new Response(new Blob([binaryOutput.buffer])).arrayBuffer();
-        console.log(wasmArray);
         wasmArray=new Uint8Array(await wasmArray);
         console.log(wasmArray);
+        resolve(wasmArray);
        } catch (e) {
         console.log(e);
         await implDie('Failed loading WebAssembly module.');
