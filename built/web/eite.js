853b8f0d7f8b88c97adb8fe00d6b88791363595f
ethus3h <kolubat@gmail.com>
2019-02-20 14:28:49 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -322,36 +322,6 @@ async function internalEiteReqWat2Wabt(watData) {
         });
     });
 return true;
-//console.log(await module.await .then(await async function (wabt) {return 'euueu';}));
-//.then(async function(wabt) {return wabt.parseWat('test.wast',watStr, {});}));
-    return watStr;
-    /*
- * Copyright 2016 WebAssembly Community Group participants
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-var features = {};
-
-return WabtModule().then(async function(wabt) {
-
-
-
-var binaryBuffer = null;
-var binaryBlobUrl = null;
-
-var wasmInstance = null;
-
-var wrappedConsole = Object.create(console);
 
 function compile() {
   var binaryOutput;
