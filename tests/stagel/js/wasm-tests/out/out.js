1a095e9bd74d33c329b05d7bb054872413fdf2d8
ethus3h <kolubat@gmail.com>
2019-02-20 18:46:19 -0500

    Update Git repository using egup-single

--- tests/stagel/js/wasm-tests/out/out.js
+++ tests/stagel/js/wasm-tests/out/out.js
@@ -1,18 +1,8 @@
-
 async function runTestsWasm(boolV) {
     await internalDebugCollect('bool V = ' + boolV + '; '); await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugStackEnter('runTestsWasm:wasm-tests'); await assertIsBool(boolV); await assertIsBool(boolA);
 
     await runTest(boolV, await implEq(42, await wasmCall('fortytwo', 0)));
-    await runTest(boolV, await implEq(4, await wasmCallArrIn([ 2, 2 ]'add')));
+    await runTest(boolV, await implEq(4, await wasmCallArrIn('add', [ 2, 2 ])));
+
     await internalDebugStackExit();
-}
-
-async function or(boolA, boolB) {
-    await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugCollect('bool B = ' + boolB + '; '); await internalDebugStackEnter('or:booleans'); await assertIsBool(boolA);await assertIsBool(boolB); let boolReturn;
-
-    let boolTemp = false;
-    boolTemp = await implNot(boolA);
-    boolTemp = await implNot(await implAnd(boolTemp, await implNot(boolB)));
-
-    boolReturn = boolTemp; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
 }
