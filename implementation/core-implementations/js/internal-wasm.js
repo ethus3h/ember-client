38b1adf3d50bfe526ccf24a8806306bc8be90d17
ethus3h <kolubat@gmail.com>
2019-02-20 18:04:43 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/internal-wasm.js
+++ implementation/core-implementations/js/internal-wasm.js
@@ -19,33 +19,3 @@ async function internalWasmCallArrOut(strRoutine, intVal) {
 async function internalWasmCallArrInOut(strRoutine, intArrayVals) {
     return await eiteHostCall('internalEiteReqWasmCall', [strRoutine, intArrayVals]);
 }
-
-/*
-async function internalWasmCallArrIn(strRoutine, intArrayVals) {
-    await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('intArray Vals = ' + intArrayVals + '; '); await internalDebugStackEnter('wasmCallArrIn:wasm'); await assertIsStr(strRoutine);await assertIsIntArray(intArrayVals); let intReturn;
-
-    let intRes = 0;
-    intRes = await internalWasmCallArrIn(strRoutine, intVal);
-
-    intReturn = ; await assertIsInt(intReturn); await internalDebugStackExit(); return intReturn;
-    await nan/res();
-}
-
-async function internalWasmCallArrOut(strRoutine, intVal) {
-    await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('int Val = ' + intVal + '; '); await internalDebugStackEnter('wasmCallArrOut:wasm'); await assertIsStr(strRoutine);await assertIsInt(intVal); let intArrayReturn;
-
-    let intArrayRes = [];
-    intRes = await internalWasmCallArrOut(strRoutine, intVal);
-
-    intArrayReturn = intArrayRes; await assertIsIntArray(intArrayReturn); await internalDebugStackExit(); return intArrayReturn;
-}
-
-async function internalWasmCallArrInOut(strRoutine, intArrayVals) {
-    await internalDebugCollect('str Routine = ' + strRoutine + '; '); await internalDebugCollect('intArray Vals = ' + intArrayVals + '; '); await internalDebugStackEnter('wasmCallArrInOut:wasm'); await assertIsStr(strRoutine);await assertIsIntArray(intArrayVals); let intArrayReturn;
-
-    let intArrayRes = [];
-    intRes = await internalWasmCallArrInOut(strRoutine, intVal);
-
-    intArrayReturn = intArrayRes; await assertIsIntArray(intArrayReturn); await internalDebugStackExit(); return intArrayReturn;
-}
-*/
