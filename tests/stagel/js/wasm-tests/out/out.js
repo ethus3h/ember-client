11ae9d448238a14ebf8668e3f6da3fc441ea459a
ethus3h <kolubat@gmail.com>
2019-02-20 18:43:53 -0500

    Update Git repository using egup-single

--- tests/stagel/js/wasm-tests/out/out.js
+++ tests/stagel/js/wasm-tests/out/out.js
@@ -1,3 +1,18 @@
-if (await or(await implEq('Zp', strType), await implEq('Zl', strType))) {
-    /* return true */
+
+async function runTestsWasm(boolV) {
+    await internalDebugCollect('bool V = ' + boolV + '; '); await internalDebugStackEnter('runTestsWasm:wasm-tests'); await assertIsBool(boolV);
+
+    await runTest(boolV, await implEq(42, await wasmCall('fortytwo', 0)));
+    await runTest(boolV, await implEq(4, await wasmCallArrIn([ 2, 2 ]'add')));
+    await internalDebugStackExit();
+}
+
+async function or(boolA, boolB) {
+    await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugCollect('bool B = ' + boolB + '; '); await internalDebugStackEnter('or:booleans'); await assertIsBool(boolA);await assertIsBool(boolB); let boolReturn;
+
+    let boolTemp = false;
+    boolTemp = await implNot(boolA);
+    boolTemp = await implNot(await implAnd(boolTemp, await implNot(boolB)));
+
+    boolReturn = boolTemp; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
 }
