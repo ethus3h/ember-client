2ec36e002ba0c0d1359729c2dae24e1d4be61c25
ethus3h <kolubat@gmail.com>
2019-02-21 14:53:39 -0500

    Update Git repository using egup-single

--- tests/stagel/js/generic-parameter/out/out.js
+++ tests/stagel/js/generic-parameter/out/out.js
@@ -1,5 +1,5 @@
 async function ne(genericA, genericB) {
-    await internalDebugCollect('generic A = ' + genericA + '; '); await internalDebugCollect('generic B = ' + genericB + '; '); await internalDebugStackEnter('ne:in'); await assertIsGeneric(genericA);await assertIsGeneric(genericB); let boolReturn;
+    await internalDebugCollect('generic A = ' + genericA + '; '); await internalDebugCollect('generic B = ' + genericB + '; '); await internalDebugStackEnter('ne:in'); await assertIsGeneric(genericA); await assertIsGeneric(genericB); let boolReturn;
 
     let boolTemp = false;
     boolTemp = await implNot(await implEq(genericA, genericB));
--- tests/stagel/js/logical-or/out/out.js
+++ tests/stagel/js/logical-or/out/out.js
@@ -1,5 +1,5 @@
 async function or(boolA, boolB) {
-    await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugCollect('bool B = ' + boolB + '; '); await internalDebugStackEnter('or:in'); await assertIsBool(boolA);await assertIsBool(boolB); let boolReturn;
+    await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugCollect('bool B = ' + boolB + '; '); await internalDebugStackEnter('or:in'); await assertIsBool(boolA); await assertIsBool(boolB); let boolReturn;
 
     let boolTemp = false;
     boolTemp = await implNot(boolA);
