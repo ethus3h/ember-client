2fb275730c78c8a27dbeeb2ae655bf3d6558f3e9
ethus3h <kolubat@gmail.com>
2019-02-21 15:21:34 -0500

    Update Git repository using egup-single

---
+++ tests/stagel/js/array-s/run/out.js
@@ -0,0 +1,10 @@
+async function strFromByte(intIn) {
+    await internalDebugCollect('int In = ' + intIn + '; '); await internalDebugStackEnter('strFromByte:in'); await assertIsInt(intIn); let strReturn;
+
+    let strArrayChars = [];
+    strArrayChars = ;
+    /* 0 through 6 */
+    let strArrayTemp = [];
+    strArrayTemp = ;
+    await append(strArrayChars, strArrayTemp);
+}
---
+++ tests/stagel/js/wasm-tests/run/out.js
@@ -0,0 +1,8 @@
+async function runTestsWasm(boolV, boolA) {
+    await internalDebugCollect('bool V = ' + boolV + '; '); await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugStackEnter('runTestsWasm:in'); await assertIsBool(boolV); await assertIsBool(boolA);
+
+    await runTest(boolV, await implEq(42, await wasmCall('fortytwo', )));
+    await runTest(boolV, await implEq(4, await wasmCallArrIn('add', )));
+
+    await internalDebugStackExit();
+}
