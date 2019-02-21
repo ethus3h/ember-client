dc8529b6d8776a49780817d08fc3d52fc5f4b161
ethus3h <kolubat@gmail.com>
2019-02-20 22:10:34 -0500

    Update Git repository using egup-single

--- implementation/portable/wasm-common/eite-shared/_export.c
+++ implementation/portable/wasm-common/eite-shared/_export.c
@@ -3,6 +3,6 @@
 #  define EXPORT __declspec(dllexport)
 #  define IMPORT __declspec(dllimport)
 #else
-#  define EXPORT
+#  define EXPORT __attribute__ ((visibility ("default")))
 #  define IMPORT __attribute__ ((visibility ("default")))
 #endif
