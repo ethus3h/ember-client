4462113240e848c88196c69e9627e61bc560ac87
ethus3h <kolubat@gmail.com>
2019-02-21 17:34:41 -0500

    Update Git repository using egup-single

--- tests/stagel/js/array-s/run/out.js
+++ tests/stagel/js/array-s/run/out.js
@@ -2,9 +2,11 @@ async function strFromByte(intIn) {
     await internalDebugCollect('int In = ' + intIn + '; '); await internalDebugStackEnter('strFromByte:in'); await assertIsInt(intIn); let strReturn;
 
     let strArrayChars = [];
-    strArrayChars = [ '', '', '', '', '', '', '' ];
+    strArrayChars = codegenArrayLiteral requested; returning [ '', '', '', '', '', '', '' ]
+[ '', '', '', '', '', '', '' ]codegenArrayLiteral has been reset to: ;
     /* 0 through 6 */
     let strArrayTemp = [];
-    strArrayTemp = [ '', '', '', '', '', '', '' ][ 'd', 'b', 'l', 'c', 'b', 'a' ];
+    strArrayTemp = codegenArrayLiteral requested; returning [ '', '', '', '', '', '', '' ][ 'd', 'b', 'l', 'c', 'b', 'a' ]
+[ '', '', '', '', '', '', '' ][ 'd', 'b', 'l', 'c', 'b', 'a' ]codegenArrayLiteral has been reset to: ;
     await append(strArrayChars, strArrayTemp);
 }
--- tests/stagel/js/wasm-tests/run/out.js
+++ tests/stagel/js/wasm-tests/run/out.js
@@ -1,8 +1,10 @@
 async function runTestsWasm(boolV, boolA) {
     await internalDebugCollect('bool V = ' + boolV + '; '); await internalDebugCollect('bool A = ' + boolA + '; '); await internalDebugStackEnter('runTestsWasm:in'); await assertIsBool(boolV); await assertIsBool(boolA);
 
-    await runTest(boolV, await implEq(42, await wasmCall('fortytwo', [ true, false ])));
-    await runTest(boolV, await implEq(4, await wasmCallArrIn('add', [ true, false ][ 2, 2 ])));
+    await runTest(boolV, await implEq(42, await wasmCall('fortytwo', codegenArrayLiteral requested; returning [ true, false ]
+[ true, false ]codegenArrayLiteral has been reset to: )));
+    await runTest(boolV, await implEq(4, await wasmCallArrIn('add', codegenArrayLiteral requested; returning [ true, false ][ 2, 2 ]
+[ true, false ][ 2, 2 ]codegenArrayLiteral has been reset to: )));
 
     await internalDebugStackExit();
 }
