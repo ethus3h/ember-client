33d645572b50cf9b9f8d26910c7a68f4b3cd5dca
ethus3h <kolubat@gmail.com>
2019-02-21 23:48:49 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/_initialization.js
+++ implementation/core-implementations/js/_initialization.js
@@ -333,7 +333,7 @@ if (typeof window !== 'undefined') {
         window.eiteWorker.onmessage = function(message) {
             // Handle messages sent to this code when it is not running as a Web worker
             const {uuid, msgid, res} = message.data;
-            implDebug('Host got message '+msgid+' from worker: '+args, 1);
+            implDebug('Host got message '+msgid+' from worker: '+res, 1);
             internalDebugLogJSObject(message);
             if (uuid === 'b8316ea083754b2e9290591f37d94765EiteWebworkerResponse') {
                 if (res || res === null) {
