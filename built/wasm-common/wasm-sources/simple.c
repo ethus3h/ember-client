ab944dc76cedd9cdc82d4ad599b443b4e8f7a74d
ethus3h <kolubat@gmail.com>
2019-02-25 03:21:50 -0500

    Update Git repository using egup-single

---
+++ built/wasm-common/wasm-sources/simple.c
@@ -0,0 +1,20 @@
+#include "_eite-shared.c"
+
+/* void _start() {
+    utf8enc();
+    utf9enc();
+}
+extern int utf8enc(); */
+
+EXPORT int fortytwo(int a) {
+    return 42;
+}
+EXPORT int add(int a, int b) {
+    return a+b;
+}
+EXPORT int utf8enc() {
+    return 44;
+}
+EXPORT int utf9enc() {
+    return 43;
+}
