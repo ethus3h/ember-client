dcabae1222da3c8127abf0015afb6c2726872786
ethus3h <kolubat@gmail.com>
2019-02-24 19:22:37 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/internal-wasm.js
+++ implementation/core-implementations/js/internal-wasm.js
@@ -1,6 +1,6 @@
 // Eventually the WASM stuff should all be available in pure StageL (+ getFileFromPath to load it), and this file's contents used only as speedups.
 
-async function internalEiteReqWasmCall(strRoutine, giVal) {
+async function internalEiteReqWasmCall(strRoutine, giVal, returnsArray=false) {
     if (typeof giVal === 'number') {
         return window.eiteWasmModule.instance.exports[strRoutine](giVal);
     }
@@ -14,17 +14,17 @@ async function internalWasmCall(strRoutine, intVal) {
 }
 
 async function internalWasmCallNoArgs(strRoutine) {
-    return await eiteHostCall('internalEiteReqWasmCall', [strRoutine]);
+    return await eiteHostCall('internalEiteReqWasmCall', [strRoutine, null, false]);
 }
 
 async function internalWasmCallArrIn(strRoutine, intArrayVals) {
-    return await eiteHostCall('internalEiteReqWasmCall', [strRoutine, intArrayVals]);
+    return await eiteHostCall('internalEiteReqWasmCall', [strRoutine, intArrayVals, false]);
 }
 
 async function internalWasmCallArrOut(strRoutine, intVal) {
-    return await eiteHostCall('internalEiteReqWasmCall', [strRoutine, intVal]);
+    return await eiteHostCall('internalEiteReqWasmCall', [strRoutine, intVal, true]);
 }
 
 async function internalWasmCallArrInOut(strRoutine, intArrayVals) {
-    return await eiteHostCall('internalEiteReqWasmCall', [strRoutine, intArrayVals]);
+    return await eiteHostCall('internalEiteReqWasmCall', [strRoutine, intArrayVals, true]);
 }
