4ecb3f605ce5b0d25113abe10bf9356804a68a66
ethus3h <kolubat@gmail.com>
2019-02-25 21:16:53 -0500

    Update Git repository using egup-single

--- implementation/portable/wasm-sources/simple.c
+++ implementation/portable/wasm-sources/simple.c
@@ -7,20 +7,11 @@
 }
 extern int utf8enc(); */
 
-https://github.com/emscripten-core/emscripten/blob/5187ea263120ff57424a184c1bc73259e24dc9a9/tests/wrap_malloc.cpp
-void * __attribute__((noinline)) malloc(size_t size)
-{
-	++totalAllocated;
-	void *ptr = emscripten_builtin_malloc(size);
-	printf("Allocated %u bytes, got %p. %d pointers allocated total.\n", size, ptr, totalAllocated);
-	return ptr;
+void * __attribute__((noinline)) malloc(size_t size) {
+    return malloc(size);
 }
-
-void __attribute__((noinline)) free(void *ptr)
-{
-	++totalFreed;
-	emscripten_builtin_free(ptr);
-	printf("Freed ptr %p, %d pointers freed total.\n", ptr, totalFreed);
+void __attribute__((noinline)) free(void *ptr) {
+    free(ptr);
 }
 EXPORT int fortytwo(int a) {
     return 42;
