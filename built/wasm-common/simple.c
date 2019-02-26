c0bf53f8029f44ef292dadb0d63690bf5f7f391a
ethus3h <kolubat@gmail.com>
2019-02-26 12:30:46 -0500

    Update Git repository using egup-single

--- built/wasm-common/simple.c
+++ built/wasm-common/simple.c
@@ -1,18 +1,18 @@
 #include "_eite-shared.c"
-/*#include <stdlib.h>
-*/
+#include <stdlib.h>
+
 /* void _start() {
     utf8enc();
     utf9enc();
 }
 extern int utf8enc(); */
 
-/* EXPORT void * __attribute__((noinline)) _malloc(size_t size) {
+EXPORT void * __attribute__((noinline)) _malloc(size_t size) {
     return malloc(size);
 }
 EXPORT void __attribute__((noinline)) _free(void *ptr) {
     free(ptr);
-} */
+}
 EXPORT int fortytwo(int a) {
     return 42;
 }
