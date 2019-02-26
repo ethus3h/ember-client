a0d17fff605135fd9d357212135bbd7b2db6abe6
ethus3h <kolubat@gmail.com>
2019-02-25 21:17:49 -0500

    Update Git repository using egup-single

--- built/wasm-common/simple.c
+++ built/wasm-common/simple.c
@@ -7,6 +7,12 @@
 }
 extern int utf8enc(); */
 
+void * __attribute__((noinline)) malloc(size_t size) {
+    return malloc(size);
+}
+void __attribute__((noinline)) free(void *ptr) {
+    free(ptr);
+}
 EXPORT int fortytwo(int a) {
     return 42;
 }
