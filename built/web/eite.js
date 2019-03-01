034fe76e1ffc982839595f445009c47f57bb25be
ethus3h <kolubat@gmail.com>
2019-02-28 23:36:10 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -4401,8 +4401,8 @@ async function byteArrayFromIntBitArray(intArrayIn) {
     intLen = await count(intArrayIn);
     let intArrayTemp = [];
     while (await implLt(intI, intLen)) {
-        //alert(intI + '=intI;intLen=' + intLen + ';arrayTemp=' + intArrayTemp + ';arrayRes=' + intArrayRes + ';intI mod 8=' + await implMod(intI, 8)+'; last item='+await get(intArrayIn, intI));
-        if (await implAnd(await implEq(0, await implMod(intI, 7), ), await implNot(await implEq(0, await count(intArrayRes))))) {
+        alert(intI + '=intI;intLen=' + intLen + ';arrayTemp=' + intArrayTemp + ';arrayRes=' + intArrayRes + ';intI mod 8=' + await implMod(intI, 8)+'; last item='+await get(intArrayIn, intI));
+        if (await implAnd(await implEq(0, await implMod(intI, 8), ), await implNot(await implEq(0, await count(intArrayRes))))) {
 alert(intArrayTemp);
             intArrayRes = await push(intArrayRes, await byteFromIntBitArray(intArrayTemp));
 alert(intArrayRes);
