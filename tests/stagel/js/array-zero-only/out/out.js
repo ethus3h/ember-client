50c1d69acd8665f5217701092a4a4af4483ae5c7
ethus3h <kolubat@gmail.com>
2019-02-28 22:40:31 -0500

    Update Git repository using egup-single

--- tests/stagel/js/array-zero-only/out/out.js
+++ tests/stagel/js/array-zero-only/out/out.js
@@ -1,4 +1,4 @@
-    while (await implLt(intI, intCount)) {
-        strOut = await implCat(strOut, await charFromByte(await get(intArrayInput, intI)));
-        intI = await implAdd(intI, 1);
-    }
+let intArrayRes = [];
+while (await implLt(8, await len(intArrayRes))) {
+    intArrayRes = await push([ 0 ], intArrayRes);
+}
