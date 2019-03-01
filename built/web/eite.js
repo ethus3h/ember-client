616e4c1feb9742a7f0e219e10da8f6e2912a2bad
ethus3h <kolubat@gmail.com>
2019-02-28 23:33:40 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -4401,7 +4401,7 @@ async function byteArrayFromIntBitArray(intArrayIn) {
     intLen = await count(intArrayIn);
     let intArrayTemp = [];
     while (await implLt(intI, intLen)) {
-        alert(intI + '=intI;intLen=' + intLen + ';arrayTemp=' + intArrayTemp + ';arrayRes=' + intArrayRes + ';intI mod 8=' + await implMod(intI, 8)+'; last item='+await get(intArrayIn, intI));
+        //alert(intI + '=intI;intLen=' + intLen + ';arrayTemp=' + intArrayTemp + ';arrayRes=' + intArrayRes + ';intI mod 8=' + await implMod(intI, 8)+'; last item='+await get(intArrayIn, intI));
         if (await implAnd(await implEq(0, await implMod(intI, 8), ), await implNot(await implEq(0, await count(intArrayRes))))) {
             intArrayRes = await push(intArrayRes, await byteFromIntBitArray(intArrayTemp));
             intArrayTemp = [  ];
@@ -4410,7 +4410,7 @@ async function byteArrayFromIntBitArray(intArrayIn) {
         intI = await implAdd(intI, 1);
     }
     await assertIsByteArray(intArrayRes);
-
+alert(intArrayReturn);
     intArrayReturn = intArrayRes; await assertIsIntArray(intArrayReturn); await internalDebugStackExit(); return intArrayReturn;
 }
 
