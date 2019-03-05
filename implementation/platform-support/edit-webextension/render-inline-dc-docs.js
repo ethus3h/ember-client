93c701cd9ddc96a76d436905c8192e042e9ac4a2
ethus3h <kolubat@gmail.com>
2019-03-04 20:07:30 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/render-inline-dc-docs.js
+++ implementation/platform-support/edit-webextension/render-inline-dc-docs.js
@@ -10,6 +10,12 @@
     let re=/􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐/g
     for(let i=0;i<elems.length;i++){
         el=elems[i];
-        if(el.textContent.match())
+        if(el.textContent.match(re)){
+            let ifr=document.createElement('iframe');
+            ifr.class='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrame';
+            ifr.src=browser.runtime.getURL('edit.html');
+            ifr.contentWindow.postMessage(['b8316ea083754b2e9290591f37d94765EiteWebextensionMessageUtf8', false, el.textContent.match(new RegExp('􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐(.*)󼅹󴶯􈡺󿔊􆲦􍸂󲀰􏼝'))], ifr.src);
+            el.insertBefore(ifr);
+        }
     }
 })();
