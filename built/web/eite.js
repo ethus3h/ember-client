f8b3ad60b47b8f8c7e5509eeb6360f1566051469
ethus3h <kolubat@gmail.com>
2019-03-05 04:45:05 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -2242,7 +2242,7 @@ async function dcaToUtf8(intArrayContent) {
 
 async function dcaFromUtf8(intArrayContent) {
     await internalDebugCollect('intArray Content = ' + intArrayContent + '; '); await internalDebugStackEnter('dcaFromUtf8:format-utf8'); await assertIsIntArray(intArrayContent); let intArrayReturn;
-    function dcBasenbUuidMonitorState(int){
+    async function dcBasenbUuidMonitorState(int){
         let boolRes;
         boolRes=await implEq(int, intDcBasenbUuidMonitorState);
         return boolRes;
