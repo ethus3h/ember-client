d6e7d7d824d4edb5b6101f46a82d92e234abeb80
ethus3h <kolubat@gmail.com>
2019-03-04 19:53:15 -0500

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/render-inline-dc-docs.js
+++ implementation/platform-support/edit-webextension/render-inline-dc-docs.js
@@ -1,6 +1,15 @@
 (async function {
-    let elems=document.getElementsByTagName('*');
+    //let elems=document.getElementsByTagName('*');
+    function textNodesUnder(el) {
+        var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+        while(n=walk.nextNode()) a.push(n);
+        return a;
+    }
+    let elems=textNodesUnder(document.body);
+    let el;
+    let re=/􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐/g
     for(let i=0;i<elems.length;i++){
-        
+        el=elems[i];
+        if(el.textContent.match())
     }
 })();
