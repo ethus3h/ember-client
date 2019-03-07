afc210b084f1249683e18bfbfa1a489afd2b4b05
ethus3h <kolubat@gmail.com>
2019-03-07 11:17:04 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -1,5 +1,7 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
+Console.profile();
+
 /* math, provides:
     implAdd
     implSub
@@ -3719,6 +3721,7 @@ async function runTestsOnly(boolV) {
 
         boolReturn = true; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
     }
+    Console.profileEnd();
 
     boolReturn = false; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
 }
