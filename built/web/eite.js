a87b3ba9a971769f29e19e27a7ab820246ae4717
ethus3h <kolubat@gmail.com>
2019-03-08 21:11:38 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -4504,9 +4504,10 @@ async function runTestsFormatUtf8(boolV) {
     await internalDebugCollect('bool V = ' + boolV + '; '); await internalDebugStackEnter('runTestsFormatUtf8:format-utf8-tests'); await assertIsBool(boolV);
 
     //STAGEL_DEBUG=2;
-    console.log('burur');
-    await runTest(boolV, await arrEq(await append([ 49, 32, 50 ], await append(await getArmoredUtf8EmbeddedStartUuid(), await append([ 244, 131, 173, 156, 244, 143, 191, 187 ], await getArmoredUtf8EmbeddedEndUuid(), ), ), ), await dcaToDcbnbUtf8([ 35, 18, 36, 291 ])));
+    //console.log('burur');
+    //await runTest(boolV, await arrEq(await append([ 49, 32, 50 ], await append(await getArmoredUtf8EmbeddedStartUuid(), await append([ 244, 131, 173, 156, 244, 143, 191, 187 ], await getArmoredUtf8EmbeddedEndUuid(), ), ), ), await dcaToDcbnbUtf8([ 35, 18, 36, 291 ])));
     console.log(await dcaToDcbnbUtf8([ 35, 18, 36, 291 ]));
+    throw 'burererer';
     console.log(await append([ 49, 32, 50 ], await append(await getArmoredUtf8EmbeddedStartUuid(), await append([ 244, 131, 173, 156, 244, 143, 191, 187 ], await getArmoredUtf8EmbeddedEndUuid(), ), ), ));
     console.log('buruar');
     //STAGEL_DEBUG=0;
