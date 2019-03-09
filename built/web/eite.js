5ca961169f8120e9eb80c99735cf2fe2a39e6adb
ethus3h <kolubat@gmail.com>
2019-03-09 01:34:31 -0500

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -1846,6 +1846,7 @@ async function byteArrayFromBasenbUtf8(intArrayIn) {
     await internalDebugCollect('intArray In = ' + intArrayIn + '; '); await internalDebugStackEnter('byteArrayFromBasenbUtf8:basenb-utf8'); await assertIsIntArray(intArrayIn); let intArrayReturn;
     await assertIsByteArray(intArrayIn);
     console.log('Trying to decode: '+intArrayIn);
+    await implWarn('bubububu');
     let intArrayRes = [];
     /* Extract remainder length */
     let intRemainder = 0;
