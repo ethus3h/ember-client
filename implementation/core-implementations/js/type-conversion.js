19d45370ad2ae8f7ba70ffc5dbcd2eca5a39ac4c
ethus3h <kolubat@gmail.com>
2019-03-09 00:18:12 -0500

    Update Git repository using egup-single

--- implementation/core-implementations/js/type-conversion.js
+++ implementation/core-implementations/js/type-conversion.js
@@ -61,7 +61,7 @@ async function internalIntBitArrayToBasenbString(intBase, intBitArrayInput) {
 
 async function internalIntBitArrayFromBasenbString(byteArrayInput, intRemainder) {
     let res;
-    res=Base16b.decode(new TextDecoder().decode(new UInt8Array(byteArrayInput)), intRemainder);
+    res=Base16b.decode(new TextDecoder().decode(new Uint8Array(byteArrayInput)), intRemainder);
     if (res !== false) {
         return new TextEncoder().encode(res);
     }
