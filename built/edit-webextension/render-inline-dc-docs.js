2f293cb2d31c3eb1476ffd825487e06e3f3597d7
ethus3h <kolubat@gmail.com>
2019-03-11 15:27:15 -0400

    Update Git repository using egup-single

--- built/edit-webextension/render-inline-dc-docs.js
+++ built/edit-webextension/render-inline-dc-docs.js
@@ -22,7 +22,7 @@
                     sheet.insertRule(selector + "{" + propText + "}", sheet.cssRules.length);
                 };
             })(document.createElement("style"));
-            let keyframeSheet = document.head.appendChild(style).sheet;
+            let keyframeSheet = document.head.appendChild(document.createElement("style")).sheet;
             // https://github.com/codepo8/CSS3-Rainbow-Dividers/blob/master/rainbows.css
             keyframeSheet.insertRule('@keyframes keyframeRule { from { background-position: top left; } to { background-position: top right; } }', keyframeSheet.cssRules.length);
             addRule(".b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderSpan::before", {
