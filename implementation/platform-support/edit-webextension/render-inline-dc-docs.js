08a60377ab82dd5f4ad6a824f883fdfc77285e60
ethus3h <kolubat@gmail.com>
2019-03-11 12:31:09 -0400

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/render-inline-dc-docs.js
+++ implementation/platform-support/edit-webextension/render-inline-dc-docs.js
@@ -11,21 +11,30 @@
     let nodeVal;
     let foundIndex;
     let begin;
+    let ifr;
+    let matched;
+    let textNode;
+    let parentNode;
     for (let i=0;i<elems.length;i++) {
         el=elems[i];
         if (el.textContent.match(re)) {
             // based on https://web.archive.org/web/20190311140420/https://stackoverflow.com/questions/8644428/how-to-highlight-text-using-javascript
-            let nodeVal=el.nodeValue;
-            let foundIndex = nodeVal.indexOf('􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐');
+            nodeVal=el.nodeValue;
+            foundIndex = nodeVal.indexOf('􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐');
             begin = nodeVal.substring(0, foundIndex);
-            let matchedText = el.textContent.match(new RegExp('􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐(.*)󼅹󴶯􈡺󿔊􆲦􍸂󲀰􏼝'));
-            let ifr=document.createElement('iframe');
+            parentNode = el.parentNode;
+            matched = el.textContent.match(new RegExp('􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐(.*)󼅹󴶯􈡺󿔊􆲦􍸂󲀰􏼝'));
+            if (begin.length > 0) {
+                textNode = document.createTextNode(begin);
+                parentNode.insertBefore(textNode, el);
+            }
+            ifr=document.createElement('iframe');
             ifr.id='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i;
             ifr.className='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrame';
             ifr.src=browser.runtime.getURL('edit.html');
-            el.parentNode.insertBefore(ifr, el);
+            parentNode.insertBefore(ifr, el);
             ifr=document.getElementById('b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i);
-            ifr.contentWindow.postMessage(['b8316ea083754b2e9290591f37d94765EiteWebextensionMessageUtf8', false, matchedText)[0]], ifr.src);
+            ifr.contentWindow.postMessage(['b8316ea083754b2e9290591f37d94765EiteWebextensionMessageUtf8', false, matched)[0]], ifr.src);
         }
     }
 })();
