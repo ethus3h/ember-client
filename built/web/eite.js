5463505774bab51b2764da9e44ff50eed623a344
ethus3h <kolubat@gmail.com>
2019-03-11 18:51:01 -0400

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -2459,6 +2459,7 @@ async function dcaFromUtf8(intArrayContent) {
                                     intArrayRes = await append(intArrayRes, await unpack32(intArrayCurrentUnmappableChar));
                                     intCollectedDcBasenbCharsCounter = await implAdd(intCollectedDcBasenbCharsCounter, await count(intArrayCurrentUnmappableChar));
                                 }
+                                intArrayCollectedDcBasenbChars = [  ];
                             }
                         }
                         else {
