b55b9b3e2fff8707b5cd42865e0b0957bf41edac
ethus3h <kolubat@gmail.com>
2019-03-11 17:10:32 -0400

    Update Git repository using egup-single

--- built/edit-webextension/render-inline-dc-docs.js
+++ built/edit-webextension/render-inline-dc-docs.js
@@ -25,20 +25,20 @@
             // https://github.com/codepo8/CSS3-Rainbow-Dividers/blob/master/rainbows.css
             stylesheet.insertRule('@keyframes keyframeRule { from { background-position: top left; } to { background-position: top right; } }', stylesheet.cssRules.length);
             addRule(".b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderSpan", {
-                position: "relative",
-                display: "inline-block"
-            });
+                    position: "relative",
+                    display: "inline-block"
+                });
             addRule(".b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderSpan::before", {
-                display: "block",
-                animation: "keyframeRule 2.5s forwards linear infinite",
-                width: "100%",
-                height: "0.2rem",
-                "background-image": "linear-gradient( left, red, orange, yellow, green, blue, indigo, violet, indigo, blue, green, yellow, orange, red )",
-                "background-image": "-moz-linear-gradient( left, red, orange, yellow, green, blue, indigo, violet, indigo, blue, green, yellow, orange, red )",
-                //background: "red",
-                "background-size": "50% auto",
-                content: "' '"
-            });
+                    display: "block",
+                    animation: "keyframeRule 2.5s forwards linear infinite",
+                    width: "100%",
+                    height: "0.2rem",
+                    "background-image": "linear-gradient( left, red, orange, yellow, green, blue, indigo, violet, indigo, blue, green, yellow, orange, red )",
+                    "background-image": "-moz-linear-gradient( left, red, orange, yellow, green, blue, indigo, violet, indigo, blue, green, yellow, orange, red )",
+                    //background: "red",
+                    "background-size": "50% auto",
+                    content: "' '"
+                });
         }
     }
     let el;
