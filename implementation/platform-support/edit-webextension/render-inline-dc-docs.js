fbc6a36e7829da9f8e5100dd0ee57cf641eda2ea
ethus3h <kolubat@gmail.com>
2019-03-11 18:16:27 -0400

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/render-inline-dc-docs.js
+++ implementation/platform-support/edit-webextension/render-inline-dc-docs.js
@@ -1,13 +1,51 @@
 (async function() {
     //let elems=document.getElementsByTagName('*');
-    function textNodesUnder(el) {
-        var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+    let textNodesUnder = function(el) {
+        let n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
         while (n=walk.nextNode()) {
             a.push(n);
         }
         return a;
     }
     let elems=textNodesUnder(document.body);
+    let added=false;
+    let addCssIfNeeded = function() {
+        if (!added) {
+            added=true;
+            let stylesheet = document.head.appendChild(document.createElement("style")).sheet;
+            let addRule = (function(sheet) {
+                    // https://web.archive.org/web/20190311175426/https://stackoverflow.com/questions/4481485/changing-css-pseudo-element-styles-via-javascript
+                    return function (selector, css) {
+                        let propText = typeof css === "string" ? css : Object.keys(css).map(function (p) {
+                            return p + ":" + (p === "content" ? "'" + css[p] + "'" : css[p]);
+                        }).join(";");
+                        sheet.insertRule(selector + "{" + propText + "}", sheet.cssRules.length);
+                    };
+                })(stylesheet);
+            // https://github.com/codepo8/CSS3-Rainbow-Dividers/blob/master/rainbows.css
+            stylesheet.insertRule('@keyframes keyframeRule { from { background-position: top left; } to { background-position: top right; } }', stylesheet.cssRules.length);
+            addRule(".b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderSpan", {
+                    position: "relative",
+                    display: "inline-block"
+                });
+            addRule(".b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrame", {
+                    display: "none",
+                    border: 0
+                });
+            addRule(".b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderSpan::before", {
+                    display: "block",
+                    animation: "keyframeRule 2.5s forwards linear infinite",
+                    width: "100%",
+                    height: "0.2rem",
+                    position: "absolute",
+                    "background-image": "linear-gradient( left, red, orange, yellow, green, blue, indigo, violet, indigo, blue, green, yellow, orange, red )",
+                    "background-image": "-moz-linear-gradient( left, red, orange, yellow, green, blue, indigo, violet, indigo, blue, green, yellow, orange, red )",
+                    //background: "red",
+                    "background-size": "50% auto",
+                    content: "' '"
+                });
+        }
+    }
     let el;
     let re=/􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐/g;
     let nodeVal;
@@ -22,9 +60,9 @@
     let limit = 1;
     let j;
     for (let i=0;i<elems.length;i++) {
-        console.log('bubububbubu');
         el=elems[i];
         if (el.textContent.match(re)) {
+            addCssIfNeeded();
             replacedAll = false;
             j = 0;
             while (!replacedAll && j < limit) {
@@ -52,9 +90,20 @@
                     ifr.id='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i;
                     ifr.className='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrame';
                     ifr.src=browser.runtime.getURL('edit.html');
+                    ifr.style.height=span.clientHeight+'px';
+                    ifr.style.width=span.clientWidth+'px';
                     parentNode.insertBefore(ifr, el);
                     ifr=document.getElementById('b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i);
-                    ifr.contentWindow.postMessage([ 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessageUtf8', false, matched], ifr.src);
+                    // Wow, I really hate developing JavaScript.
+                    let replaceSpan=function(span, ifr){/*console.log('tebuice');*/span.remove();ifr.style.display='inline-block';};
+                    replaceSpan(span, ifr);
+                    // console.log('bubube');
+                    //alert('a');
+                    //const sleep = t => x => new Promise(r => setTimeout(()=>r(x), t));
+                    //await sleep(1000); alert('b');
+                    //window.setTimeout(alert, 1);
+                    //window.setTimeout(replaceSpan, 1, span, ifr);
+                    //ifr.contentWindow.postMessage([ 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessageUtf8', false, matched], ifr.src);
                 }
             }
         }
