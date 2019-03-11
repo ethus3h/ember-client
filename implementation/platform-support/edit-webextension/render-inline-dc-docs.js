397b0fa5d22b39b44f9ec1c7be0092acf6f5d0e0
ethus3h <kolubat@gmail.com>
2019-03-11 12:36:49 -0400

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/render-inline-dc-docs.js
+++ implementation/platform-support/edit-webextension/render-inline-dc-docs.js
@@ -16,31 +16,40 @@
     let textNode;
     let parentNode;
     let span;
+    let replacedAll;
     for (let i=0;i<elems.length;i++) {
         el=elems[i];
         if (el.textContent.match(re)) {
-            // based on https://web.archive.org/web/20190311140420/https://stackoverflow.com/questions/8644428/how-to-highlight-text-using-javascript
-            nodeVal=el.nodeValue;
-            foundIndex = nodeVal.indexOf('􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐');
-            begin = nodeVal.substring(0, foundIndex);
-            parentNode = el.parentNode;
-            matched = el.textContent.match(new RegExp('􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐(.*)󼅹󴶯􈡺󿔊􆲦􍸂󲀰􏼝'));
-            if (begin.length > 0) {
-                textNode = document.createTextNode(begin);
-                parentNode.insertBefore(textNode, el);
+            replacedAll = false;
+            while (!replacedAll) {
+                // based on https://web.archive.org/web/20190311140420/https://stackoverflow.com/questions/8644428/how-to-highlight-text-using-javascript
+                nodeVal=el.nodeValue;
+                foundIndex = nodeVal.indexOf('􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐');
+                if (foundIndex < 0) {
+                    replacedAll = true;
+                }
+                else {
+                    begin = nodeVal.substring(0, foundIndex);
+                    parentNode = el.parentNode;
+                    matched = el.textContent.match(new RegExp('􍁝􋶀󼷢󺀊󸥎􈺍󲋠􏺐(.*)󼅹󴶯􈡺󿔊􆲦􍸂󲀰􏼝'));
+                    if (begin.length > 0) {
+                        textNode = document.createTextNode(begin);
+                        parentNode.insertBefore(textNode, el);
+                    }
+                    span = document.createElement("span");
+                    span.className = span.className + ' b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderSpan';
+                    span.appendChild(document.createTextNode(matched));
+                    parentNode.insertBefore(span, el);
+                    ifr=document.createElement('iframe');
+                    nodeVal = nodeVal.substring(foundIndex + matched.length);
+                    ifr.id='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i;
+                    ifr.className='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrame';
+                    ifr.src=browser.runtime.getURL('edit.html');
+                    parentNode.insertBefore(ifr, el);
+                    ifr=document.getElementById('b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i);
+                    ifr.contentWindow.postMessage(['b8316ea083754b2e9290591f37d94765EiteWebextensionMessageUtf8', false, matched)[0]], ifr.src);
+                }
             }
-            span = document.createElement("span");
-            span.className = span.className + ' b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderSpan';
-            span.appendChild(document.createTextNode(matched));
-            parentNode.insertBefore(span, el);
-            ifr=document.createElement('iframe');
-            nodeVal = nodeVal.substring(foundIndex + matched.length);
-            ifr.id='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i;
-            ifr.className='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrame';
-            ifr.src=browser.runtime.getURL('edit.html');
-            parentNode.insertBefore(ifr, el);
-            ifr=document.getElementById('b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i);
-            ifr.contentWindow.postMessage(['b8316ea083754b2e9290591f37d94765EiteWebextensionMessageUtf8', false, matched)[0]], ifr.src);
         }
     }
 })();
