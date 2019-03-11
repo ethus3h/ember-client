5cef50e934aebc642fc2ccd9198d959296db9e62
ethus3h <kolubat@gmail.com>
2019-03-11 17:19:04 -0400

    Update Git repository using egup-single

--- built/edit-webextension/render-inline-dc-docs.js
+++ built/edit-webextension/render-inline-dc-docs.js
@@ -89,8 +89,8 @@
                     ifr.id='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i;
                     ifr.className='b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrame';
                     ifr.src=browser.runtime.getURL('edit.html');
-                    ifr.style.height='10px';
-                    ifr.style.width=span.clientWidth;
+                    ifr.style.height=span.clientHeight+'px';
+                    ifr.style.width=span.clientWidth+'px';
                     parentNode.insertBefore(ifr, el);
                     ifr=document.getElementById('b8316ea083754b2e9290591f37d94765EiteWebextensionInlineRenderFrameId'+i);
                     ifr.contentWindow.postMessage([ 'b8316ea083754b2e9290591f37d94765EiteWebextensionMessageUtf8', false, matched], ifr.src);
