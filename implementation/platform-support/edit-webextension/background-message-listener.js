b469f8b6d04fa2c99b805aa37e59dee57ed60771
ethus3h <kolubat@gmail.com>
2019-03-12 11:14:18 -0400

    Update Git repository using egup-single

--- implementation/platform-support/edit-webextension/background-message-listener.js
+++ implementation/platform-support/edit-webextension/background-message-listener.js
@@ -1,6 +1,6 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
-window.b8316ea083754b2e9290591f37d94765EiteWebextensionMessageDocumentId
+window.b8316ea083754b2e9290591f37d94765EiteWebextensionMessageDocuments={};
 
 browser.runtime.onMessage.addListener(function(message) {
     // Inline document renderer messages.
@@ -28,15 +28,7 @@ browser.runtime.onMessage.addListener(function(message) {
      * */
     if (message.data[0].contains('b8316ea083754b2e9290591f37d94765EiteWebextensionMessageDocumentId')) {
         // We've been given document contents by the render-inline-dc-docs.js.
-        browser.tabs.query(
-            { currentWindow: true, active: true },
-            function (tabArray) {
-                browser.tabs.sendMessage(tabArray[0].id, message.data).then(response=>{
-                    // Handle response from content script
-                    window.close();
-                }).catch(console.error.bind(console));
-            }
-        )}
+        window.b8316ea083754b2e9290591f37d94765EiteWebextensionMessageDocuments[message.data[0]]=message.data[1];
     }
 );
 
