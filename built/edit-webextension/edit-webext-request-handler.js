41edaeb01c48098f65d13a3d983a09827c36af36
ethus3h <kolubat@gmail.com>
2019-03-14 11:32:53 -0400

    Update Git repository using egup-single

--- built/edit-webextension/edit-webext-request-handler.js
+++ built/edit-webextension/edit-webext-request-handler.js
@@ -86,7 +86,6 @@ window.addEventListener('message', messageEventHandler);
 
 if (window.location.hash.contains('b8316ea083754b2e9290591f37d94765EiteWebextensionMessageDocumentId')) {
     browser.runtime.sendMessage([window.location.hash.substr(1).replace('Document','GetDocumentBy')]).then(async function(responseMessage) {
-        alert(responseMessage.response);
         await setupIfNeeded();
         document.getElementById('DcSelection').style.display='none';
         document.getElementById('editorColumn').style.display='none';
@@ -100,6 +99,7 @@ if (window.location.hash.contains('b8316ea083754b2e9290591f37d94765EiteWebextens
         //await eiteCall('setupIfNeeded', []);
         //alert('erciorcl');
         //alert(await eiteCall('dcaFromDcbnbFragmentUtf8', [new TextEncoder().encode(responseMessage.response)]));
+        alert(await dcaFromDcbnbFragmentUtf8(new TextEncoder().encode(responseMessage.response)));
         await runDocument(await dcaFromDcbnbFragmentUtf8(new TextEncoder().encode(responseMessage.response)));
         //await runDocument(await eiteCall('dcaFromDcbnbFragmentUtf8', [new TextEncoder().encode(responseMessage.response)]));
         //await eiteCall('runDocument', [await eiteCall('dcaFromDcbnbFragmentUtf8', [new TextEncoder().encode(responseMessage.response)])]);
