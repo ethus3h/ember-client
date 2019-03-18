4a551fc1f86c2d3eebc6451654cc0190be8102c8
ethus3h <kolubat@gmail.com>
2019-03-17 23:23:00 -0400

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -2843,25 +2843,32 @@ async function dcbnbGetLastChar(intArrayIn) {
     boolPastFirstBasenbChar = false;
     alert('Starting loop last')
     while (boolContinue) {
-        alert('iter last: '+intArrayRemaining);
+        alert(boolContinue+' iter last: '+intArrayRemaining);
         intArrayNextUtf8 = await pack32(await lastCharOfUtf8String(intArrayRemaining));
         if (await implNot(await isBasenbChar(intArrayNextUtf8))) {
+            alert('Is not basenbchr');
             if (await implEq(0, await count(intArrayRes))) {
+                alert('Count res is 0');
                 intArrayNextUtf8 = intArrayRes;
             }
             intArrayContinue = false;
         }
         else {
+            alert('It is basenbchr');
             if (await isBasenbDistinctRemainderChar(intArrayNextUtf8)) {
+                alert('It is Remainerchar');
                 if (boolPastFirstBasenbChar) {
+                alert('It is Past first char');
                     intArrayContinue = false;
                 }
                 else {
+                alert('Found first remainder char');
                     intArrayRes = await append(intArrayNextUtf8, intArrayRes);
                     boolPastFirstBasenbChar = true;
                 }
             }
             else {
+                alert('It is basenb regular char');
                 intArrayRes = await append(intArrayNextUtf8, intArrayRes);
                 intTempArrayCount = await count(intArrayNextUtf8);
                 intArrayRemaining = await anSubset(intArrayRemaining, 0, await implMul(-1, intTempArrayCount));
