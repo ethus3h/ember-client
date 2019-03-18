5b948c476ff46a6d3b5313ad6c9a00198f0e0034
ethus3h <kolubat@gmail.com>
2019-03-17 22:55:26 -0400

    Update Git repository using egup-single

--- implementation/core-implementations/js/type-conversion.js
+++ implementation/core-implementations/js/type-conversion.js
@@ -53,7 +53,8 @@ async function firstCharOfUtf8String(intArrayInput) {
 async function lastCharOfUtf8String(intArrayInput) {
     // Returns a decimal representing the Unicode codepoint of the last character, given decimal representation of a UTF-8 string as input.
     let utf8decoder = new TextDecoder();
-    return utf8decoder.decode(new Uint8Array(intArrayInput)).codePointAt(0);
+    let tempStrCharArray = [...utf8decoder.decode(new Uint8Array(intArrayInput))];
+    return .codePointAt(0);
 }
 
 async function internalIntBitArrayToBasenbString(intBase, intBitArrayInput) {
