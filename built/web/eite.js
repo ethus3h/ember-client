bb9705a7c6c37868b214f86a2b6ac8b6aed75765
ethus3h <kolubat@gmail.com>
2019-03-19 01:21:31 -0400

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -2029,7 +2029,7 @@ async function byteArrayFromBasenbUtf8(intArrayIn) {
     else {
         intRemainder = await implSub(63497, await unpack32(intArrayRemainderArr));
     }
-    console.log('Remiainedr array is '+intArrayRemainder);
+    console.log('Remiainedr array is '+intArrayRemainderArr+'; reminer is '+intRemainder);
     if (await ngt(await count(intArrayIn), await count(intArrayRemainderArr))) {
         /* All we have is a remainder, or a chunk of a character without a remainder, so return an exception UUID to represent the error (3362daa3-1705-40ec-9a97-59d052fd4037) */
         intArrayRes = [ 51, 98, 218, 163, 23, 5, 64, 236, 154, 151, 89, 208, 82, 253, 64, 55 ];
