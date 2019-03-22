deda268cc0dc556f2c7a5c69e1da85df19deae20
ethus3h <kolubat@gmail.com>
2019-03-21 22:06:14 -0400

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -855,6 +855,7 @@ async function firstCharOfUtf8String(intArrayInput) {
 async function lastCharOfUtf8String(intArrayInput) {
     // Returns a decimal representing the Unicode codepoint of the last character, given decimal representation of a UTF-8 string as input.
     let utf8decoder = new TextDecoder();
+    console.log('Working on '+intArrayInput);
     // You have got to be kidding me. https://web.archive.org/web/20190318025116/https://stackoverflow.com/questions/46157867/how-to-get-the-nth-unicode-character-from-a-string-in-javascript
     let tempStrCharArray = [...utf8decoder.decode(new Uint8Array(intArrayInput))];
     return tempStrCharArray.slice(-1)[0].codePointAt(0);
