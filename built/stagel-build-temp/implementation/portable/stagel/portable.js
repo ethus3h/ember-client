ab4db63cd0f6fa642e569a0ff742e494ffae1ade
ethus3h <kolubat@gmail.com>
2019-03-23 21:40:27 -0400

    [egup] Update repository metadata

--- built/stagel-build-temp/implementation/portable/stagel/portable.js
+++ built/stagel-build-temp/implementation/portable/stagel/portable.js
@@ -1,2 +1,59 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
+async function runTestsOnly(boolV) {
+    await internalDebugCollect('bool V = ' + boolV + '; '); await internalDebugStackEnter('runTestsOnly:unit-testing'); await assertIsBool(boolV); let boolReturn;
+
+    /* Run tests without report. b/v=verbose: true=print test result lines; false=return value only */
+    /* This runs each component's test suite */
+    /* General tests */
+    /*runTestsBits b/v */
+    await runTestsMath(boolV);
+    await runTestsPack32(boolV);
+    await runTestsTypeConversion(boolV);
+    /*runTestsWasm b/v */
+    /* Core tests */
+    await runTestsDcData(boolV);
+    await runTestsFormatDc(boolV);
+    /* Format tests */
+    await runTestsFormatAscii(boolV);
+    await runTestsFormatAsciiSafeSubset(boolV);
+    await runTestsFormatHtml(boolV);
+    await runTestsFormatHtmlFragment(boolV);
+    await runTestsFormatIntegerList(boolV);
+    await runTestsFormatSems(boolV);
+    await runTestsFormatUtf8(boolV);
+    /* Did anything fail? */
+    if (await implEq(intFailedTests, 0)) {
+
+        boolReturn = true; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
+    }
+
+    boolReturn = false; await assertIsBool(boolReturn); await internalDebugStackExit(); return boolReturn;
+}
+
+async function testing(boolV, strTestSuite) {
+    await internalDebugCollect('bool V = ' + boolV + '; '); await internalDebugCollect('str TestSuite = ' + strTestSuite + '; '); await internalDebugStackEnter('testing:unit-testing'); await assertIsBool(boolV); await assertIsStr(strTestSuite);
+
+    if (boolV) {
+        intArrayTestFrameBuffer = await append(intArrayTestFrameBuffer, await prepareStrForEcho(await implCat('Started running test suite: ', strTestSuite)));
+        await renderDrawContents(intArrayTestFrameBuffer);
+    }
+
+    await internalDebugStackExit();
+}
+
+async function runTest(boolV, boolTestReturn) {
+    await internalDebugCollect('bool V = ' + boolV + '; '); await internalDebugCollect('bool TestReturn = ' + boolTestReturn + '; '); await internalDebugStackEnter('runTest:unit-testing'); await assertIsBool(boolV); await assertIsBool(boolTestReturn); let boolReturn;
+
+    intTotalTests = await implAdd(intTotalTests, 1);
+    if (boolTestReturn) {
+        if (boolV) {
+            intArrayTestFrameBuffer = await append(intArrayTestFrameBuffer, await prepareStrForEcho(await implCat('Test #', await implCat(await strFrom(intTotalTests), ' passed.'))));
+        }
+        intPassedTests = await implAdd(intPassedTests, 1);
+    }
+    else {
+        if (boolV) {
+            intArrayTestFrameBuffer = await append(intArrayTestFrameBuffer, await prepareStrForEcho(await implCat('Test #', await implCat(await strFrom(intTotalTests), ' failed.'))));
+        }
+        intFailedTests = await implAdd(
