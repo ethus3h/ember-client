e302728b58dc4fe754fd9fe9eb262ee3e2fbaa39
ethus3h <kolubat@gmail.com>
2019-03-28 20:33:46 -0400

    Update Git repository using egup-single

--- implementation/core-implementations/js/storage.js
+++ implementation/core-implementations/js/storage.js
@@ -8,7 +8,13 @@ async function storageSetup() {
 }
 
 async function storageSave(data) {
-    
+    node.files.add(node.types.Buffer.from(data), (err, files) => {
+    if (err) return console.error(err)
+
+    // 'hash', known as CID, is a string uniquely addressing the data
+    // and can be used to get it again. 'files' is an array because
+    // 'add' supports multiple additions, but we only added one entry
+    console.log(files[0].hash)
 }
 
 async function storageRetrieve(id) {
