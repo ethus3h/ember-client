53b9be8b2a78b29e3b1d4e9ad1737b84b9f09679
ethus3h <kolubat@gmail.com>
2019-03-28 20:35:51 -0400

    Update Git repository using egup-single

--- implementation/core-implementations/js/storage.js
+++ implementation/core-implementations/js/storage.js
@@ -9,14 +9,19 @@ async function storageSetup() {
 
 async function storageSave(data) {
     node.files.add(node.types.Buffer.from(data), (err, files) => {
-    if (err) return console.error(err)
-
-    // 'hash', known as CID, is a string uniquely addressing the data
-    // and can be used to get it again. 'files' is an array because
-    // 'add' supports multiple additions, but we only added one entry
-    console.log(files[0].hash)
+        if (err) {
+            await implDie(err.toString());
+        }
+        // "'hash', known as CID, is a string uniquely addressing the data and can be used to get it again. 'files' is an array because 'add' supports multiple additions, but we only added one entry" —https://js.ipfs.io/
+        return files[0].hash;
+    });
 }
 
 async function storageRetrieve(id) {
-    
+    node.files.cat('QmPChd2hVbrJ6bfo3WBcTW4iZnpHm8TEzWkLHmLpXhF68A', (err, data) => {
+    if (err) return console.error(err)
+
+    // convert Buffer back to string
+    console.log(data.toString())
+  })
 }
