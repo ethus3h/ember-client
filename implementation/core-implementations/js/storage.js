429dd6983a7147772a0dacce9b3cafbd4ea730a1
ethus3h <kolubat@gmail.com>
2019-03-28 20:30:16 -0400

    Update Git repository using egup-single

--- implementation/core-implementations/js/storage.js
+++ implementation/core-implementations/js/storage.js
@@ -1,15 +1,8 @@
 async function storageSetup() {
     ipfsNode = new IPFS();
-    let response = await new Promise(resolve => {
-        var oReq = new XMLHttpRequest();
-        oReq.open('GET', path, true);
-        oReq.responseType = 'arraybuffer';
-        oReq.onload = function(oEvent) {
-            resolve(new Uint8Array(oReq.response)); // Note: not oReq.responseText
-        };
-        oReq.onerror = function() {
-            resolve(undefined);
-        }
-        oReq.send(null);
+    await new Promise(resolve => {
+        ipfsNode.once('ready', () => {
+            resolve()
+        });
     });
 }
