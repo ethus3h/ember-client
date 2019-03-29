6a4bb7968528491fc9b19cf540e1385956724648
ethus3h <kolubat@gmail.com>
2019-03-28 22:10:54 -0400

    Update Git repository using egup-single

--- implementation/core-implementations/js/storage.js
+++ implementation/core-implementations/js/storage.js
@@ -1,10 +1,11 @@
 async function storageSetup() {
-    ipfsNode = new IPFS();
+    // Later, use OrbitDB. Currently they don't support granting write access after a database has been created, which makes it unusable for this.
+    /* ipfsNode = new IPFS();
     await new Promise(resolve => {
         ipfsNode.once('ready', () => {
             resolve()
         });
-    });
+    }); */
 }
 
 async function storageSave(data) {
@@ -12,20 +13,20 @@ async function storageSave(data) {
     if (data.constructor.name !== 'Uint8Array') {
         data = new Uint8Array(data);
     }
-    ipfsNode.files.add(ipfsNode.types.Buffer.from(data), (err, files) => {
+    /* ipfsNode.files.add(ipfsNode.types.Buffer.from(data), (err, files) => {
         if (err) {
             await implDie(err.toString());
         }
         // "'hash', known as CID, is a string uniquely addressing the data and can be used to get it again. 'files' is an array because 'add' supports multiple additions, but we only added one entry" —https://js.ipfs.io/
         return files[0].hash;
-    });
+    }); */
 }
 
 async function storageRetrieve(id) {
-    ipfsNode.files.cat(id, (err, data) => {
+    /* ipfsNode.files.cat(id, (err, data) => {
         if (err) {
             await implDie(err.toString());
         }
         return new Uint8Array(data);
-    });
+    }); */
 }
