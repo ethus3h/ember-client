c44e0abf4c3196ee8e8201812b9fd65f30e7ed38
ethus3h <kolubat@gmail.com>
2019-04-01 04:51:14 -0400

    Update Git repository using egup-single

--- implementation/platform-support/web/main.js
+++ implementation/platform-support/web/main.js
@@ -4,7 +4,7 @@ window.onload = async function() {
     //eiteCall('startEite');
     let doc = await eiteCall('loadStoredDocument', ['sems', 'eite.sems']);
     let execId = await eiteCall('runDocumentPrepare', [doc]);
-    await eiteCall('setExecOption', ['stopExecAtTick', 30]);
+    await eiteCall('setExecOption', [execId, 'stopExecAtTick', 30]);
     await eiteCall('runDocumentGo', [execId]);
 };
 
