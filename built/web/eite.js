42c38e02ebb393e38465191a493afb9d89ea9b43
ethus3h <kolubat@gmail.com>
2019-04-23 11:07:28 -0400

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -6621,7 +6621,7 @@ async function kvSetValue(strArrayData, strKey, strVal) {
                 boolContinue = false;
             }
             else {
-                if (await implNot(await implLt(intC, intL))) {
+                if (await implNot(await implLe(intC, intL))) {
                     boolContinue = false;
                 }
                 if (await implEq(0, await implMod(intC, 2))) {
