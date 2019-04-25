faac980e1136813fab7611fe8bf1f1c6638698a0
ethus3h <kolubat@gmail.com>
2019-04-24 22:50:53 -0400

    Update Git repository using egup-single

--- implementation/platform-support/web/api.php
+++ implementation/platform-support/web/api.php
@@ -95,15 +95,17 @@ function validateSession() {
 }
 if ($action==='getSession') {
     $userData=$database->getRow($table, "publicId", $user);
-    if($userData["hashedSecretKey"]===password_hash($secretkey)) {
-        $newSession=uuidgen();
-        $database->addRowFromArrays('idxSession', ['nodeId', 'sessionKey', 'created', 'expires', 'events'], ['NULL', $newSession, $timestamp, $timestamp + 1000, '']);
-        $resultsArray=$newSession;
+    if ($userData != null) {
+        if($userData["hashedSecretKey"]===password_hash($secretkey, PASSWORD_DEFAULT)) {
+            $newSession=uuidgen();
+            $database->addRowFromArrays('idxSession', ['nodeId', 'sessionKey', 'created', 'expires', 'events'], ['NULL', $newSession, $timestamp, $timestamp + 1000, '']);
+            $resultsArray=$newSession;
+        }
+    } else {
+        $resultsArray="ERROR: Unknown user. c5e74673-32dd-408a-be6e-165361256fba";
     }
-            $resultsArray="ERROR: Unknown action. 29a80dff-cbf7-4183-a645-4b6af5a50bdf";
-
 } elseif ($action==='hashSecret') {
-    $resultsArray=password_hash($secretkey);
+    $resultsArray=password_hash($secretkey, PASSWORD_DEFAULT);
 } elseif (validateSession()) {
     if ($action==='getTable') {
         $resultsArray=$database->getTable($table);
