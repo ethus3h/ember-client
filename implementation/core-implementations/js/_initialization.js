999c45c5a1fd273d91bbcfa372b272bbb41ed427
ethus3h <kolubat@gmail.com>
2019-04-26 21:19:36 -0400

    Update Git repository using egup-single

--- implementation/core-implementations/js/_initialization.js
+++ implementation/core-implementations/js/_initialization.js
@@ -1,6 +1,6 @@
 await eiteLibrarySetup();
 async function eiteLibrarySetup() {
-    // This function is run when the eite is imported as a script tag. It has to be manually run when eite is imported as a module.
+    // This function is run when the eite is imported as a script tag. It has to be manually run when eite is imported as a module (unless you call setupIfNeeded or an API interface that calls it for you as the first thing after importing it).
     // Preferences (most preferences should be implemented in EITE itself rather than this implementation of its data format): set defaults if not set already
     if (getSharedState('STAGEL_DEBUG') === undefined) {
         setSharedState('STAGEL_DEBUG', 1);
