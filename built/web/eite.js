d04e2d1dbd769218aef4293b6ac662242b0c5ee6
ethus3h <kolubat@gmail.com>
2019-04-30 18:05:07 -0400

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -436,10 +436,13 @@ async function eiteLibrarySetup() {
     implDebug('Done basic setup', 0);
 }
 
-async function getSharedState(name) {
-    console.log("Requseting shared state "+name+getWindowOrSelf()['internalDelegateStateRequests']);
+async function getSharedState(name, iterCount) {
+    console.log("Requseting shared state "+name+getWindowOrSelf()['internalDelegateStateRequests']+iterCount);
+    if(iterCount == undefined) {
+        iterCount = 0;
+    }
     if (getWindowOrSelf()['internalDelegateStateRequests'] === true) {
-        return await eiteHostCall('getSharedState', [name]);
+        return await eiteHostCall('getSharedState', [name, iterCount + 1]);
     }
     else {
         return getWindowOrSelf()[name];
