4c54661e572a02d886874b035db47535f251acc4
ethus3h <kolubat@gmail.com>
2019-04-30 22:17:36 -0400

    Update Git repository using egup-single

--- built/web/eite.js
+++ built/web/eite.js
@@ -1533,7 +1533,7 @@ async function dcDatasetLength(dataset) {
 
 async function dcDataLookupById(dataset, rowNum, fieldNum) {
     console.log(',8');
-    await assertIsDcDataset(dataset); await assertIsInt(rowNum); await assertIsInt(fieldNum); let strReturn;
+    await assertIsDcDataset(dataset); console.log(',9');await assertIsInt(rowNum); console.log(',10');await assertIsInt(fieldNum);console.log(',11'); let strReturn;
 
     // This routine returns the value of the specified cell of the nth row in the dataset (zero-indexed, such that the 0th row is the first content row, and the header row is not available (would be -1 but isn't available from this routine)).
     console.log(',e');
