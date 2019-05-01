00dabe6ee5455f91e5e0124d886bc9b0b0eeca49
ethus3h <kolubat@gmail.com>
2019-05-01 14:14:16 -0400

    [egup] Update repository metadata

--- implementation/core-implementations/js/_initialization.js
+++ implementation/core-implementations/js/_initialization.js
@@ -501,8 +501,6 @@ async function internalLoadDatasets() {
         temp[dataset] = [];
         // I guess the anonymous functions defined as parameters to the Papa.parse call inherit the value of dataset from the environment where they were defined (i.e., here)??
         temp[dataset] = await eiteHostCall('internalEiteReqLoadDataset', [dataset]);
-        console.log(temp);
-        console.log('BUBUBUBBUBUBUBBUBU');
         await setSharedState('dcData', temp);
         count = count + 1;
     }
