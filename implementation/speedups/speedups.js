4ecb2c3f976d8715851e633b113280f225ab3a17
ethus3h <kolubat@gmail.com>
2019-05-03 12:13:11 -0400

    Update Git repository using egup-single

--- implementation/speedups/speedups.js
+++ implementation/speedups/speedups.js
@@ -134,13 +134,25 @@ registerSpeedup('arrEq', async function (genericArrayA, genericArrayB) {
 registerSpeedup('kvHasValue', async function (strArrayData, strKey) {
     await internalDebugCollect('strArray Data = ' + strArrayData + '; '); await internalDebugCollect('str Key = ' + strKey + '; '); await internalDebugStackEnter('kvHasValue:key-value'); await assertIsStrArray(strArrayData); await assertIsStr(strKey); let boolReturn;
 
-    await assertIsKvArray(strArrayData);//based on https://stackoverflow.com/questions/52723904/every-other-element-in-an-array
+    await assertIsKvArray(strArrayData); //based on https://stackoverflow.com/questions/52723904/every-other-element-in-an-array
     if (strArrayData.filter((elem,i) => i&1).contains(strKey)) {
         await internalDebugStackExit();
         return true;
     }
     await internalDebugStackExit();
     return false;
+});
+
+registerSpeedup('kvGetValue', async function (strArrayData, strKey) {
+    await internalDebugCollect('strArray Data = ' + strArrayData + '; '); await internalDebugCollect('str Key = ' + strKey + '; '); await internalDebugStackEnter('kvGetValue:key-value'); await assertIsStrArray(strArrayData); await assertIsStr(strKey); let boolReturn;
+
+    await assertIsKvArray(strArrayData); //based on https://stackoverflow.com/questions/52723904/every-other-element-in-an-array
+    if (strArrayData.filter((elem,i) => i&1).contains(strKey)) {
+        await internalDebugStackExit();
+        return strArrayData[strKey];
+    }
+    await internalDebugStackExit();
+    return '';
 });
 
 registerSpeedup('kvGetValue', async function (strArrayData, strKey) {
