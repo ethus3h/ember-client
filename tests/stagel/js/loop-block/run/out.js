c33cc770f1a7a13a36767f6c40d594a04064214e
ethus3h <kolubat@gmail.com>
2019-05-17 03:20:51 -0400

    Update Git repository using egup-single

--- tests/stagel/js/loop-block/run/out.js
+++ tests/stagel/js/loop-block/run/out.js
@@ -4,3 +4,7 @@ let intC = 0;
 let intI = 0;
 let strElem = '';
 while (await implLt(intI, await count(strArrayTemp))) {
+strElem = await get(strArrayTemp, intI);
+intI = await inc(intI);
+    intC = await inc(intC);
+}
