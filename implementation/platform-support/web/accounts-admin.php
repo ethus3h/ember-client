76cd7d394766913829bfd5e649ad8ecb3a94136d
ethus3h <kolubat@gmail.com>
2019-06-03 22:39:45 -0400

    Update Git repository using egup-single

--- implementation/platform-support/web/accounts-admin.php
+++ implementation/platform-support/web/accounts-admin.php
@@ -41,25 +41,34 @@ function getParam($name) {
 include('active.fracturedb.php');
 $database=new FractureDB($mysqlTablePrefix.'eite_node', $mysqlUser, $mysqlPassword, $mysqlServer);
 $accessKey=getParam('accessKey');
+$oldPermissions=getParam('oldPermissions');
+$accountId=getParam('accountId');
 if($accessKey === $mysqlPassword) {
-echo '<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8" />
-<style type="text/css" media="all">table,tr,td{border:1px dotted maroon;}"</style>
-<title>User Access Management</title>
-</head>
-<body><a href="/">→ Home</a><br><br>
-<table><thead><tr><th>ID</th><th>Public ID</th><th>Name</th><th>Location</th><th>Employees Count</th><th>Payment Method</th><th>Email</th><th>Other</th><th>Account approved?</th><th>(Dis)Approve</th></tr></thead>
-<tbody>';
-$resultsArray=$database->getTable('idxPerson');
-$counter = 0;
-while ($counter <= (count($resultsArray) - 1)) {
-    $userRow=$resultsArray[$counter];
-    echo '<tr><td>'.$userRow['id'].'</td><td>'.$userRow['publicId'].'</td><td>'.$userRow['name'].'</td><td>'.$userRow['location'].'</td><td>'.$userRow['employeesCount'].'</td><td>'.$userRow['paymentMethod'].'</td><td>'.$userRow['email'].'</td><td>'.$userRow['other'].'</td><td>'.$userRow['permissions'].'</td><td><form method="post" action="accounts-admin.php"><input type="hidden" name="oldPermissions" value="'.$userRow['permissions'].'"><input type="hidden" name="accountId" value="'.$userRow['id'].'"><input type="submit" value="Toggle"></form></td></tr>';
-    $counter++;
-}
-echo '</tbody></table></body></html>';
+    if($oldPermissions === '') {
+        echo '<!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="utf-8" />
+        <style type="text/css" media="all">table,tr,td{border:1px dotted maroon;}"</style>
+        <title>User Access Management</title>
+        </head>
+        <body><a href="/">→ Home</a><br><br>
+        <table><thead><tr><th>ID</th><th>Public ID</th><th>Name</th><th>Location</th><th>Employees Count</th><th>Payment Method</th><th>Email</th><th>Other</th><th>Account approved?</th><th>(Dis)Approve</th></tr></thead>
+        <tbody>';
+        $resultsArray=$database->getTable('idxPerson');
+        $counter = 0;
+        while ($counter <= (count($resultsArray) - 1)) {
+            $userRow=$resultsArray[$counter];
+            echo '<tr><td>'.$userRow['id'].'</td><td>'.$userRow['publicId'].'</td><td>'.$userRow['name'].'</td><td>'.$userRow['location'].'</td><td>'.$userRow['employeesCount'].'</td><td>'.$userRow['paymentMethod'].'</td><td>'.$userRow['email'].'</td><td>'.$userRow['other'].'</td><td>'.$userRow['permissions'].'</td><td><form method="post" action="accounts-admin.php"><input type="hidden" name="oldPermissions" value="'.$userRow['permissions'].'"><input type="hidden" name="accountId" value="'.$userRow['id'].'"><input type="submit" value="Toggle"></form></td></tr>';
+            $counter++;
+        }
+        echo '</tbody></table></body></html>';
+    }
+    else {
+        if($oldPermissions === '0') {
+            setField($table, $field, $value, $id = '')
+        }
+    }
 }
 else {
     http_response_code(403);
