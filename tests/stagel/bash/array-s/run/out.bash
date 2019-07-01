8886c3958880108aa2eee762d0da048b36a0fe42
ethus3h <kolubat@gmail.com>
2019-07-01 00:21:54 -0400

    Update Git repository using egup-single

--- tests/stagel/bash/array-s/run/out.bash
+++ tests/stagel/bash/array-s/run/out.bash
@@ -2,7 +2,7 @@ strFromByte() {intIn="$1"; shift; internalDebugCollect "int In = ${intIn[@]}; ";
 
     let strArrayChars = [];
     strArrayChars=[ "", "", "", "", "", "", "" ];
-    /* 0 through 6 */
+    # 0 through 6
     let strArrayTemp = [];
     strArrayTemp=[ "d", "b", "l", 
     "c", "b", "a" ];
