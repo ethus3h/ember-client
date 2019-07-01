5bede1652c4079ce29c718f1494e5001d240ff8d
ethus3h <kolubat@gmail.com>
2019-07-01 00:04:07 -0400

    [egup] Update repository metadata

--- tests/stagel/js/two-routines/run/out.js
+++ tests/stagel/js/two-routines/run/out.js
@@ -2,12 +2,4 @@ async function fbSwitcher(intNum) {
     await internalDebugCollect('int Num = ' + intNum + '; '); await internalDebugStackEnter('fbSwitcher:in'); await assertIsInt(intNum); let strReturn;
 
 
-    strReturn = 'Fizz'; await assertIsStr(strReturn); await internalDebugStackExit(); return strReturn;
-}
-
-async function fbSwitchar(intNum) {
-    await internalDebugCollect('int Num = ' + intNum + '; '); await internalDebugStackEnter('fbSwitchar:in'); await assertIsInt(intNum); let strReturn;
-
-
-    strReturn = 'Fazz'; await assertIsStr(strReturn); await internalDebugStackExit(); return strReturn;
-}
+    
