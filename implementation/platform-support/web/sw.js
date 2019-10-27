42b1e229359165223a359f9bfdf08cd3a8829568
ethus3h <kolubat@gmail.com>
2019-10-27 15:56:52 -0400

    Update Git repository using egup-single

--- implementation/platform-support/web/sw.js
+++ implementation/platform-support/web/sw.js
@@ -1,7 +1,15 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 
-window.onload = function() {
-    eiteCall('runTests');
-};
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('Service worker registration succeeded:', registration);
+  }, /*catch*/ function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+} else {
+  console.log('Service workers are not supported.');
+}
 
 // @license-end
