03c7c9a7794a29b0b6abb752281cbba09b45d93b
ethus3h <kolubat@gmail.com>
2020-01-01 19:09:10 -0500

    Update Git repository using egup-single

--- implementation/platforms/js/main-loop.js
+++ implementation/platforms/js/main-loop.js
@@ -1,5 +1,5 @@
 // This file contains the implementation of the main loop of the JavaScript implementation of the EITE NDW
 
-function ndw(entrypoint='startEite', ) {
+async function ndw(entrypoint='startEite') {
 
 }
