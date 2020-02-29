230ad00f11162721630985c1a5f87f54cae20826
ethus3h <kolubat@gmail.com>
2020-02-29 01:24:39 -0500

    Update Git repository using egup-single

--- tests/stagel/bash/generic-ga-gi-parameter/out/out.bash
+++ tests/stagel/bash/generic-ga-gi-parameter/out/out.bash
@@ -2,7 +2,7 @@ ne() {
     IFS=$'\037' read -r -a genericArrayA <<< "$1"; shift; IFS=$'\037' read -r -a genericItemB <<< "$1"; shift; StageL_internalDebugCollect "genericArray A = ${genericArrayA[@]}; "; StageL_internalDebugCollect "genericItem B = ${genericItemB[@]}; "; StageL_internalDebugStackEnter 'ne:in'; StageL_assertIsGenericArray "$(join_by $'\037' "${genericArrayA[@]}")"; StageL_assertIsGenericItem "$(join_by $'\037' "${genericItemB[@]}")"
 
     boolTemp='false'
-    boolTemp="$(StageL_Not "$(StageL_Eq "$(join_by $'\037' "${genericArrayA[@]}")" "$(join_by $'\037' "${genericItemB[@]}")")")"
+    boolTemp="$(StageL_not "$(StageL_eq "$(join_by $'\037' "${genericArrayA[@]}")" "$(join_by $'\037' "${genericItemB[@]}")")")"
 
     boolReturn="$boolTemp"; StageL_assertIsBool "$boolReturn"; StageL_internalDebugStackExit; print "$boolReturn"
 }
