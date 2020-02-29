a1dd1c49ce94e9d98d78d4ba16f5ba258e45b6b4
ethus3h <kolubat@gmail.com>
2020-02-28 23:17:57 -0500

    Update Git repository using egup-single

--- tests/stagel/bash/generic-parameter/run/out.bash
+++ tests/stagel/bash/generic-parameter/run/out.bash
@@ -1,8 +1,8 @@
 ne() {
-    genericA="$1"; shift; genericB="$1"; shift; StageL_internalDebugCollect "generic A = ${genericA[@]}; ";StageL_internalDebugCollect "generic B = ${genericB[@]}; ";StageL_internalDebugStackEnter 'ne:in'; StageL_assertIsGeneric "$genericA"; StageL_assertIsGeneric "$genericB"; 
+    genericA="$1"; shift; genericB="$1"; shift; StageL_internalDebugCollect "generic A = ${genericA[@]}; ";StageL_internalDebugCollect "generic B = ${genericB[@]}; ";StageL_internalDebugStackEnter 'ne:in'; StageL_assertIsGeneric "$genericA"; aoeu StageL_assertIsGeneric "$genericB"; aoeu 
 
     boolTemp='false'
     boolTemp="$(StageL_Not "$(StageL_Eq "$genericA" "$genericB")")"
 
-    boolReturn="$boolTemp"; StageL_assertIsBool "$boolReturn";StageL_internalDebugStackExit print "$boolReturn"
+    boolReturn="$boolTemp"; StageL_assertIsBool "$boolReturn"; StageL_internalDebugStackExit print "$boolReturn"
 
