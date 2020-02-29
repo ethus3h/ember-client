1ae50759cddb5b269b7cbd6502f415ade90a141c
ethus3h <kolubat@gmail.com>
2020-02-29 01:13:37 -0500

    [egup] Update repository metadata

--- tests/stagel/js/two-routines/run/out.js
+++ tests/stagel/js/two-routines/run/out.js
@@ -9,4 +9,5 @@ async function fbSwitchar(intNum) {
     StageL_internalDebugCollect('int Num = ' + intNum + '; '); StageL_internalDebugStackEnter('fbSwitchar:in'); StageL_assertIsInt(intNum); let strReturn;
 
 
-    
+    strReturn = 'Fazz'; StageL_assertIsStr(strReturn); StageL_internalDebugStackExit(); return strReturn;
+}
