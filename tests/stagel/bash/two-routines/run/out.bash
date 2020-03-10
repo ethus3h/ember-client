2519d8b1308d954e2cda460ada43ba0d7f829f7a
ethus3h <kolubat@gmail.com>
2020-03-09 20:39:13 -0400

    Update Git repository using egup-single

--- tests/stagel/bash/two-routines/run/out.bash
+++ tests/stagel/bash/two-routines/run/out.bash
@@ -9,4 +9,5 @@ fbSwitchar() {
     intNum="$1"; shift; StageL_internalDebugCollect "int Num = ${intNum[@]}; "; StageL_internalDebugStackEnter 'fbSwitchar:in'; StageL_assertIsInt "$intNum"
 
 
-    strReturn=
+    strReturn='Fazz'; StageL_assertIsStr "$strReturn"; StageL_internalDebugStackExit; print "$strReturn"
+}
