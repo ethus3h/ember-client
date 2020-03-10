8a18b55209d1e96375798596c443379e6dc6d914
ethus3h <kolubat@gmail.com>
2020-03-09 21:13:19 -0400

    Update Git repository using egup-single

--- tests/stagel/bash/array-zero-only/run/out.bash
+++ tests/stagel/bash/array-zero-only/run/out.bash
@@ -1,4 +1,4 @@
 intArrayRes=()
 while [[ "true" == "$(StageL_lt '8' "$(StageL_len "$(join_by $'\037' "${intArrayRes[@]}")")")" ]]; then
-    intArrayRes="$(StageL_push "$(join_by $'\037' '0')")" "$(join_by $'\037' "${intArrayRes[@]}")")"
+    intArrayRes="$(StageL_push "$(join_by $'\037' '0')" "$(join_by $'\037' "${intArrayRes[@]}")")"
 done
