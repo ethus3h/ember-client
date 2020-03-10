82f10d1cd80229ebf498896b9e1857ec34998472
ethus3h <kolubat@gmail.com>
2020-03-09 22:08:03 -0400

    Update Git repository using egup-single

--- tests/stagel/js/if-with-inner-block/out/out.js
+++ tests/stagel/js/if-with-inner-block/out/out.js
@@ -1,9 +1,10 @@
-{
-    if (/* comment */
+async function test(intNum) {
+    StageL_internalDebugCollect('int Num = ' + intNum + '; '); StageL_internalDebugStackEnter('test:in'); StageL_assertIsInt(intNum); let boolReturn;
 
-        StageL_eq(0, StageL_mod(intNum, 3))) {
-        {
-            StageL_eq(0, 0);
-        }
+    if (StageL_eq(0, StageL_mod(intNum, 3))) {
+        let boolOut = false;
+        boolOut = StageL_eq(0, 0);
+
+        boolReturn = boolOut; StageL_assertIsBool(boolReturn); StageL_internalDebugStackExit(); return boolReturn;
     }
 }
--- tests/stagel/js/if-with-inner-block/run/out.js
+++ tests/stagel/js/if-with-inner-block/run/out.js
@@ -1,7 +1,10 @@
-StageL_r/b(StageL_test(intNum));
-{
+async function test(intNum) {
+    StageL_internalDebugCollect('int Num = ' + intNum + '; '); StageL_internalDebugStackEnter('test:in'); StageL_assertIsInt(intNum); let boolReturn;
+
     if (StageL_eq(0, StageL_mod(intNum, 3))) {
         let boolOut = false;
         boolOut = StageL_eq(0, 0);
 
-        
+        boolReturn = boolOut; StageL_assertIsBool(boolReturn); StageL_internalDebugStackExit(); return boolReturn;
+    }
+}
