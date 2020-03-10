87cf5dfa5f7e692d5b3757865c7396e69fcac913
ethus3h <kolubat@gmail.com>
2020-03-09 21:22:06 -0400

    Update Git repository using egup-single

--- tests/stagel/bash/array-s/run/out.bash
+++ tests/stagel/bash/array-s/run/out.bash
@@ -2,10 +2,10 @@ strFromByte() {
     intIn="$1"; shift; StageL_internalDebugCollect "int In = ${intIn[@]}; "; StageL_internalDebugStackEnter 'strFromByte:in'; StageL_assertIsInt "$intIn"
 
     strArrayChars=()
-    strArrayChars=( '' '' '' '' '' '' '' )
+    strArrayChars="$(join_by $'\037' '' '' '' '' '' '' '' )
     # 0 through 6
     strArrayTemp=()
-    strArrayTemp=( 'd' 'b' 'l'
+    strArrayTemp="$(join_by $'\037' 'd' 'b' 'l'
     'c' 'b' 'a' )
     StageL_append "$(join_by $'\037' "${strArrayChars[@]}")" "$(join_by $'\037' "${strArrayTemp[@]}")"
 }
