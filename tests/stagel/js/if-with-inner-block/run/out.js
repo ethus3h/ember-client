09407198967972d4b697a5b12201f99d88a48519
ethus3h <kolubat@gmail.com>
2020-03-09 22:06:01 -0400

    Update Git repository using egup-single

--- tests/stagel/js/if-with-inner-block/run/out.js
+++ tests/stagel/js/if-with-inner-block/run/out.js
@@ -1,5 +1,7 @@
-if (StageL_eq(0, StageL_mod(intNum, 3))) {
-    let boolOut = false;
-    boolOut = StageL_eq(0, 0);
+StageL_r/b(StageL_test(intNum));
+{
+    if (StageL_eq(0, StageL_mod(intNum, 3))) {
+        let boolOut = false;
+        boolOut = StageL_eq(0, 0);
 
-    
+        
