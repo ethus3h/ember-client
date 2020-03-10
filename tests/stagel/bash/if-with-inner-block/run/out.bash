4ada870700ab9b4432259fc2ce8284b226e7d446
ethus3h <kolubat@gmail.com>
2020-03-09 20:31:02 -0400

    Update Git repository using egup-single

--- tests/stagel/bash/if-with-inner-block/run/out.bash
+++ tests/stagel/bash/if-with-inner-block/run/out.bash
@@ -1,9 +1,9 @@
-{
-    if (/* comment */
 
-        await Eq(0, await Mod(intNum, 3))) {
-        {
-            await Eq(0, 0);
+    if [[ "true" == # comment
+
+        StageL_eq '0' "$(StageL_mod "$intNum" '3')" ]]; then
+        
+            StageL_eq '0' '0'
         }
-    }
+    fi
 }
