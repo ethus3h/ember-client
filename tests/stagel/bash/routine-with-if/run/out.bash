4962d5ad9668869a6a3eaf5d2239c46c7237c139
ethus3h <kolubat@gmail.com>
2020-03-09 21:51:49 -0400

    Update Git repository using egup-single

--- tests/stagel/bash/routine-with-if/run/out.bash
+++ tests/stagel/bash/routine-with-if/run/out.bash
@@ -1,5 +1,5 @@
 fbSwitcher() {
-    intNum="$1"; shift; StageL_internalDebugCollect "int Num = ${intNum[@]}; "; StageL_internalDebugStackEnter 'fbSwitcher:in'; StageL_assertIsInt "$intNum"
+    intNum="$1"; shift; StageL_internalDebugCollect "int Num = $(join_by ' ' "${intNum[@]}"); "; StageL_internalDebugStackEnter 'fbSwitcher:in'; StageL_assertIsInt "$intNum"
 
     if [[ "true" == "$(StageL_eq '0' '0')" ]]; then
 
