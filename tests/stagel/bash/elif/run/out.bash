b9c867664628e56f3fef472196efe697dd9d4604
ethus3h <kolubat@gmail.com>
2020-03-09 21:34:11 -0400

    Update Git repository using egup-single

--- tests/stagel/bash/elif/run/out.bash
+++ tests/stagel/bash/elif/run/out.bash
@@ -1,10 +1,8 @@
 strN=''
 if [[ "true" == "$(StageL_eq 'a' "$strN")" ]]; then
     strN='FizzBuzz'
-fi
-else if ("$(StageL_eq 'b' "$strN")" ]]; then
+elif [[ "true" == "$(StageL_eq 'b' "$strN")" ]]; then
     strN='c'
-fi
 else
     strN='b'
-}
+fi
