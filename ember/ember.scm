6ca88ae5c8c5e38110a1dbf52d357169173440a4
ethus3h <kolubat@gmail.com>
2020-03-10 23:35:53 -0400

    Update Git repository using egup-single

--- ember/ember.scm
+++ ember/ember.scm
@@ -11,7 +11,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (ember ember)
+(define-module (dce dce)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
