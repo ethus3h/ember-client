754407fafa44da1e4ccae4b43dbef7db188aa973
ethus3h <kolubat@gmail.com>
2020-03-10 23:36:54 -0400

    Update Git repository using egup-single

--- support/extras/packaging/guix/channels.scm
+++ support/extras/packaging/guix/channels.scm
@@ -1,5 +1,5 @@
 ;; Definitive copy of file is at ember-information-technology-environment/support/extras/packaging/guix/channels.scm
-;; DCE (Deterministic Computing Environment)
+;; DCE (Deterministic Computing Environment) and related software
 (cons (channel
         (name 'dce')
         (url "https://gitlab.com/ethus3h/ember-information-technology-environment.git"))
