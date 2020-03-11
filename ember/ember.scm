29eaad34d72a04648dbbc764a1bfb70ed064e44e
ethus3h <kolubat@gmail.com>
2020-03-10 23:33:52 -0400

    Update Git repository using egup-single

--- ember/ember.scm
+++ ember/ember.scm
@@ -24,7 +24,7 @@
     (version "1.1.4.473")
     (source (origin
               (method url-fetch)
-              (uri (string-append "http://web.archive.org/web/20200311033244if_/https://gitlab.com/ethus3h/ember-shared/-/archive/v1.1.4.473/ember-shared-v1.1.4.473.tar.gz" version
+              (uri (string-append "http://web.archive.org/web/20200311033244if_/https://gitlab.com/ethus3h/ember-shared/-/archive/v"1.1.4.473"/ember-shared-v" version
                                   ".tar.gz"))
               (sha256
                (base32
