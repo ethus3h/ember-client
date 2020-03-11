62887cdef019a0f9d9752b18e876b8fec03d5ba7
ethus3h <kolubat@gmail.com>
2020-03-10 23:43:35 -0400

    Update Git repository using egup-single

--- dce/dce.scm
+++ dce/dce.scm
@@ -31,7 +31,7 @@
     (build-system gnu-build-system)
     (arguments '(#:phases (modify-phases %standard-phases (delete 'configure))))
     (inputs `(("xxd" ,xxd)))
-    (synopsis "Hello, GNU world: An example GNU package")
+    (synopsis "ember-shared error-notify script")
     (description "Guess what GNU Hello prints!")
     (home-page "https://www.gnu.org/software/hello/")
     (license agpl3+)))
