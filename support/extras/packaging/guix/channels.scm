489cdb5bb4b6faabda2d4b117628b35bb3f2917e
ethus3h <kolubat@gmail.com>
2020-03-10 23:23:16 -0400

    Update Git repository using egup-single

--- support/extras/packaging/guix/channels.scm
+++ support/extras/packaging/guix/channels.scm
@@ -1,3 +1,4 @@
+;; Definitive copy of file is at ember-information-technology-environment/support/extras/packaging/guix/channels.scm
 (cons (channel
         (name 'ember')
         (url "https://gitlab.com/ethus3h/ember-information-technology-environment.git"))
