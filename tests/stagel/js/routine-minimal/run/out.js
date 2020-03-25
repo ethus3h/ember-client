48253b2844254fd125365cd5865c8f750bf1b525
ethus3h <kolubat@gmail.com>
2020-03-24 23:20:25 -0400

    Update Git repository using egup-single

--- tests/stagel/js/routine-minimal/run/out.js
+++ tests/stagel/js/routine-minimal/run/out.js
@@ -1,7 +1,3 @@
 async function fbSwitcher(intNum) {
     StageL_internalDebugCollect('int Num = ' + intNum + '; '); StageL_internalDebugStackEnter('fbSwitcher:in'); StageL_assertIsInt(intNum); let strReturn;
 
-    /* Test comment; semicolon */
-
-    strReturn = ''; StageL_assertIsStr(strReturn); StageL_internalDebugStackExit(); return strReturn;
-}
